75ae063ee0325efa31bfc7ff6ed1fe5f187dffc6
David Ok <david.ok8@gmail.com>
2013-10-16 21:24:14 +0200

    MultiArray extension (work in progress)
    This is subject to change.

--- src/DO/Core/MultiArray.hpp
+++ src/DO/Core/MultiArray.hpp
@@ -142,11 +142,11 @@ namespace DO {
       , is_wrapped_data_(false)
     {}
     //! Constructor that wraps plain data with its known sizes.
-    inline MultiArray(value_type *data, const vector_type& sizes)
+    inline MultiArray(value_type *data, const vector_type& sizes, bool getDataOwnership = false)
       : begin_(data)
       , end_(data+compute_size(sizes))
       , sizes_(sizes), strides_(compute_strides(sizes))
-      , is_wrapped_data_(true)
+      , is_wrapped_data_(!getDataOwnership)
     {}
     //! \brief Default constructor that allocates an N-dimensional array with 
     //! the specified sizes.
