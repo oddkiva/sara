2e725ace4217c3a9694d8731b19c02fcf2d77f6b
David OK <david.ok8@gmail.com>
2014-01-28 01:25:08 +0100

    Added other useful 3D geometric objects.

---
+++ src/DO/Geometry/Objects/CSG.hpp
@@ -0,0 +1,83 @@
+// ========================================================================== //
+// This file is part of DO++, a basic set of libraries in C++ for computer 
+// vision.
+//
+// Copyright (C) 2013 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public 
+// License v. 2.0. If a copy of the MPL was not distributed with this file, 
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+#pragma once
+
+#include <DO/Core/EigenExtension.hpp>
+
+namespace DO { namespace CSG {
+
+  class Object
+  {
+  public:
+    virtual bool contains(const Point2d& p) const = 0;
+  };
+
+  template <typename Obj>
+  class Singleton : public Object
+  {
+    Obj obj_;
+  public:
+    explicit Singleton(const Obj& obj) : obj_(obj) {}
+    bool contains(const Point2d& p) const
+    { return inside(p, obj_); }
+  };
+
+  //! CSG object difference
+  class Difference : public Object
+  {
+    const Object *o1_;
+    const Object *o2_;
+  public:
+    inline Difference(const Object *o1, const Object *o2)
+      : o1_(o1), o2_(o2) {}
+    bool contains(const Point2d& p) const
+    { return o1_->contains(p) && !o2_->contains(p); }
+  };  
+
+  //! CSG object union
+  class Union : public Object
+  {
+    const Object *o1_;
+    const Object *o2_;
+  public:
+    inline Union(const Object *o1, const Object *o2)
+      : o1_(o1), o2_(o2) {}
+    bool contains(const Point2d& p) const
+    { return o1_->contains(p) || o2_->contains(p); }
+  };
+
+  //! CSG object intersection
+  class Intersection : public Object
+  {
+    const Object *o1_;
+    const Object *o2_;
+  public:
+    inline Intersection(const Object *o1, const Object *o2)
+      : o1_(o1), o2_(o2) {}
+    bool contains(const Point2d& p) const
+    { return o1_->contains(p) && o2_->contains(p); }
+  };
+
+  //! Difference of two objects
+  inline Difference operator-(const Object& o1, const Object& o2)
+  { return Difference(&o1, &o2); }
+
+  //! Intersection of two objects
+  inline Intersection operator*(const Object& o1, const Object& o2)
+  { return Intersection(&o1, &o2); }
+
+  //! Union of two objects
+  inline Union operator+(const Object& o1, const Object& o2)
+  { return Union(&o1, &o2); }
+
+} /* namespace CSG */
+} /* namespace DO */
---
+++ src/DO/Geometry/Objects/Cube.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace DO {
+
+  class Cube
+  {
+    Point3d a_, b_;
+  public:
+    Cube() {}
+    Cube(Vector3d& origin, double side)
+      : a_(origin), b_((origin.array()+side).matrix()) {}
+
+    Point3d const& a() const { return a_; }
+    Point3d const& b() const { return b_; }
+
+    friend bool inside(const Point3d& p, const Cube& cube)
+    { return p.cwiseMin(cube.a_) == cube.a_ && p.cwiseMax(cube.b_) == cube.b_; }
+
+    friend bool area(const Cube& c)
+    { return std::pow((c.b_ - c.a_)(0), 3); }
+  };
+
+}
