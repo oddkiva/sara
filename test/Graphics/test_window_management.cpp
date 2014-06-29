5edc2cb96592e2349e53b76f309b94a70a3d2471
David OK <david.ok8@gmail.com>
2014-06-29 01:45:53 +0100

    Temporarily disable tests to check there is no segfault in Travis CI.

--- test/Graphics/test_window_management.cpp
+++ test/Graphics/test_window_management.cpp
@@ -37,7 +37,7 @@ TEST(TestWindow, test_open_and_close_window)
   EXPECT_TRUE(guarded_widget.isNull());
 }
 
-TEST(TestWindow, test_open_and_close_gl_window)
+TEST(TestWindow, DISABLED_test_open_and_close_window)
 {
   Window w = openGLWindow(300, 300, "My Window", 10, 10);
   EXPECT_NE(w, Window(0));
@@ -54,7 +54,7 @@ TEST(TestWindow, test_open_and_close_gl_window)
   EXPECT_TRUE(guarded_widget.isNull());
 }
 
-TEST(TestWindow, test_open_and_close_graphics_view)
+TEST(TestWindow, DISABLED_test_open_and_close_window)
 {
   Window w = openGraphicsView(300, 300, "My Window", 10, 10);
   EXPECT_NE(w, Window(0));
@@ -71,29 +71,29 @@ TEST(TestWindow, test_open_and_close_graphics_view)
   EXPECT_TRUE(guarded_widget.isNull());
 }
 
-TEST(TestWindow, test_get_and_set_active_window)
+TEST(TestWindow, DISABLED_test_open_and_close_window)
 {
   Window w1 = openWindow(300, 300, "My Window", 10, 10);
   Window w2 = openGLWindow(300, 300, "My GL Window", 10, 10);
-  // TODO: Fixme.
-  //Window w3 = openGLGraphicsView(300, 300, "My Graphics View", 10, 10);
+  // TODO: FIXME.
+  //Window w3 = openGraphicsView(300, 300, "My Graphics View", 10, 10);
 
   EXPECT_EQ(w1, getActiveWindow());
 
   setActiveWindow(w2);
   EXPECT_EQ(w2, getActiveWindow());
 
-  // TODO: Fixme.
+  // TODO: FIXME.
   //setActiveWindow(w3);
   //EXPECT_EQ(w3, getActiveWindow());
 
   closeWindow(w1);
   closeWindow(w2);
-  // TODO: Fixme.
+  // TODO: FIXME.
   //closeWindow(w3);
 }
 
-TEST(TestWindow, test_resize_window)
+TEST(TestWindow, DISABLED_test_open_and_close_window)
 {
   Window w = openWindow(300, 300, "My Window", 10, 10);
   EXPECT_EQ(w, getActiveWindow());
