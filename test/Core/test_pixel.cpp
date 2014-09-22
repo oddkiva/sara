498649dfe95f81eb5c3e4b87ef3d0b02fff29fd7
David OK <david.ok8@gmail.com>
2014-09-22 03:51:23 +0100

    Rewrite unit tests for the 'Pixel' class.

--- test/Core/test_pixel.cpp
+++ test/Core/test_pixel.cpp
@@ -9,68 +9,30 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 // ========================================================================== //
 
+#include <limits>
+#include <stdint.h>
+#include <vector>
+
 #include <gtest/gtest.h>
-#include <DO/Core/Color.hpp>
 
-using namespace DO;
+#include "pixel.hpp"
+
+
 using namespace std;
+using namespace DO;
 
-template <class ChannelType>
-class RgbTest : public testing::Test
+
+TEST(Test_Pixel, test_rgb_32f)
 {
-protected:
-  typedef testing::Test Base;
-  RgbTest() : Base() {}
-};
+  typedef Pixel<float, Rgb> Rgb32f;
 
-typedef testing::Types<
-  unsigned char, unsigned short, unsigned int,
-  char, short, int, float, double
-> ChannelTypes;
-
-TYPED_TEST_CASE_P(RgbTest);
-
-TYPED_TEST_P(RgbTest, assignmentTest)
-{
-  typedef TypeParam ChannelType, T;
-  typedef Color<ChannelType, Rgb> Color3;
-
-  Color3 a1(black<ChannelType>());
-  EXPECT_EQ(a1, black<ChannelType>());
-
-  Color3 a2(1, 2, 3);
-  EXPECT_EQ(a2(0), 1);
-  EXPECT_EQ(a2(1), 2);
-  EXPECT_EQ(a2(2), 3);
-
-  // Test mutable getters.
-  a1.template channel<R>() = 64;
-  a1.template channel<G>() = 12;
-  a1.template channel<B>() = 124;
-  EXPECT_EQ(a1.template channel<R>(), static_cast<T>(64));
-  EXPECT_EQ(a1.template channel<G>(), static_cast<T>(12));
-  EXPECT_EQ(a1.template channel<B>(), static_cast<T>(124));
-
-  // Test immutable getters.
-  const Color3& ca1 = a1;
-  EXPECT_EQ(ca1.template channel<R>(), static_cast<T>(64));
-  EXPECT_EQ(ca1.template channel<G>(), static_cast<T>(12));
-  EXPECT_EQ(ca1.template channel<B>(), static_cast<T>(124));
-
-  // Test assignment for each channel of the RGB pixel.
-  red(a1) = 89; green(a1) = 50; blue(a1) = 12;
-  EXPECT_EQ(red(a1), static_cast<T>(89));
-  EXPECT_EQ(green(a1), static_cast<T>(50));
-  EXPECT_EQ(blue(a1), static_cast<T>(12));
-
-  // 
-  EXPECT_EQ(red(ca1), static_cast<T>(89));
-  EXPECT_EQ(green(ca1), static_cast<T>(50));
-  EXPECT_EQ(blue(ca1), static_cast<T>(12));
+  Rgb32f red(1., 0, 0);
+  EXPECT_EQ(red.channel<R>(), 1.f);
+  EXPECT_EQ(red.channel<G>(), 0.f);
+  EXPECT_EQ(red.channel<B>(), 0.f);
+  EXPECT_EQ(red.num_channels(), 3);
 }
 
-REGISTER_TYPED_TEST_CASE_P(RgbTest, assignmentTest);
-INSTANTIATE_TYPED_TEST_CASE_P(DO_Core_Test, RgbTest, ChannelTypes);
 
 int main(int argc, char** argv) 
 {
