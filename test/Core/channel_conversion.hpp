d87bc58139900b92d4e4977adc9eae9c7f660092
David OK <david.ok8@gmail.com>
2014-09-22 03:53:32 +0100

    Distinguish channel conversion and color conversion.

---
+++ test/Core/channel_conversion.hpp
@@ -0,0 +1,119 @@
+#pragma once
+
+
+#include <DO/Core/StaticAssert.hpp>
+#include "colorspace.hpp"
+
+
+// Channel conversion.
+namespace DO {
+
+  //! \brief Convert integral channel value to floating-point value.
+  template <typename Int, typename Float>
+  inline Float float_normalized_channel(Int src)
+  {
+    DO_STATIC_ASSERT(
+      std::numeric_limits<Int>::is_integer,
+      CHANNEL_CONVERSION_MUST_BE_FROM_INTEGER_TYPE_TO_FLOATING_POINT_TYPE);
+
+    using std::numeric_limits;
+    const Float float_min = static_cast<Float>(numeric_limits<Int>::min());
+    const Float float_max = static_cast<Float>(numeric_limits<Int>::max());
+    const Float float_range = float_max - float_min; 
+    return (static_cast<Float>(src) - float_min) / float_range;
+  }
+  
+  //! \brief Convert floating-point channel value to integer value.
+  template <typename Int, typename Float>
+  inline Int int_rescaled_channel(Float src)
+  {
+    DO_STATIC_ASSERT(
+      std::numeric_limits<Int>::is_integer,
+      CHANNEL_CONVERSION_MUST_BE_FROM_FLOATING_POINT_TYPE_TO_INTEGER_TYPE);
+
+    using std::numeric_limits;
+    const Float float_min = static_cast<Float>(numeric_limits<Int>::min());
+    const Float float_max = static_cast<Float>(numeric_limits<Int>::max());
+    const Float float_range = float_max - float_min; 
+    src = float_min + src * float_range;
+    
+    const Float delta_max = std::abs(src-float_max)/float_range;
+    const Float delta_min = std::abs(src-float_min)/float_range;
+    const Float eps = sizeof(Float) == 4 ?
+      Float(1e-5) : // 'Float' == 'float'
+      Float(1e-9);  // 'Float' == 'double'
+
+    Int dst;
+    if (delta_max <= eps)
+      dst = std::numeric_limits<Int>::max();
+    else if (delta_min <= eps)
+      dst = std::numeric_limits<Int>::min();
+    else
+      dst = static_cast<Int>(floor(src + 0.5));
+    return dst;
+  }
+
+} /* namespace DO */
+
+
+// Gray channel conversion.
+namespace DO {
+
+  //! \brief Convert a double gray value to a float gray value. 
+  inline void convert_channel(double src, float& dst)
+  {
+    dst = static_cast<float>(src);
+  }
+
+  //! \brief Convert a float gray value to a double gray value. 
+  inline void convert_channel(float src, double& dst)
+  {
+    dst = static_cast<double>(src);
+  }
+
+  //! \brief Convert an integer gray value to a float gray value.
+  template <typename Int>
+  inline void convert_channel(Int src, float& dst)
+  {
+    dst = float_normalized_channel<Int, float>(src);
+  }
+
+  //! \brief Convert an integer gray value to a double gray value.
+  template <typename Int>
+  inline void convert_channel(Int src, double& dst)
+  {
+    dst = float_normalized_channel<Int, double>(src);
+  }
+
+  //! \brief Convert a float gray value to an integer gray value.
+  template <typename Int>
+  inline void convert_channel(float src, Int& dst)
+  {
+    dst = int_rescaled_channel<Int, float>(src);
+  }
+
+  //! \brief Convert a double gray value to a integer gray value.
+  template <typename Int>
+  inline void convert_channel(double src, Int& dst)
+  {
+    dst = int_rescaled_channel<Int, double>(src);
+  }
+
+  //! \brief Convert an integer gray value to another one.
+  template <typename SrcInt, typename DstInt>
+  inline void convert_channel(SrcInt src, DstInt& dst)
+  {
+    dst =
+      int_rescaled_channel<DstInt, double> (
+      float_normalized_channel<SrcInt, double>(src) );
+  }
+
+  //! \brief Convert channels from a pixel vector to another pixel vector.
+  template <typename T, typename U, int N>
+  inline void convert_channel(const Matrix<T, N, 1>& src, Matrix<U, N, 1>& dst)
+  {
+    for (int i = 0; i < N; ++i)
+      convert_channel(src[i], dst[i]);
+  }
+
+}
---
+++ test/Core/color_conversion.hpp
@@ -0,0 +1,115 @@
+﻿#pragma once
+
+
+#include <DO/Core/Meta.hpp>
+#include <DO/Core/EigenExtension.hpp>
+
+#include "pixel.hpp"
+
+
+// Color space conversion with floating-point values.
+namespace DO {
+
+  //! Convert RGB color to gray color.
+  template <typename T>
+  inline void rgb_to_gray(const Matrix<T, 3, 1>& rgb, T& gray)
+  {
+    DO_STATIC_ASSERT(
+      !std::numeric_limits<T>::is_integer,
+      CONVERSION_FROM_RGB_TO_GRAY_IS_SUPPORTED_ONLY_FOR_FLOATING_POINT_TYPE);
+
+    gray = T(0.2125)*rgb[0] + T(0.7154)*rgb[1] + T(0.0721)*rgb[2]; 
+  }
+
+  //! Convert Grayscale color to RGB color.
+  template <typename T>
+  inline void gray_to_rgb(T gray, Matrix<T, 3, 1>& rgb)
+  {
+    rgb.fill(gray);
+  }
+
+  //! Convert RGB color to YUV color.
+  template <typename T>
+  inline void rgb_to_yuv(const Matrix<T, 3, 1>& rgb, Matrix<T, 3, 1>& yuv)
+  {
+    DO_STATIC_ASSERT(
+      !std::numeric_limits<T>::is_integer,
+      CONVERSION_FROM_GRAY_TO_RGB_IS_SUPPORTED_ONLY_FOR_FLOATING_POINT_TYPE);
+
+    yuv[0] = 0.299*rgb[0] + 0.587*rgb[1] + 0.114*rgb[2];
+    yuv[1] = 0.492*(rgb[2] - yuv[0]);
+    yuv[2] = 0.877*(rgb[0] - yuv[0]);
+  }
+
+  //! Convert YUV color to RGB color.
+  template <typename T>
+  inline void yuv_to_rgb(const Matrix<T, 3, 1>& yuv, Matrix<T, 3, 1>& rgb)
+  {
+    DO_STATIC_ASSERT(
+      !std::numeric_limits<T>::is_integer,
+      CONVERSION_FROM_GRAY_TO_RGB_IS_SUPPORTED_ONLY_FOR_FLOATING_POINT_TYPE);
+
+    rgb[0] = yuv[0]                     + T(1.13983)*yuv[2];
+    rgb[1] = yuv[0] - T(0.39465)*yuv[1] - T(0.58060)*yuv[2];
+    rgb[2] = yuv[0] + T(2.03211)*yuv[1];
+  }
+
+  //! Convert YUV color to gray color.
+  template <typename T>
+  inline void yuv_to_gray(const Matrix<T, 3, 1>& yuv, T& gray)
+  {
+    gray = yuv[0];
+  }
+
+}
+
+// Unified API.
+namespace DO {
+
+  //! Color conversion from RGBA to RGB.
+  template <typename T>
+  void convert_color(Pixel<T, Rgba>& src, const Pixel<T, Rgb>& dst)
+  {
+    for (int i = 0; i < 3; ++i)
+      dst[i] = src[i];
+  }
+
+  //! Pixel conversion from RGB to RGBA.
+  template <typename T>
+  void convert_color(Pixel<T, Rgb>& src, const Pixel<T, Rgba>& dst)
+  {
+    using namespace std;
+    for (int i = 0; i < 3; ++i)
+      dst[i] = src[i];
+    dst[3] = numeric_limits<T>::is_integer ? numeric_limits<T>::max() : T(1);
+  }
+
+  //! \brief Convert color from RGB to gray.
+  template <typename T>
+  inline void convert_color(const Pixel<T, Rgb>& src, T& dst)
+  {
+    rgb_to_gray(src, dst);
+  }
+
+  //! \brief Convert color from gray to RGB.
+  template <typename T>
+  inline void convert_color(T& src, Pixel<T, Rgb>& dst)
+  {
+    gray_to_rgb(src, dst);
+  }
+ 
+  //! \brief Convert color from RGB to YUV.
+  template <typename T>
+  inline void convert_color(const Pixel<T, Rgb>& src, Pixel<T, Yuv>& dst)
+  {
+    rgb_to_yuv(src.template cast<U>(), dst);
+  }
+
+  //! \brief Convert color from YUV to RGB.
+  template <typename T>
+  inline void convert_color(const Pixel<T, Yuv>& src, Pixel<T, Rgb>& dst)
+  {
+    yuv_to_rgb(src, dst);
+  }
+
+}
