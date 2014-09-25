fcac21b4c2a14609b005b4518bca8070a90580d2
David OK <david.ok8@gmail.com>
2014-09-25 21:11:28 +0100

    Reformatted lines of code.

--- src/DO/Core/MultiArray/ElementTraits.hpp
+++ src/DO/Core/MultiArray/ElementTraits.hpp
@@ -1,5 +1,20 @@
+// ========================================================================== //
+// This file is part of DO++, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2014 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+//! @file
+
 #ifndef DO_CORE_MULTIARRAY_ELEMENTTRAITS_HPP
 #define DO_CORE_MULTIARRAY_ELEMENTTRAITS_HPP
+
+#include <DO/Core/EigenExtension.hpp>
 
 
 namespace DO {
@@ -13,39 +28,39 @@ namespace DO {
   template <typename T>
   struct ElementTraits
   {
-    typedef T value_type; //!< STL-like typedef.
-    typedef size_t size_type; //!< STL-like typedef.
-    typedef T * pointer; //!< STL-like typedef.
-    typedef const T * const_pointer; //!< STL-like typedef.
-    typedef T& reference; //!< STL-like typedef.
-    typedef const T& const_reference; //!< STL-like typedef.
-    typedef T * iterator; //!< STL-like typedef.
-    typedef const T * const_iterator; //!< STL-like typedef.
+    typedef T value_type;               //!< STL-like typedef.
+    typedef size_t size_type;           //!< STL-like typedef.
+    typedef T * pointer;                //!< STL-like typedef.
+    typedef const T * const_pointer;    //!< STL-like typedef.
+    typedef T& reference;               //!< STL-like typedef.
+    typedef const T& const_reference;   //!< STL-like typedef.
+    typedef T * iterator;               //!< STL-like typedef.
+    typedef const T * const_iterator;   //!< STL-like typedef.
     static const bool is_scalar = true; //!< STL-like typedef.
   };
 
   //! \brief The specialized element traits class when the entry is a matrix.
   //! Again the matrix is viewed as a scalar. Additions and subtractions between
-  //! matrices are OK but multiplication will be the pointwise matrix 
+  //! matrices are OK but multiplication will be the point-wise matrix 
   //! multiplication.
   //!
   //! This may be questionable and this may change in the future.
   template <typename T, int M, int N>
   struct ElementTraits<Matrix<T, M, N> >
   {
-    const static bool is_square_matrix = (M == N); //!< STL-like typedef.
+    const static bool is_square_matrix = (M == N);  //!< STL-like typedef.
     typedef typename Meta::Choose<
       is_square_matrix,
       Matrix<T, N, N>,
-      Array<T, M, N> >::Type value_type; //!< STL-like typedef.
-    typedef size_t size_type; //!< STL-like typedef.
-    typedef value_type * pointer; //!< STL-like typedef.
-    typedef const value_type * const_pointer; //!< STL-like typedef.
-    typedef value_type& reference; //!< STL-like typedef.
-    typedef const value_type& const_reference; //!< STL-like typedef.
-    typedef value_type * iterator; //!< STL-like typedef.
-    typedef const value_type * const_iterator; //!< STL-like typedef.
-    static const bool is_scalar = false; //!< STL-like typedef.
+      Array<T, M, N> >::Type value_type;            //!< STL-like typedef.
+    typedef size_t size_type;                       //!< STL-like typedef.
+    typedef value_type * pointer;                   //!< STL-like typedef.
+    typedef const value_type * const_pointer;       //!< STL-like typedef.
+    typedef value_type& reference;                  //!< STL-like typedef.
+    typedef const value_type& const_reference;      //!< STL-like typedef.
+    typedef value_type * iterator;                  //!< STL-like typedef.
+    typedef const value_type * const_iterator;      //!< STL-like typedef.
+    static const bool is_scalar = false;            //!< STL-like typedef.
   };
 
   //! \brief The specialized element traits class when the entry is an array.
@@ -53,15 +68,15 @@ namespace DO {
   template <typename T, int M, int N>
   struct ElementTraits<Array<T, M, N> >
   {
-    typedef Array<T, M, N> value_type; //!< STL-like typedef.
-    typedef size_t size_type; //!< STL-like typedef.
-    typedef value_type * pointer; //!< STL-like typedef.
-    typedef const value_type * const_pointer; //!< STL-like typedef.
-    typedef value_type& reference; //!< STL-like typedef.
-    typedef const value_type& const_reference; //!< STL-like typedef.
-    typedef value_type * iterator; //!< STL-like typedef.
-    typedef const value_type * const_iterator; //!< STL-like typedef.
-    static const bool is_scalar = false; //!< STL-like typedef.
+    typedef Array<T, M, N> value_type;          //!< STL-like typedef.
+    typedef size_t size_type;                   //!< STL-like typedef.
+    typedef value_type * pointer;               //!< STL-like typedef.
+    typedef const value_type * const_pointer;   //!< STL-like typedef.
+    typedef value_type& reference;              //!< STL-like typedef.
+    typedef const value_type& const_reference;  //!< STL-like typedef.
+    typedef value_type * iterator;              //!< STL-like typedef.
+    typedef const value_type * const_iterator;  //!< STL-like typedef.
+    static const bool is_scalar = false;        //!< STL-like typedef.
   };
 
   //! @}
