a0a427361cc51f49a893df0fa06c6aa8df905598
David OK <david.ok8@gmail.com>
2014-09-25 21:12:50 +0100

    Add copyright notice.

--- src/DO/Core/Pixel/ColorConversion.hpp
+++ src/DO/Core/Pixel/ColorConversion.hpp
@@ -1,4 +1,15 @@
-﻿#ifndef DO_CORE_PIXEL_COLOR_CONVERSION_HPP
+﻿// ========================================================================== //
+// This file is part of DO++, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2013 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+#ifndef DO_CORE_PIXEL_COLOR_CONVERSION_HPP
 #define DO_CORE_PIXEL_COLOR_CONVERSION_HPP
 
 
