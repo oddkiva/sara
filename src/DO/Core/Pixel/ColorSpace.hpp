38be0cd76ebf54199fbd6446a44bfd706044675d
David Ok <david.ok8@gmail.com>
2014-09-25 04:28:18 +0100

    Add docstrings for classes used for template meta-programming.

--- src/DO/Core/Pixel/ColorSpace.hpp
+++ src/DO/Core/Pixel/ColorSpace.hpp
@@ -7,6 +7,7 @@
 // Template meta-programming utilities.
 namespace DO { namespace Meta {
 
+  //! \brief Array of integers for template meta-programming.
   template <int _Value0, int _Value1, int _Value2>
   struct IntArray_3 {
     enum {
@@ -17,16 +18,25 @@ namespace DO { namespace Meta {
     };
   };
 
+  //! \brief Getter class for the 'IntArray_3' class.
   template <typename IntArray, int index> struct Get;
 
+  //! \brief Getter class for the element 0 of 'IntArray_3'.
   template <typename IntArray> struct Get<IntArray, 0>
   { enum { value = IntArray::value_0 }; };
+
+  //! \brief Getter class for the element 1 of 'IntArray_3'.
   template <typename IntArray> struct Get<IntArray, 1>
   { enum { value = IntArray::value_1 }; };
+
+  //! \brief Getter class for the element 2 of 'IntArray_3'.
   template <typename IntArray> struct Get<IntArray, 2>
   { enum { value = IntArray::value_2 }; };
+
+  //! \brief Getter class for the element 3 of 'IntArray_3'.
   template <typename IntArray> struct Get<IntArray, 3>
   { enum { value = IntArray::value_3 }; };
+  //! @}
 
 } /* namespace Meta */
 } /* namespace DO */
