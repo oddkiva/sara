03075adcaf7e081863d6e4680a99efc6e64fcee0
David OK <david.ok8@gmail.com>
2014-09-26 18:32:59 +0100

    Add stub unit tests for Deriche filter.

---
+++ test/ImageProcessing/test_deriche_filter.cpp
@@ -0,0 +1,34 @@
+// ========================================================================== //
+// This file is part of DO++, a basic set of libraries in C++ for computer 
+// vision.
+//
+// Copyright (C) 2013 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public 
+// License v. 2.0. If a copy of the MPL was not distributed with this file, 
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+
+#include <gtest/gtest.h>
+
+#include <DO/Defines.hpp>
+#include <DO/ImageProcessing/Deriche.hpp>
+
+#include "utilities.hpp"
+
+
+using namespace std;
+using namespace DO;
+
+
+TEST(TestDericheFilter, test_inplace_deriche_blur)
+{
+}
+
+
+int main(int argc, char** argv) 
+{
+  testing::InitGoogleTest(&argc, argv); 
+  return RUN_ALL_TESTS();
+}
