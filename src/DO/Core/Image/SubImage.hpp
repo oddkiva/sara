1864e0647809165c576fa5a52bdf8cf68584545f
David OK <david.ok8@gmail.com>
2014-10-01 03:21:56 +0100

    Rename functions and fix bugs.

--- src/DO/Core/Image/SubImage.hpp
+++ src/DO/Core/Image/SubImage.hpp
@@ -16,28 +16,28 @@
 
 
 #include <DO/Core/Image/Image.hpp>
-#include <DO/Core/Pixel/ChannelConversion.hpp>
+#include <DO/Core/Image/Operations.hpp>
 
 
 namespace DO {
 
   //! Get the subimage of an image.
   template <typename T, int N>
-  Image<T, N> subimage(const Image<T, N>& src,
-                       const Matrix<int, N, 1>& begin_coords,
-                       const Matrix<int, N, 1>& end_coords)
+  Image<T, N> get_subimage(const Image<T, N>& src,
+                           const Matrix<int, N, 1>& begin_coords,
+                           const Matrix<int, N, 1>& end_coords)
   {
-    Image<T,N> dst(begin_coords - end_coords);
+    Image<T, N> dst(end_coords - begin_coords);
 
     typedef typename Image<T, N>::const_subarray_iterator const_subarray_iterator;
-    const_subarray_iterator src_it = src.begin_subrange(a, b);
+    const_subarray_iterator src_it = src.begin_subrange(begin_coords, end_coords);
 
     for (typename Image<T, N>::iterator dst_it = dst.begin();
          dst_it != dst.end(); ++dst_it, ++src_it)
     {
       // If a and b are coordinates out bounds.
-      if ((src_it.position() - begin_coords).minCoeff() < 0 ||
-          (src_it.position() - end_coords).minCoeff() >= 0)
+      if (src_it.position().minCoeff() < 0 ||
+          (src_it.position() - src.sizes()).minCoeff() >= 0)
         *dst_it = color_min_value<T>();
       else
         *dst_it = *src_it;
@@ -47,20 +47,21 @@ namespace DO {
 
   //! \brief Get the subimage of an image.
   template <typename T>
-  inline Image<T> subimage(const Image<T>& src, int top_left_x, int top_left_y,
-                    int width, int height)
+  inline Image<T> get_subimage(const Image<T>& src,
+                               int top_left_x, int top_left_y,
+                               int width, int height)
   {
     Vector2i begin_coords(top_left_x, top_left_y);
     Vector2i end_coords(top_left_x+width, top_left_y+height);
-    return subimage(src, begin_coords, end_coords);
+    return get_subimage(src, begin_coords, end_coords);
   }
 
   //! \brief Get the subimage of an image.
   template <typename T>
-  inline Image<T> subimage(const Image<T>& src, int center_x, int center_y,
-                           int radius)
+  inline Image<T> get_subimage(const Image<T>& src, int center_x, int center_y,
+                               int radius)
   {
-    return subimage(src, center_x-r, center_y-r, 2*r+1, 2*r+1);
+    return get_subimage(src, center_x-radius, center_y-radius, 2*radius+1, 2*radius+1);
   }
 
 } /* namespace DO */
