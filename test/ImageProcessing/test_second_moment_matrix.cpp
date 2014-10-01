cb827c268f18e5cf5a6738eb6c58fc23b7bfe848
David OK <david.ok8@gmail.com>
2014-10-01 04:50:46 +0100

    Add unit test for the computation of second moment matrix.

---
+++ test/ImageProcessing/test_second_moment_matrix.cpp
@@ -0,0 +1,44 @@
+// ========================================================================== //
+// This file is part of DO++, a basic set of libraries in C++ for computer 
+// vision.
+//
+// Copyright (C) 2013 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public 
+// License v. 2.0. If a copy of the MPL was not distributed with this file, 
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+#define _USE_MATH_DEFINES
+
+#include <gtest/gtest.h>
+
+#include <DO/ImageProcessing/SecondMomentMatrix.hpp>
+
+#include "../AssertHelpers.hpp"
+
+
+using namespace DO;
+using namespace std;
+
+
+TEST(TestSecondMomentMatrix, test_second_moment_matrix)
+{
+  Image<Vector2f> vector_field(3, 3);
+  vector_field.matrix().fill(Vector2f::Ones());
+
+  Image<Matrix2f> true_moments(3, 3);
+  true_moments.array().fill(Matrix2f::Ones());
+
+  Image<Matrix2f> moments;
+  moments = vector_field.compute<SecondMomentMatrix>();
+  for (int i = 0; i != moments.size(); ++i)
+    EXPECT_MATRIX_NEAR(true_moments.array()[i], moments.array()[i], 1e-3);
+}
+
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv); 
+  return RUN_ALL_TESTS(); 
+}
