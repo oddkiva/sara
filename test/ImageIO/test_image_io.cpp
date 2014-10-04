72fda90ab7f43b62bbb2d36055b121e6babd2237
David OK <david.ok8@gmail.com>
2014-10-04 19:07:11 +0100

    Rewrite unit tests.

--- test/ImageIO/test_image_io.cpp
+++ test/ImageIO/test_image_io.cpp
@@ -21,13 +21,13 @@ using namespace DO;
 using namespace std;
 
 
-TEST(TestImageIO, test_image_reading)
+TEST(TestImageIO, test_image_read_write)
 {
   string filepaths[] =
   {
-    srcPath("image.jpg"),
-    srcPath("image.png"),
-    srcPath("image.tif")
+    "image.jpg",
+    "image.png",
+    "image.tif"
   };
 
   Image<Rgb8> true_image(2, 2);
@@ -36,24 +36,56 @@ TEST(TestImageIO, test_image_reading)
 
   for (int i = 0; i < 3; ++i)
   {
-    Image<Rgb8> image;
+    imwrite(true_image, filepaths[i], 100);
 
+    Image<Rgb8> image;
     EXPECT_TRUE(imread(image, filepaths[i]));
     EXPECT_MATRIX_EQ(image.sizes(), Vector2i(2, 2));
 
     for (int y = 0; y < true_image.width(); ++y)
-      for (int x = 0; x < true_image.width(); ++x)
+      for (int x = 0; x < true_image.height(); ++x)
         EXPECT_MATRIX_EQ(true_image(x, y), image(x, y));
   }
-
 }
 
 
 TEST(TestImageIO, test_read_exif_info)
 {
-  string filepath = srcPath("image.jpg");
+  string filepath = "image.jpg";
   EXIFInfo exif_info;
-  EXPECT_TRUE(read_exif_info(exif_info, filepath));
+  read_exif_info(exif_info, filepath);
+
+  ostringstream os;
+  os << exif_info;
+  string content = os.str();
+
+  size_t pos;
+
+  pos = content.find("Camera make");        EXPECT_NE(string::npos, pos);
+  pos = content.find("Camera model");       EXPECT_NE(string::npos, pos);
+  pos = content.find("Software");           EXPECT_NE(string::npos, pos);
+  pos = content.find("Bits per sample");    EXPECT_NE(string::npos, pos);
+  pos = content.find("Image width");        EXPECT_NE(string::npos, pos);
+  pos = content.find("Image height");       EXPECT_NE(string::npos, pos);
+  pos = content.find("Image description");  EXPECT_NE(string::npos, pos);
+  pos = content.find("Image orientation");  EXPECT_NE(string::npos, pos);
+  pos = content.find("Image copyright");    EXPECT_NE(string::npos, pos);
+  pos = content.find("Image date/time");    EXPECT_NE(string::npos, pos);
+  pos = content.find("Original date/time"); EXPECT_NE(string::npos, pos);
+  pos = content.find("Digitize date/time"); EXPECT_NE(string::npos, pos);
+  pos = content.find("Subsecond time");     EXPECT_NE(string::npos, pos);
+  pos = content.find("Exposure time");      EXPECT_NE(string::npos, pos);
+  pos = content.find("F-stop");             EXPECT_NE(string::npos, pos);
+  pos = content.find("ISO speed");          EXPECT_NE(string::npos, pos);
+  pos = content.find("Subject distance");   EXPECT_NE(string::npos, pos);
+  pos = content.find("Exposure bias");      EXPECT_NE(string::npos, pos);
+  pos = content.find("Flash used?");        EXPECT_NE(string::npos, pos);
+  pos = content.find("Metering mode");      EXPECT_NE(string::npos, pos);
+  pos = content.find("Lens focal length");  EXPECT_NE(string::npos, pos);
+  pos = content.find("35mm focal length");  EXPECT_NE(string::npos, pos);
+  pos = content.find("GPS Latitude");       EXPECT_NE(string::npos, pos);
+  pos = content.find("GPS Longitude");      EXPECT_NE(string::npos, pos);
+  pos = content.find("GPS Altitude");       EXPECT_NE(string::npos, pos);
 }
 
 int main(int argc, char **argv)
