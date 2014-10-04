a3274f66ad1f73fd0dbfc4d924ff3e74ecaa9b13
David OK <david.ok8@gmail.com>
2014-10-04 17:44:52 +0100

    Workaround for code portability.
    INT16_MAX and so on are not standard for C++ before C++11.

--- test/Core/test_smart_color_conversion.cpp
+++ test/Core/test_smart_color_conversion.cpp
@@ -9,6 +9,7 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 // ========================================================================== //
 
+#include <limits>
 #include <stdint.h>
 
 #include <gtest/gtest.h>
@@ -25,10 +26,13 @@ using namespace DO;
 
 TEST(TestSmartConvertColor, test_rgb_to_gray)
 {
+  const int16_t int16_max = numeric_limits<int16_t>::max();
+  const uint8_t uint8_max = numeric_limits<uint8_t>::max();
+
   Pixel<double, Rgb> rgbd(1,1,1);
   Pixel<float, Rgb> rgbf(1,1,1);
-  Pixel<int16_t, Rgb> rgb16; rgb16.fill(INT16_MAX);
-  Pixel<uint8_t, Rgb> rgb8u; rgb8u.fill(UINT8_MAX);
+  Pixel<int16_t, Rgb> rgb16; rgb16.fill(int16_max);
+  Pixel<uint8_t, Rgb> rgb8u; rgb8u.fill(uint8_max);
 
   double grayd;
   float grayf;
@@ -37,32 +41,35 @@ TEST(TestSmartConvertColor, test_rgb_to_gray)
 
   smart_convert_color(rgbd, grayd);  EXPECT_NEAR(grayd, 1, 1e-3);
   smart_convert_color(rgbd, grayf);  EXPECT_NEAR(grayf, 1, 1e-3);
-  smart_convert_color(rgbd, gray16); EXPECT_EQ(gray16, INT16_MAX);
-  smart_convert_color(rgbd, gray8u); EXPECT_EQ(gray8u, UINT8_MAX);
+  smart_convert_color(rgbd, gray16); EXPECT_EQ(gray16, int16_max);
+  smart_convert_color(rgbd, gray8u); EXPECT_EQ(gray8u, uint8_max);
 
   smart_convert_color(rgbf, grayd);  EXPECT_NEAR(grayd, 1, 1e-3);
   smart_convert_color(rgbf, grayf);  EXPECT_NEAR(grayf, 1, 1e-3);
-  smart_convert_color(rgbf, gray16); EXPECT_EQ(gray16, INT16_MAX);
-  smart_convert_color(rgbf, gray8u); EXPECT_EQ(gray8u, UINT8_MAX);
+  smart_convert_color(rgbf, gray16); EXPECT_EQ(gray16, int16_max);
+  smart_convert_color(rgbf, gray8u); EXPECT_EQ(gray8u, uint8_max);
 
   smart_convert_color(rgb16, grayd);  EXPECT_NEAR(grayd, 1, 1e-3);
   smart_convert_color(rgb16, grayf);  EXPECT_NEAR(grayf, 1, 1e-3);
-  smart_convert_color(rgb16, gray16); EXPECT_EQ(gray16, INT16_MAX);
-  smart_convert_color(rgb16, gray8u); EXPECT_EQ(gray8u, UINT8_MAX);
+  smart_convert_color(rgb16, gray16); EXPECT_EQ(gray16, int16_max);
+  smart_convert_color(rgb16, gray8u); EXPECT_EQ(gray8u, uint8_max);
 
   smart_convert_color(rgb8u, grayd);  EXPECT_NEAR(grayd, 1, 1e-3);
   smart_convert_color(rgb8u, grayf);  EXPECT_NEAR(grayf, 1, 1e-3);
-  smart_convert_color(rgb8u, gray16); EXPECT_EQ(gray16, INT16_MAX);
-  smart_convert_color(rgb8u, gray8u); EXPECT_EQ(gray8u, UINT8_MAX);
+  smart_convert_color(rgb8u, gray16); EXPECT_EQ(gray16, int16_max);
+  smart_convert_color(rgb8u, gray8u); EXPECT_EQ(gray8u, uint8_max);
 }
 
 
 TEST(TestSmartConvertColor, test_gray_to_rgb)
 {
+  const int16_t int16_max = numeric_limits<int16_t>::max();
+  const uint8_t uint8_max = numeric_limits<uint8_t>::max();
+
   double grayd = 1;
   float grayf = 1;
-  uint8_t gray8u = UINT8_MAX;
-  int16_t gray16 = INT16_MAX;
+  uint8_t gray8u = uint8_max;
+  int16_t gray16 = int16_max;
 
   Pixel<double, Rgb> rgbd;
   Pixel<float, Rgb> rgbf;
@@ -71,8 +78,8 @@ TEST(TestSmartConvertColor, test_gray_to_rgb)
 
   Pixel<double, Rgb> true_rgbd = Vector3d::Ones();
   Pixel<float, Rgb> true_rgbf = Vector3f::Ones();
-  Pixel<int16_t, Rgb> true_rgb16; true_rgb16.fill(INT16_MAX);
-  Pixel<uint8_t, Rgb> true_rgb8u; true_rgb8u.fill(UINT8_MAX);
+  Pixel<int16_t, Rgb> true_rgb16; true_rgb16.fill(int16_max);
+  Pixel<uint8_t, Rgb> true_rgb8u; true_rgb8u.fill(uint8_max);
 
   smart_convert_color(grayd, rgbd);  EXPECT_MATRIX_NEAR(rgbd, true_rgbd, 1e-3);
   smart_convert_color(grayd, rgbf);  EXPECT_MATRIX_NEAR(rgbf, true_rgbf, 1e-3);
@@ -98,10 +105,14 @@ TEST(TestSmartConvertColor, test_gray_to_rgb)
 
 TEST(TestSmartConvertColor, test_rgb_to_yuv)
 {
+  const int16_t int16_max = numeric_limits<int16_t>::max();
+  const int16_t int16_min = numeric_limits<int16_t>::min();
+  const uint8_t uint8_max = numeric_limits<uint8_t>::max();
+
   Pixel<double, Yuv> yuvd(1,0,0);
   Pixel<float, Yuv> yuvf(1,0,0);
-  Pixel<int16_t, Yuv> yuv16(INT16_MAX, INT16_MIN, INT16_MIN);
-  Pixel<uint8_t, Yuv> yuv8u(UINT8_MAX, 0, 0);
+  Pixel<int16_t, Yuv> yuv16(int16_max, int16_min, int16_min);
+  Pixel<uint8_t, Yuv> yuv8u(uint8_max, 0, 0);
 
   Pixel<double, Rgb> rgbd;
   Pixel<float, Rgb> rgbf;
@@ -110,8 +121,8 @@ TEST(TestSmartConvertColor, test_rgb_to_yuv)
 
   Pixel<double, Rgb> true_rgbd(1,1,1);
   Pixel<float, Rgb> true_rgbf(1,1,1);
-  Pixel<int16_t, Rgb> true_rgb16; true_rgb16.fill(INT16_MAX);
-  Pixel<uint8_t, Rgb> true_rgb8u; true_rgb8u.fill(UINT8_MAX);
+  Pixel<int16_t, Rgb> true_rgb16; true_rgb16.fill(int16_max);
+  Pixel<uint8_t, Rgb> true_rgb8u; true_rgb8u.fill(uint8_max);
 
   smart_convert_color(yuvd, rgbd);  EXPECT_MATRIX_NEAR(rgbd, true_rgbd, 1e-3);
   smart_convert_color(yuvd, rgbf);  EXPECT_MATRIX_NEAR(rgbf, true_rgbf, 1e-3);
@@ -137,10 +148,13 @@ TEST(TestSmartConvertColor, test_rgb_to_yuv)
 
 TEST(TestSmartConvertColor, test_gray_to_gray)
 {
+  const int16_t int16_max = numeric_limits<int16_t>::max();
+  const uint8_t uint8_max = numeric_limits<uint8_t>::max();
+
   double grayd = 1;
   float grayf = 1;
-  uint8_t gray8u = UINT8_MAX;
-  int16_t gray16 = INT16_MAX;
+  uint8_t gray8u = uint8_max;
+  int16_t gray16 = int16_max;
 
   double dst_grayd;
   float dst_grayf;
@@ -148,25 +162,25 @@ TEST(TestSmartConvertColor, test_gray_to_gray)
   int16_t dst_gray16;
 
   smart_convert_color(grayd, dst_grayf);  EXPECT_NEAR(dst_grayf, 1, 1e-3);
-  smart_convert_color(grayd, dst_gray16); EXPECT_EQ(dst_gray16, INT16_MAX);
-  smart_convert_color(grayd, dst_gray8u); EXPECT_EQ(dst_gray8u, UINT8_MAX);
+  smart_convert_color(grayd, dst_gray16); EXPECT_EQ(dst_gray16, int16_max);
+  smart_convert_color(grayd, dst_gray8u); EXPECT_EQ(dst_gray8u, uint8_max);
 
   smart_convert_color(grayf, dst_grayd);  EXPECT_NEAR(dst_grayd, 1, 1e-3);
-  smart_convert_color(grayf, dst_gray16); EXPECT_EQ(dst_gray16, INT16_MAX);
-  smart_convert_color(grayf, dst_gray8u); EXPECT_EQ(dst_gray8u, UINT8_MAX);
+  smart_convert_color(grayf, dst_gray16); EXPECT_EQ(dst_gray16, int16_max);
+  smart_convert_color(grayf, dst_gray8u); EXPECT_EQ(dst_gray8u, uint8_max);
 
   smart_convert_color(gray16, dst_grayd);  EXPECT_NEAR(dst_grayd, 1, 1e-3);
   smart_convert_color(gray16, dst_grayf);  EXPECT_NEAR(dst_grayf, 1, 1e-3);
-  smart_convert_color(gray16, dst_gray8u); EXPECT_EQ(dst_gray8u, UINT8_MAX);
+  smart_convert_color(gray16, dst_gray8u); EXPECT_EQ(dst_gray8u, uint8_max);
 
   smart_convert_color(gray8u, dst_grayd);  EXPECT_NEAR(dst_grayd, 1, 1e-3);
   smart_convert_color(gray8u, dst_grayf);  EXPECT_NEAR(dst_grayf, 1, 1e-3);
-  smart_convert_color(gray8u, dst_gray16); EXPECT_EQ(dst_gray16, INT16_MAX);
+  smart_convert_color(gray8u, dst_gray16); EXPECT_EQ(dst_gray16, int16_max);
 }
 
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-  testing::InitGoogleTest(&argc, argv); 
+  testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
