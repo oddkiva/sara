86cc1c8610968ad7fbebddbd4764c1e5aaf49e66
David OK <david.ok8@gmail.com>
2014-10-13 01:22:42 +0100

    Add `EXPECT_ITEMS_EQ` assertion.

--- test/AssertHelpers.hpp
+++ test/AssertHelpers.hpp
@@ -2,14 +2,17 @@
 #define DO_ASSERT_HELPERS_HPP
 
 
+#include <set>
+#include <vector>
+
 #include <Eigen/Core>
 
 #include <gtest/gtest.h>
 
 
 template <typename Matrix1, typename Matrix2>
-::testing::AssertionResult assert_matrix_equal(const char* m_expr,
-                                               const char* n_expr,
+::testing::AssertionResult assert_matrix_equal(const char *m_expr,
+                                               const char *n_expr,
                                                const Matrix1& m,
                                                const Matrix2& n)
 {
@@ -24,9 +27,9 @@ template <typename Matrix1, typename Matrix2>
 
 
 template <typename Matrix1, typename Matrix2>
-::testing::AssertionResult assert_matrix_near(const char* m_expr,
-                                              const char* n_expr,
-                                              const char* abs_error_expr,
+::testing::AssertionResult assert_matrix_near(const char *m_expr,
+                                              const char *n_expr,
+                                              const char *abs_error_expr,
                                               const Matrix1& m,
                                               const Matrix2& n,
                                               double abs_error)
@@ -50,5 +53,17 @@ template <typename Matrix1, typename Matrix2>
   ASSERT_PRED_FORMAT3(assert_matrix_near, m, n, abs_error)
 #define EXPECT_MATRIX_NEAR(m, n, abs_error)\
   EXPECT_PRED_FORMAT3(assert_matrix_near, m, n, abs_error)
+
+
+template <typename T>
+inline std::set<T> to_std_set(const std::vector<T>& v)
+{
+  return std::set<T>(v.begin(), v.end());
+}
+
+// Define a macro that does something 'self.assertItemsEqual' in Python.
+#define EXPECT_ITEMS_EQ(vector1, vector2) \
+  EXPECT_EQ(to_std_set(vector1), to_std_set(vector2))
+
 
 #endif /* DO_ASSERT_HELPERS_HPP */
