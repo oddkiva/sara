df0fb34e27dadc8b2f404b28b273570d007da27e
David OK <david.ok8@gmail.com>
2014-10-13 01:24:35 +0100

    Improve readability of tests on K-nn search.

--- test/KDTree/test_knn_search.cpp
+++ test/KDTree/test_knn_search.cpp
@@ -44,18 +44,18 @@ class TestKDTree : public testing::Test
 protected:
   MatrixXd data;
   size_t num_points;
-  size_t num_points_in_circle;
+  size_t num_points_in_each_circle;
 
   TestKDTree()
   {
     // We construct two sets in points. The first one lives in the 
     // zero-centered unit circle and the second in the zero-centered
     // circle with radius 10.
-    num_points_in_circle = 5;
-    num_points = 2*num_points_in_circle;
+    num_points_in_each_circle = 20;
+    num_points = 2*num_points_in_each_circle;
     data.resize(2, num_points);
 
-    const size_t& N = num_points_in_circle;
+    const size_t& N = num_points_in_each_circle;
     for (size_t i = 0; i < N; ++i)
     {
       double theta = i / (2*N*M_PI);
@@ -76,7 +76,7 @@ TEST_F(TestKDTree, test_simple_knn_search)
   KDTree tree(data);
 
   Vector2d query = Vector2d::Zero();
-  size_t num_nearest_neighbors = num_points_in_circle;
+  size_t num_nearest_neighbors = num_points_in_each_circle;
 
   vector<int> nn_indices;
   vector<double> nn_squared_distances;
@@ -85,10 +85,11 @@ TEST_F(TestKDTree, test_simple_knn_search)
                   nn_squared_distances);
 
   // Check equality of items.
-  EXPECT_ITEMS_EQ(nn_indices, range(num_points_in_circle));
+  EXPECT_ITEMS_EQ(nn_indices, range(num_points_in_each_circle));
 
   // Check the squared distances.
-  for (size_t j = 0; j < nn_indices.size(); ++j)
+  EXPECT_EQ(nn_squared_distances.size(), num_points_in_each_circle);
+  for (size_t j = 0; j < nn_squared_distances.size(); ++j)
     EXPECT_NEAR(nn_squared_distances[j], 1., 1e-10);
 }
 
@@ -98,7 +99,7 @@ TEST_F(TestKDTree, test_simple_knn_search_with_query_point_in_data)
   KDTree tree(data);
 
   size_t query_index = 0;
-  size_t num_nearest_neighbors = num_points_in_circle-1;
+  size_t num_nearest_neighbors = num_points_in_each_circle-1;
 
   vector<int> indices;
   vector<double> squared_distances;
@@ -106,62 +107,83 @@ TEST_F(TestKDTree, test_simple_knn_search_with_query_point_in_data)
   tree.knn_search(query_index, num_nearest_neighbors, indices,
                   squared_distances);
 
-  EXPECT_EQ(indices.size(), num_nearest_neighbors);
-  for (size_t j = 0; j < indices.size(); ++j)
-  {
-    EXPECT_LE(indices[j], num_nearest_neighbors);
+  // Check the indices of the neighbors.
+  EXPECT_ITEMS_EQ(indices, range(1, num_points_in_each_circle));
+
+  // Check the squared distances of the neighbors.
+  EXPECT_EQ(num_nearest_neighbors, squared_distances.size());
+  for (size_t j = 0; j < squared_distances.size(); ++j)
     EXPECT_LE(squared_distances[j], 2.);
-  }
 }
+
 
 TEST_F(TestKDTree, test_batch_knn_search)
 {
+  // Input data.
   KDTree tree(data);
-  const size_t& num_queries = num_points_in_circle;
-  const size_t& num_nearest_neighbors = num_points_in_circle;
+  const size_t& num_queries = num_points_in_each_circle;
+  const size_t& num_nearest_neighbors = num_points_in_each_circle;
   MatrixXd queries(data.leftCols(num_queries));
 
+  // In-out data.
   vector<vector<int> > indices;
   vector<vector<double> > squared_distances;
 
+  // Use case.
   tree.knn_search(queries, num_nearest_neighbors, indices, squared_distances);
 
+  // Check the contents of the retrieval.
   EXPECT_EQ(indices.size(), num_queries);
+  EXPECT_EQ(squared_distances.size(), num_queries);
+
   for (size_t i = 0; i < num_queries; ++i)
   {
-    EXPECT_EQ(indices[i].size(), num_queries);
-    for (size_t j = 0; j < indices[i].size(); ++j)
-    {
-      EXPECT_LE(indices[i][j], num_queries);
+    // Check the indices.
+    EXPECT_ITEMS_EQ(indices[i], range(num_points_in_each_circle));
+
+    // Check the squared distances.
+    EXPECT_EQ(num_nearest_neighbors, squared_distances.size());
+    for (size_t j = 0; j < squared_distances[i].size(); ++j)
       EXPECT_LE(squared_distances[i][j], 2.);
-    }
   }
 }
 
 TEST_F(TestKDTree, test_batch_knn_search_with_query_point_in_data)
 {
+  // Input data.
   KDTree tree(data);
-  const size_t num_queries = num_points_in_circle;
-  const size_t num_nearest_neighbors = num_points_in_circle-1;
+  const size_t num_queries = num_points_in_each_circle;
+  const size_t num_nearest_neighbors = num_points_in_each_circle-1;
 
   vector<size_t> queries(num_queries);
   for (size_t i = 0; i != queries.size(); ++i)
     queries[i] = i;
 
+  // In-out data.
   vector<vector<int> > indices;
   vector<vector<double> > squared_distances;
 
+  // Use case.
   tree.knn_search(queries, num_nearest_neighbors, indices, squared_distances);
 
+  // Check the number of queries.
   EXPECT_EQ(indices.size(), num_queries);
+  EXPECT_EQ(squared_distances.size(), num_queries);
+
+  // Check the contents of the retrieval.
   for (size_t i = 0; i != indices.size(); ++i)
   {
-    EXPECT_EQ(indices[i].size(), num_nearest_neighbors);
+    // The correct list of indices is: {0, 1, 2, 3, 4 } \ {i},
+    // where i = 0, 1, ... 4.
+    vector<int> true_indices = range(num_points_in_each_circle);
+    true_indices.erase(true_indices.begin()+i);
+
+    EXPECT_ITEMS_EQ(indices[i], true_indices);
+
+    // Check the squared distances.
+    EXPECT_EQ(squared_distances[i].size(), true_indices.size());
     for (size_t j = 0; j < indices[i].size(); ++j)
-    {
-      EXPECT_LE(indices[i][j], num_nearest_neighbors);
       EXPECT_LE(squared_distances[i][j], 2.);
-    }
   }
 }
 
