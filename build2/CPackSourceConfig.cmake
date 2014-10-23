f6336c292db90edf43d0d586bca320cd95a08bc4
unknown <david.ok@uk-lon01-nb10.mirriad.corp>
2014-10-23 15:38:08 +0100

    Remove all unnecessary files in `gtest`.

---
+++ build2/CPackSourceConfig.cmake
@@ -0,0 +1,86 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+SET(CPACK_BINARY_BUNDLE "")
+SET(CPACK_BINARY_CYGWIN "")
+SET(CPACK_BINARY_DEB "")
+SET(CPACK_BINARY_DRAGNDROP "")
+SET(CPACK_BINARY_NSIS "")
+SET(CPACK_BINARY_OSXX11 "")
+SET(CPACK_BINARY_PACKAGEMAKER "")
+SET(CPACK_BINARY_RPM "")
+SET(CPACK_BINARY_STGZ "")
+SET(CPACK_BINARY_TBZ2 "")
+SET(CPACK_BINARY_TGZ "")
+SET(CPACK_BINARY_TZ "")
+SET(CPACK_BINARY_WIX "")
+SET(CPACK_BINARY_ZIP "")
+SET(CPACK_CMAKE_GENERATOR "Visual Studio 9 2008 Win64")
+SET(CPACK_COMPONENTS_ALL "Eigen3;Sources;Core;Graphics;Libraries")
+SET(CPACK_COMPONENTS_ALL_SET_BY_USER "TRUE")
+SET(CPACK_COMPONENT_Eigen3_REQUIRED "1")
+SET(CPACK_COMPONENT_Sources_REQUIRED "1")
+SET(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+SET(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+SET(CPACK_GENERATOR "ZIP")
+SET(CPACK_IGNORE_FILES "/CVS/;/\\.svn/;/\\.bzr/;/\\.hg/;/\\.git/;\\.swp$;\\.#;/#")
+SET(CPACK_INSTALLED_DIRECTORIES "C:/Users/david.ok/Desktop/github/DO-CV;/")
+SET(CPACK_INSTALL_CMAKE_PROJECTS "")
+SET(CPACK_INSTALL_PREFIX "C:/Program Files/DO-CV")
+SET(CPACK_MODULE_PATH "C:/Users/david.ok/Desktop/github/DO-CV/cmake")
+SET(CPACK_NSIS_DISPLAY_NAME "DO++-1.0.0")
+SET(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "
+    WriteRegExpandStr HKCU \"Environment\" DOPLUSPLUS \"$INSTDIR\"
+    SendMessage \${HWND_BROADCAST} \${WM_WININICHANGE} 0 \"STR:Environment\" /TIMEOUT=5000
+    ")
+SET(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "
+    DeleteRegValue HKCU \"Environment\" DOPLUSPLUS
+    SendMessage \${HWND_BROADCAST} \${WM_WININICHANGE} 0 \"STR:Environment\" /TIMEOUT=5000
+    ")
+SET(CPACK_NSIS_INSTALLER_ICON_CODE "")
+SET(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+SET(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES")
+SET(CPACK_NSIS_MODIFY_PATH "ON")
+SET(CPACK_NSIS_PACKAGE_NAME "DO++-1.0.0")
+SET(CPACK_OUTPUT_CONFIG_FILE "C:/Users/david.ok/Desktop/github/DO-CV/build2/CPackConfig.cmake")
+SET(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+SET(CPACK_PACKAGE_DESCRIPTION_FILE "C:/Program Files (x86)/CMake 2.8/share/cmake-2.8/Templates/CPack.GenericDescription.txt")
+SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "DO++ - Basic set of libraries for computer vision")
+SET(CPACK_PACKAGE_FILE_NAME "DO++-1.0.0-Source")
+SET(CPACK_PACKAGE_INSTALL_DIRECTORY "DO++-1.0.0")
+SET(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "DO++ 1.0.0")
+SET(CPACK_PACKAGE_NAME "DO++")
+SET(CPACK_PACKAGE_RELOCATABLE "true")
+SET(CPACK_PACKAGE_VENDOR "David Ok")
+SET(CPACK_PACKAGE_VERSION "1.0.0")
+SET(CPACK_PACKAGE_VERSION_MAJOR "1")
+SET(CPACK_PACKAGE_VERSION_MINOR "0")
+SET(CPACK_PACKAGE_VERSION_PATCH "0")
+SET(CPACK_RESOURCE_FILE_LICENSE "C:/Users/david.ok/Desktop/github/DO-CV/COPYING.README")
+SET(CPACK_RESOURCE_FILE_README "C:/Program Files (x86)/CMake 2.8/share/cmake-2.8/Templates/CPack.GenericDescription.txt")
+SET(CPACK_RESOURCE_FILE_WELCOME "C:/Program Files (x86)/CMake 2.8/share/cmake-2.8/Templates/CPack.GenericWelcome.txt")
+SET(CPACK_SET_DESTDIR "OFF")
+SET(CPACK_SOURCE_CYGWIN "")
+SET(CPACK_SOURCE_GENERATOR "ZIP")
+SET(CPACK_SOURCE_IGNORE_FILES "/CVS/;/\\.svn/;/\\.bzr/;/\\.hg/;/\\.git/;\\.swp$;\\.#;/#")
+SET(CPACK_SOURCE_INSTALLED_DIRECTORIES "C:/Users/david.ok/Desktop/github/DO-CV;/")
+SET(CPACK_SOURCE_OUTPUT_CONFIG_FILE "C:/Users/david.ok/Desktop/github/DO-CV/build2/CPackSourceConfig.cmake")
+SET(CPACK_SOURCE_PACKAGE_FILE_NAME "DO++-1.0.0-Source")
+SET(CPACK_SOURCE_TBZ2 "")
+SET(CPACK_SOURCE_TGZ "")
+SET(CPACK_SOURCE_TOPLEVEL_TAG "win32-Source")
+SET(CPACK_SOURCE_TZ "")
+SET(CPACK_SOURCE_ZIP "ON")
+SET(CPACK_STRIP_FILES "")
+SET(CPACK_SYSTEM_NAME "win32")
+SET(CPACK_TOPLEVEL_TAG "win32-Source")
+SET(CPACK_WIX_SIZEOF_VOID_P "")
