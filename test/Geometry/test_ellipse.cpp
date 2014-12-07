5cd9850b10c07d58c58d96717563febf841fbde2
David OK <david.ok8@gmail.com>
2014-12-07 03:19:00 +0000

    Restructure ellipse unit tests.
    They are disabled as they are taking too long to terminate.

--- test/Geometry/test_ellipse.cpp
+++ test/Geometry/test_ellipse.cpp
@@ -1,3 +1,6 @@
+#include <DO/Core.hpp>
+#include <DO/Geometry/Objects/Cone.hpp>
+#include <DO/Geometry/Objects/CSG.hpp>
 #include <DO/Geometry/Objects/Ellipse.hpp>
 #include <DO/Geometry/Objects/Triangle.hpp>
 #include <DO/Geometry/Tools/Utilities.hpp>
@@ -20,7 +23,76 @@ protected:
 };
 
 
-TEST_F(TestEllipse, test_segment_area)
+// TODO: make test faster. TOO SLOW!!!
+TEST_F(TestEllipse, DISABLED_test_sector_area)
+{
+  Ellipse E(250, 150, to_radian(75.), _center);
+
+  CSG::Singleton<Ellipse> ell(E);
+
+  try {
+
+    int steps = 18;
+    for (int i0 = 0; i0 <= steps; ++i0)
+    {
+      double theta0 = i0*2*M_PI/steps;
+      for (int i1 = i0+1; i1 <= i0+steps; ++i1)
+      {
+        double theta1 = i1*2*M_PI/steps;
+
+        // Compute the sector area in a closed form.
+        double analyticSectorArea = sector_area(E, theta0, theta1);
+
+        // Build constructive solid geometry.
+        CSG::Singleton<AffineCone2> cone ( affine_cone2(
+          theta0+E.orientation(), theta1+E.orientation(), E.center()) );
+        auto E_and_Cone = ell*cone;
+        auto E_minus_Cone = ell - E_and_Cone;
+
+        // Use the CSGs to estimate the sector area by pixel counting.
+        auto inside_E_and_Cone = [&](const Point2d& p){ 
+          return E_and_Cone.contains(p);
+        };
+        auto inside_E_minus_Cone = [&](const Point2d& p){ 
+          return E_minus_Cone.contains(p);
+        };
+
+        // Use the lambda functors to estimate the elliptic sector area.
+        int estimated_sector_area;
+        if (i1 - i0 < steps/2)
+          estimated_sector_area = sweep_count_pixels(inside_E_and_Cone);
+        else if (abs(i1-i0) == steps/2)
+          estimated_sector_area = area(E) / 2.;
+        else
+          estimated_sector_area = sweep_count_pixels(inside_E_minus_Cone);
+
+        // Absolute error and relative error.
+        double abs_error = fabs(estimated_sector_area -analyticSectorArea);
+        double rel_error = abs_error/estimated_sector_area;
+
+        double thres = 1e-1;
+        EXPECT_NEAR(rel_error, 0, thres);
+        if (rel_error > thres)
+        {
+          printStage("Numerical error");
+          cout << i0 << "     " << i1 << endl;
+          CHECK(abs(i1-i0));
+          CHECK(analytic_sector_area);
+          CHECK(estimated_sector_area);
+          CHECK(abs_error);
+          CHECK(rel_error);
+        }
+      }
+    }
+  }
+  catch (exception& e)
+  {
+    cout << e.what() << endl;
+  }
+}
+
+// TODO: make test faster. TOO SLOW!!!
+TEST_F(TestEllipse, DISABLED_test_segment_area)
 {
   Ellipse E(270, 150, to_radian(42.), Point2d::Zero());
 
@@ -52,20 +124,17 @@ TEST_F(TestEllipse, test_segment_area)
 
         const double thres = 0.1;
         EXPECT_NEAR(relError, 0, thres);
-#if 0
         if (relError > thres)
         {
           printStage("Numerical error");
           CHECK(i0);
           CHECK(i1);
-          CHECK(toDegree(theta0));
-          CHECK(toDegree(theta1));
+          CHECK(to_degree(theta0));
+          CHECK(to_degree(theta1));
           CHECK(segArea1);
           CHECK(segArea2);
           CHECK(relError);
-          getKey();
         }
-#endif
       }
     }
   }
