a18571aa3770fe7e578f30fe2b1ffed4a28e647b
David OK <david.ok8@gmail.com>
2015-05-20 16:22:45 +0100

    DOC: improve the documentation.

--- src/DO/Geometry/Objects/BBox.hpp
+++ src/DO/Geometry/Objects/BBox.hpp
@@ -27,7 +27,7 @@ namespace DO {
     //! Default constructor.
     BBox() = default;
 
-    //! Constructor from the BBox end points.
+    //! \brief Constructor from the BBox end points.
     BBox(const Point2d& top_left, const Point2d& bottom_right)
       : _top_left(top_left), _bottom_right(bottom_right)
     {
@@ -39,7 +39,8 @@ namespace DO {
       }
     }
 
-    //! Constructor from a point set.
+    //! @{
+    //! \brief Constructor from a point set.
     BBox(const Point2d *begin, const Point2d *end)
     {
       if (!begin)
@@ -57,35 +58,48 @@ namespace DO {
       }
     }
 
-    //! Constructor from a point set.
     BBox(const std::vector<Point2d>& points)
       : BBox(&points.front(), &points.front() + points.size())
     {
     }
+    //! @}
 
-    Point2d& top_left()     { return _top_left; }
+    //! @{
+    //! \brief Return BBox vertex.
+    Point2d& top_left() { return _top_left; }
     Point2d& bottom_right() { return _bottom_right; }
+
+    const Point2d& top_left() const { return _top_left; }
+    const Point2d& bottom_right() const { return _bottom_right; }
+    Point2d top_right() const { return _top_left + Point2d(width(), 0); }
+    Point2d bottom_left() const { return _bottom_right - Point2d(width(), 0); }
+    //! @}
+
+    //! @{
+    //! \brief Return BBox coordinates.
     double& x1() { return  _top_left.x(); }
     double& y1() { return  _top_left.y(); }
     double& x2() { return  _bottom_right.x(); }
     double& y2() { return  _bottom_right.y(); }
 
-    const Point2d& top_left()     const { return _top_left; }
-    const Point2d& bottom_right() const { return _bottom_right; }
-    Point2d        top_right()    const { return _top_left+Point2d(width(), 0); }
-    Point2d        bottom_left()  const { return _bottom_right-Point2d(width(), 0); }
-
     double x1() const { return  _top_left.x(); }
     double y1() const { return  _top_left.y(); }
     double x2() const { return  _bottom_right.x(); }
     double y2() const { return  _bottom_right.y(); }
+    //! @}
 
+    //! @{
+    //! \brief Return BBox sizes.
     double width() const  { return std::abs(_bottom_right.x() - _top_left.x()); }
     double height() const { return std::abs(_bottom_right.y() - _top_left.y()); }
     Vector2d sizes() const { return _bottom_right - _top_left; }
+    //! @}
 
+    //! \brief Return BBox center.
     Point2d center() const { return 0.5*(_top_left + _bottom_right); }
 
+    //! @{
+    //! \brief Convenience functions.
     static BBox infinite()
     {
       BBox b;
@@ -99,25 +113,30 @@ namespace DO {
       BBox b(Point2d::Zero(), Point2d::Zero());
       return b;
     }
+    //! @}
 
   private:
+    //! @{
     Point2d _top_left;
     Point2d _bottom_right;
+    //! @}
 
   };
 
-  // Utility functions.
+  //! @{
+  //! Utility functions.
   double area(const BBox& bbox);
   bool inside(const Point2d& p, const BBox& bbox);
   bool degenerate(const BBox& bbox, double eps = 1e-3);
   bool intersect(const BBox& bbox1, const BBox& bbox2);
   double jaccard_similarity(const BBox& bbox1, const BBox& bbox2);
   double jaccard_distance(const BBox& bbox1, const BBox& bbox2);
+  //! @}
 
   //! I/O.
   std::ostream& operator<<(std::ostream& os, const BBox& bbox);
 
-  //! Intersection test.
+  //! Return the intersection of two BBoxes.
   BBox intersection(const BBox& bbox1, const BBox& bbox2);
 
 
