ad705180e3297fbcb9c8f31d43a5e0f627f9dca8
David OK <david.ok8@gmail.com>
2015-06-09 10:18:26 +0100

    MAINT: clean packaging information.

--- cmake/DOInstall.cmake
+++ cmake/DOInstall.cmake
@@ -5,17 +5,16 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
 # Eigen 3
 do_message("Installing Eigen")
-install(DIRECTORY ${Eigen3_DIR}/Eigen
+install(DIRECTORY ${DO_ThirdParty_DIR}/eigen
         DESTINATION include
         COMPONENT Eigen3)
 set(CPACK_COMPONENT_Eigen3_REQUIRED 1)
 
 # DO++ source files
 install(FILES ${DO_DIR}/COPYING.README
-              ${DO_DIR}/COPYING.MPL2 
+              ${DO_DIR}/COPYING.MPL2
               ${DO_DIR}/CPACK_PACKAGING.README
               ${DO_DIR}/CMakeLists.txt
-              ${DO_DIR}/Doxyfile.in
         DESTINATION .
         COMPONENT Sources)
 install(DIRECTORY ${DO_DIR}/cmake
@@ -48,17 +47,6 @@ foreach (component ${DO_COMPONENTS})
             DESTINATION include/DO
             COMPONENT ${component}
             FILES_MATCHING PATTERN "*.hpp")
-    # Install dynamic libraries
-    # if (NOT ${DO_${component}_LIBRARIES} STREQUAL "") # This is how I check if the library is not header-only...
-        # install(TARGETS DO_${component}
-                # ARCHIVE DESTINATION lib
-                # COMPONENT ${component})
-        # install(TARGETS DO_${component}_SHARED
-                # LIBRARY DESTINATION bin
-                # ARCHIVE DESTINATION bin
-                # RUNTIME DESTINATION bin
-                # COMPONENT ${component})
-    # endif ()
 endforeach (component)
 
 
@@ -80,18 +68,22 @@ endif ()
 
 # List all available components for installation.
 set(CPACK_COMPONENTS_ALL Eigen3 Sources Core Graphics Libraries)
-# Create windows package with NSIS.
-set(CPACK_GENERATOR NSIS)
-set(CPACK_PACKAGE_NAME "DO++")
-set(CPACK_PACKAGE_VENDOR "David Ok")
+
+set(CPACK_PACKAGE_NAME "DO_Sara")
+set(CPACK_PACKAGE_VENDOR "DO-CV")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY
-    "DO++ - Basic set of libraries for computer vision")
-# License
+  "DO-CV - An easy-to-use C++ set of libraries for computer vision")
 set(CPACK_RESOURCE_FILE_LICENSE "${DO_DIR}/COPYING.README")
-    
-# Put version.
+set(CPACK_PACKAGE_CONTACT "David OK")
+
 set(CPACK_PACKAGE_VERSION_MAJOR "${DO_VERSION_MAJOR}")
 set(CPACK_PACKAGE_VERSION_MINOR "${DO_VERSION_MINOR}")
 set(CPACK_PACKAGE_VERSION_PATCH "${DO_VERSION_PATCH}")
 set(CPACK_PACKAGE_VERSION "${DO_VERSION}")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "${CPACK_PACKAGE_NAME}-${DO_VERSION}")
+
+if (WIN32)
+  set(CPACK_GENERATOR NSIS)
+elseif (UNIX)
+  set(CPACK_GENERATOR "DEB")
+endif()
