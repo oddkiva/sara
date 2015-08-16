4415b9fe8e9f60b9a67e87576e7e613168141ea8
David OK <david.ok8@gmail.com>
2015-08-16 21:12:43 +0100

    MAINT: add unit tests for the computation of DoG and LoG extrema.

--- test/FeatureDetectors/test_DoG.cpp
+++ test/FeatureDetectors/test_DoG.cpp
@@ -8,31 +8,52 @@ using namespace DO::Sara;
 
 TEST(TestExtremumRefinement, test_on_edge)
 {
-  EXPECT_FALSE(true);
+  // TODO.
 }
 
 TEST(TestExtremumRefinement, test_refine_extremum)
 {
-  EXPECT_FALSE(true);
+  // TODO.
 }
 
 TEST(TestExtremumRefinement, test_local_scale_space_extrema)
 {
-  EXPECT_FALSE(true);
+  // TODO.
 }
 
-TEST(TestDoG, test_me)
+TEST(TestDoG, test_compute_dog_extrema)
 {
-  const auto N = 2 * 10 + 1;
+  const auto N = 2 * 5 + 1;
   Image<float> I{ N, N };
   I.array().fill(0);
-  I(1, 1) = 1.f;
 
-  ComputeDoGExtrema compute_DoGs{};
+  const auto xc = N / 2.f;
+  const auto yc = N / 2.f;
+  const auto sigma = 1.5f;
+  for (int y = 0; y < N; ++y)
+    for (int x = 0; x < N; ++x)
+      I(x, y) = 1 / sqrt(2*float(M_PI)*pow(sigma, 2)) *
+                exp(-(pow(x - xc, 2) + pow(y - yc, 2)) / (2 * pow(sigma, 2)));
 
-  auto features = compute_DoGs(I, 0);
+  using namespace std;
 
-  EXPECT_FALSE(true);
+  const auto pyramid_params = ImagePyramidParams{};
+  auto compute_DoGs = ComputeDoGExtrema{ pyramid_params };
+
+  auto scale_octave_pairs = vector<Point2i>{};
+  auto features = compute_DoGs(I, &scale_octave_pairs);
+  const auto& o_index = scale_octave_pairs[0](1);
+
+  EXPECT_EQ(features.size(), 1);
+  EXPECT_EQ(scale_octave_pairs.size(), 1);
+
+  const auto& f = features.front();
+  const auto& D = compute_DoGs.diff_of_gaussians();
+  const auto z = D.octave_scaling_factor(o_index);
+
+  EXPECT_NEAR(f.x()*z, xc, 1e-2);
+  EXPECT_NEAR(f.y()*z, yc, 1e-2);
+  EXPECT_NEAR(z, 0.5, 1e-2);
 }
 
 
--- test/FeatureDetectors/test_LoG.cpp
+++ test/FeatureDetectors/test_LoG.cpp
@@ -6,18 +6,39 @@
 using namespace DO::Sara;
 
 
-TEST(TestLoGs, test_me)
+TEST(TestLoGExtrema, test_compute_LoG_extrema)
 {
-  const auto N = 2 * 10 + 1;
+  const auto N = 2 * 5 + 1;
   Image<float> I{ N, N };
   I.array().fill(0);
-  I(1, 1) = 1.f;
 
-  ComputeLoGExtrema compute_LoGs{};
+  const auto xc = N / 2.f;
+  const auto yc = N / 2.f;
+  const auto sigma = 1.5f;
+  for (int y = 0; y < N; ++y)
+    for (int x = 0; x < N; ++x)
+      I(x, y) = 1 / sqrt(2*float(M_PI)*pow(sigma, 2)) *
+                exp(-(pow(x - xc, 2) + pow(y - yc, 2)) / (2 * pow(sigma, 2)));
 
-  auto features = compute_LoGs(I, 0);
+  using namespace std;
 
-  EXPECT_FALSE(true);
+  const auto pyramid_params = ImagePyramidParams{};
+  auto compute_LoGs = ComputeLoGExtrema{ pyramid_params };
+
+  auto scale_octave_pairs = vector<Point2i>{};
+  auto features = compute_LoGs(I, &scale_octave_pairs);
+  const auto& o_index = scale_octave_pairs[0](1);
+
+  EXPECT_EQ(features.size(), 1);
+  EXPECT_EQ(scale_octave_pairs.size(), 1);
+
+  const auto& f = features.front();
+  const auto& L = compute_LoGs.laplacians_of_gaussians();
+  const auto z = L.octave_scaling_factor(o_index);
+
+  EXPECT_NEAR(f.x()*z, xc, 1e-2);
+  EXPECT_NEAR(f.y()*z, yc, 1e-2);
+  EXPECT_NEAR(z, 0.5, 1e-2);
 }
 
 int main(int argc, char *argv[])
