d98ce53c5bdbc05f632b851dea3936efba869f87
David OK <david.ok8@gmail.com>
2015-08-28 01:41:04 +0100

    MAINT: clean implementation of `Frame` class.

--- src/DO/Sara/Graphics/Frame.hpp
+++ src/DO/Sara/Graphics/Frame.hpp
@@ -34,12 +34,16 @@ namespace GLObject {
   class Frame
   {
   public:
+    //! \brief Default constructor.
     Frame();
+
+    //! \brief Destructor.
     ~Frame();
+
+    //! \brief Draw frame in the window.
     void draw(double axisLength, double axisRadius);
-    static void generate_gl_list(GLuint *frameList,
-                             double axisLength = defaultAxisLength,
-                             double axisRadius = defaultAxisRadius);
+              double axisRadius = defaultAxisRadius);
+
   private:
     GLUquadricObj * pQuadObj;
   };
