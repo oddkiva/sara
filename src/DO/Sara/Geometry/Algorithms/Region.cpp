895e0784efc1d8a5e28e21cfed2003b12c4df77c
David OK <david.ok8@gmail.com>
2015-10-05 16:12:19 +0100

    ENH: add region inner boundary algorithm.

---
+++ src/DO/Sara/Geometry/Algorithms/Region.cpp
@@ -0,0 +1,110 @@
+// ========================================================================== //
+// This file is part of DO-CV, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2015 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+#include <DO/Sara/Geometry/Algorithms.hpp>
+
+
+using namespace std;
+
+
+namespace DO { namespace Sara {
+
+  vector<Point2i>>
+  compute_superpixels_inner_boundary(const Image<int>& regions, int region_id)
+  {
+#ifndef CONNECTIVITY_4
+    const Vector2i dirs[] = {
+      Vector2i(1, 0),
+      Vector2i(1, 1),
+      Vector2i(0, 1),
+      Vector2i(-1, 1),
+      Vector2i(-1, 0),
+      Vector2i(-1, -1),
+      Vector2i(0, -1),
+      Vector2i(1, -1)
+    };
+#else
+    const Vector2i dirs[] = {
+      Vector2i(1, 0),
+      Vector2i(0, 1),
+      Vector2i(-1, 0),
+      Vector2i(0, -1),
+    };
+#endif
+
+    const int num_dirs{ sizeof(dirs) / sizeof(Vector2i) };
+
+    // Find the starting point.
+    auto start_point = Point2i{ -1, -1 };
+    for (int y = 0; y < labels.height(); ++y)
+    {
+      for (int x = 0; x < labels.width(); ++x)
+      {
+        if (labels(x, y) == int(label))
+        {
+          start_point = Point2i(x, y);
+          break;
+        }
+      }
+    }
+    if (start_point == Point2i{ -1, -1 })
+      return {};
+
+    auto boundary = vector<Point2i>{ start_point };
+
+#ifndef CONNECTIVITY_4
+    int dir = 7;
+#else
+    int dir = 0;
+#endif
+    do {
+      const Point2i& current_point{ boundary.back() };
+
+#ifndef CONNECTIVITY_4
+      dir = (dir % 2) == 0 ? (dir + 7) % 8 : (dir + 6) % 8;
+#else
+      dir = (dir + 3) % 4;
+#endif
+      for (int d = 0; d < num_dirs; ++d)
+      {
+        Point2i next_point{ current_point + dirs[(dir + d) % num_dirs] };
+
+        if (next_point.minCoeff() < 0 ||
+          (labels.sizes() - next_point).minCoeff() <= 0)
+          continue;
+
+        if (labels(next_point) == int(label))
+        {
+          region_boundaries[label].push_back(next_point);
+          dir = (dir + d) % num_dirs;
+          break;
+        }
+      }
+    } while (boundary.back() != start_point);
+    boundary.pop_back();
+
+    return boundary;
+  }
+
+  vector<vector<Point2i>>
+  compute_region_inner_boundaries(const Image<int>& labels)
+  {
+    const auto num_regions = labels.array().maxCoeff() + 1;
+    auto region_boundaries = vector<vector<Point2i>>(num_regions);
+
+    for (unsigned label = 0; label < num_regions; ++label)
+      region_boundaries[label] = compute_region_inner_boundary(labels, label);
+
+    return region_boundaries;
+  }
+
+} /* namespace Sara */
+} /* namespace DO */
