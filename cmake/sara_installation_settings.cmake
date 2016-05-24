58115594deb57eced93d4ea79811f1061ea90f6f
David OK <david.ok@mirriad.com>
2016-05-24 06:15:20 -0400

    MAINT: copy the files in the 'cmake' directory and not the directory!
    This tiny difference (additional '/') makes the whole thing.

--- cmake/sara_installation_settings.cmake
+++ cmake/sara_installation_settings.cmake
@@ -18,7 +18,7 @@ if (NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
                 ${DO_Sara_DIR}/COPYING.MPL2
           DESTINATION ${SARA_INSTALL_DIR}/include/DO/Sara
           COMPONENT Sources)
-  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/cmake
+  install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/cmake/
           DESTINATION ${SARA_INSTALL_DIR}/share/cmake/Modules
           COMPONENT Sources)
   install(DIRECTORY ${DO_Sara_DIR}/src/DO
