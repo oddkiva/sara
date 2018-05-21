5fe57920e8991324a3164445eaef147cb66be281
David OK <david.ok8@gmail.com>
2018-05-21 11:25:21 +0100

    WIP: implementation in progress.

--- cpp/src/DO/Sara/ImageProcessing/GemmBasedConvolution.hpp
+++ cpp/src/DO/Sara/ImageProcessing/GemmBasedConvolution.hpp
@@ -1,93 +1,96 @@
 #pragma once
 
 #include <DO/Sara/Core/EigenExtension.hpp>
+#include <DO/Sara/Core/Tensor.hpp>
 #include <DO/Sara/Core/Image/Operations.hpp>
 
 
 namespace DO { namespace Sara {
 
-  namespace runtime {
+  // In this world everything, everything is **ROW-MAJOR**.
+  template <typename T, int N>
+  using TensorView_ = TensorView<T, N, RowMajor>;
 
-    template <typename T>
-    class TensorView
-    {
-    public:
-      DynamicTensorView() = default;
+  template <typename T, int N>
+  using Tensor_ = Tensor<T, N, RowMajor>;
 
-      DynamicTensorView& reshape(const std::vector<std::size_t>& shape);
-
-    public:
-      T* data{nullptr};
-      std::vector<std::size_t> shape;
-      std::vector<std::size_t> strides;
-    };
-
-    template <typename T>
-    class Tensor
-    {
-    public:
-      std::unique_ptr<T[]> data;
-      std::vector<std::size_t> shape;
-      std::vector<std::size_t> strides;
-    };
-
-  } /* namespace runtime */
-
-
-  template <typename T>
-  auto patch(const ImageView<T, 3>& in, const Vector3i& beg,
-             const Vector3i& end) -> Image<T, 3>
+  template <typename T, int N>
+  auto image_view(TensorView_<T, N> in) -> ImageView<T, N>
   {
-    return safe_crop(in, beg, end);
+    auto out_sizes = in.sizes();
+    std::reverse(out_sizes.data(), out_sizes.data() + N);
+    return ImageView<T, N>{in.data(), out_sizes};
   }
 
   template <typename T, int N>
-  auto reshape_2d(ImageView<T, N>& in, const Vector2i& shape)
-      -> ImageView<T, 2>
+  auto patch(const TensorView_<T, N>& in, const Matrix<int, N, 1>& beg,
+             const Matrix<int, N, 1>& end) -> Image<T, N>
+  {
+    return safe_crop(image_view(in), beg, end);
+  }
+
+  template <typename T, int N>
+  auto vec(const TensorView_<T, N>& in)
+      -> Map<const Matrix<typename ElementTraits<T>::value_type,    
+             Dynamic, 1>>
+  {
+    return {
+      reinterpret_cast<typename ElementTraits<T>::pointer>(in.data()),
+      static_cast<int64_t>(in.size())
+    };
+  }
+
+  template <typename T, int N>
+  auto vec(TensorView_<T, N>& in)
+      -> Map<const Matrix<typename ElementTraits<T>::value_type,    
+             Dynamic, 1>>
+  {
+    return {
+      reinterpret_cast<typename ElementTraits<T>::pointer>(in.data()),
+      static_cast<int64_t>(in.size())
+    };
+  }
+
+  template <typename T, int N>
+  auto reshape_2d(TensorView_<T, N>& in, const Vector2i& shape)
+      -> TensorView_<T, 2>
   {
     return {in.data(), shape};
   }
 
   template <typename T, int N>
-  auto im2col(const ImageView<T, N>& in, const Matrix<T, N, 1>& kernel_sizes)
-      -> Matrix<T, Dynamic, Dynamic>
+  auto im2col(const TensorView_<T, N>& x, const Matrix<int, N, 1>& kernel_sizes)
+      -> Tensor_<T, 2>
   {
     const auto num_rows = std::accumulate(
-        in.sizes().data(), in.sizes().data() + in.sizes().size(), 1);
+        x.sizes().data(),
+        x.sizes().data() + x.sizes().size(), 1,
+        std::multiplies<int>());
     const auto num_cols = std::accumulate(
-        kernel_sizes.data(), kernel_sizes.data() + kernel_sizes.size(), 1);
+        kernel_sizes.data(),
+        kernel_sizes.data() + kernel_sizes.size(), 1,
+        std::multiplies<int>());
 
-    auto phi_x = Matrix<T, Dynamic, Dynamic>{num_rows, num_cols};
+    auto phi_x = Tensor_<T, 2>{num_rows, num_cols};
 
     const Matrix<T, N, 1> radius = kernel_sizes / 2;
-    for (auto c = in.begin_array(); !c; ++c)
+    for (auto c = x.begin_array(); !c.end(); ++c)
     {
       auto s = c.coords() - radius;
       auto e = c.coords() + radius;
 
-      auto p = patch(in, s, e);
-      phi_x.col(c) = flatten(patch(in, s, e));
+      phi_x.matrix().row(c) = vec(patch(x, s, e)).transpose();
     }
 
     return phi_x;
   }
 
   template <typename T, int N>
-  auto im2col(const ImageView<T, N + 1>& in,
-              const Matrix<T, N, 1>& kernel_sizes)
-      -> Matrix<T, Dynamic, Dynamic>
-  {
-    Matrix<int, N, 1> sizes;
-    sizes << in.size(0) * in.size(1), in.sizes().tail(in.sizes().size() - 2);
-    return im2col(reshape(in, sizes), kernel_sizes);
-  }
-
-  template <typename T>
-  void gemm_convolve(ImageView<T, 4>& kx,  //
-                     const ImageView<T, 3>& x, const ImageView<T, 3>& k)
+  void gemm_convolve(TensorView_<T, N>& kx,  //
+                     const TensorView_<T, N>& x, const TensorView_<T, N>& k)
   {
     auto phi_x = im2col(x, k.sizes());
-    kx.array() = reshape(phi_x.matrix() * vec(k), x.sizes());
+    kx.array() = reshape(phi_x.matrix() * k.vector(), x.sizes());
   }
 
   template <typename T>
