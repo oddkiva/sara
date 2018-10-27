6eff2b1130018e4206059d016fda11022e9c7739
David OK <david.ok8@gmail.com>
2018-10-27 21:36:17 +0100

    MAINT: pursue build automation script with PowerShell.

--- build.ps1
+++ build.ps1
@@ -3,24 +3,27 @@ $conan_vsver = @{2015="14"; 2017="15"};
 
 $vsver = 2015
 
-$build_dir = "sara-build-vs$vsver"
+$build_dirname = "sara-build-vs$vsver"
 
 $cmake_toolset = $cmake_vsver[$vsver]
 $conan_compiler_version = $conan_vsver[$vsver]
 
-iex "New-Item -ItemType directory -Path ..\build_dir"
-iex "cd ..\$build_dir"
-{
-  iex "conan install ..\sara -s compiler.version=$toolset"
+iex "New-Item -ItemType directory -Path ..\$build_dirname"
 
-  $cmake_options  = "-DSARA_BUILD_VIDEOIO:BOOL=ON "
-  $cmake_options += "-DSARA_BUILD_SHARED_LIBS:BOOL=ON "
-  $cmake_options += "-DSARA_BUILD_SAMPLES:BOOL=ON "
-  $cmake_options += "-DSARA_BUILD_TESTS:BOOL=ON"
+# Install dependencies with conan.
+iex "pushd '..\$build_dirname'"
+iex "conan install '..\sara' -s compiler.version='$conan_compiler_version'"
+iex "popd"
 
-  $cmake_command  = "cmake ..\sara -G'Visual Studio 15 2017 Win64' "
-  $cmake_command += "-T $cmake_toolset "
-  $cmake_command += "$cmake_options"
-  iex "$cmake_command"
-}
-iex "cd ..\sara"
+# Configure the build solution with CMake.
+$cmake_options  = "-DSARA_USE_CONAN:BOOL=ON "
+$cmake_options += "-DSARA_BUILD_VIDEOIO:BOOL=ON "
+$cmake_options += "-DSARA_BUILD_SHARED_LIBS:BOOL=ON "
+$cmake_options += "-DSARA_BUILD_SAMPLES:BOOL=ON "
+$cmake_options += "-DSARA_BUILD_TESTS:BOOL=ON"
+
+# Invoke CMake command.
+$cmake_command  = "cmake -H'.' -B'..\$build_dirname' -G'Visual Studio 15 2017 Win64' "
+$cmake_command += "-T '$cmake_toolset' "
+$cmake_command += "'$cmake_options'"
+iex "$cmake_command"
