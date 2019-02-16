e14f3ef15a8cb5b02e81f0cf34514df9e3f32938
David OK <david.ok@mirriad.com>
2019-02-16 00:01:52 +0000

    WIP: save work.

--- cpp/src/DO/Sara/Core/Math/JenkinsTraub.cpp
+++ cpp/src/DO/Sara/Core/Math/JenkinsTraub.cpp
@@ -1,5 +1,6 @@
-#include <DO/Sara/Core/Math/UnivariatePolynomial.hpp>
+#include <DO/Sara/Core/EigenExtension.hpp>
 #include <DO/Sara/Core/Math/NewtonRaphson.hpp>
+#include <DO/Sara/Core/Math/UnivariatePolynomial.hpp>
 
 #include <complex>
 #include <ctime>
@@ -23,84 +24,117 @@ namespace DO { namespace Sara {
     return x;
   }
 
+  // Sigma is a real polynomial. So (s1, s2) is a pair of identical real numbers
+  // or a conjugate complex pair.
+  auto sigma_generic_formula(const std::complex<double>& s1)
+      -> UnivariatePolynomial<double>
+  {
+    const auto a = std::real(s1);
+    const auto b = std::imag(s1);
+    return Z.pow<double>(2) - 2 * a * Z + (a * a + b * b);
+  }
+
+  // See formula (2.2) at page 547.
+  // Don't use because overflow and underflow problems would occur (page 563).
+  auto K1_generic_recurrence_formula(const UnivariatePolynomial<double>& K0,
+                                     const UnivariatePolynomial<double>& P,
+                                     const UnivariatePolynomial<double>& sigma,
+                                     const std::complex<double>& s1)
+      -> UnivariatePolynomial<double>
+  {
+    const auto s2 = std::conj(s1);
+
+    Matrix2cd a, b, c;
+    a <<  P(s1),  P(s2),  //
+         K0(s1), K0(s2);
+
+    b <<     K0(s1),     K0(s2),  //
+         s1 * P(s1), s2 * P(s2);
+
+    c << s1 * P(s1), s2 * P(s2),  //
+              P(s1),      P(s2);
+
+    const auto m = std::real(a.determinant() / c.determinant());
+    const auto n = std::real(b.determinant() / c.determinant());
+
+    return ((K0 + (m * Z + n)*P) / sigma).first;
+  }
+
+  // See formula (2.7) at page 548.
+  auto
+  sigma_formula_from_shift_polynomials(const UnivariatePolynomial<double>& K0,
+                                       const UnivariatePolynomial<double>& K1,
+                                       const UnivariatePolynomial<double>& K2,
+                                       const std::complex<double>& s1)
+      -> UnivariatePolynomial<double>
+  {
+    const auto s2 = std::conj(s1);
+    const auto a2 = std::real(K1(s1) * K2(s2) - K1(s2) * K2(s1));
+    const auto a1 = std::real(K0(s2) * K2(s1) - K0(s1) * K2(s2));
+    const auto a0 = std::real(K0(s1) * K1(s2) - K0(s2) * K1(s1));
+
+    // return (a2 * Z.pow(2) + a1 * Z + a0) / a2;
+    return Z.pow<double>(2) + (a1 / a2) * Z + (a0 / a2);
+  }
+
+  // See formula at "Stage 1: no-shift process" at page 556.
   auto K1_no_shift_polynomial(const UnivariatePolynomial<double>& K0,
                               const UnivariatePolynomial<double>& P)
       -> UnivariatePolynomial<double>
   {
-    // See stage 1 formula: no-shift process (page 556).
     auto K1 = (K0 - (K0(0) / P(0)) * P) / Z;
     return K1.first;
   }
 
-  auto K1_fixed_shift_polynomial(const UnivariatePolynomial<double>& K0,
-                                 const UnivariatePolynomial<double>& P,
-                                 const UnivariatePolynomial<double>& sigma,
-                                 const std::complex<double>& s1,
-                                 const std::complex<double>& s2)
+
+  // This is the scaled recurrence formula (page 563).
+  auto K0_polynomial(const UnivariatePolynomial<double>& P)
       -> UnivariatePolynomial<double>
   {
-    return K0[Z] + P[Z]
+    return derivative(P) / P.degree();
   }
 
-  auto K0_(const UnivariatePolynomial<double>& P)
-      -> UnivariatePolynomial<double>
+  // Return linear polynomial remainder of division of:
+  // - P  / sigma
+  // - K0 / sigma
+  //
+  // Used for stage 2 (fixed-shift process).
+  // TODO: For stage 2, P(s1) and P(s2) are evaluated only once.
+  //
+  // Used for stage 3 (variable-shift process).
+  // TODO: s1 and s2 are updated every time.
+  auto calculate_remainders(const UnivariatePolynomial<double>& P,
+                            const UnivariatePolynomial<double>& K0,
+                            const UnivariatePolynomial<double>& sigma,
+                            const std::complex<double>& s1)
+      -> std::pair<UnivariatePolynomial<double>, UnivariatePolynomial<double>>
   {
-    // Write the scaled recurrence formula (page 563).
-    const auto n = P.degree();
-    auto K0 = UnivariatePolynomial<double>{n - 1};
+    const auto s2 = std::conj(s1);
 
-    for (int i = 0; i < n; ++i)
-      K0[n - 1 - i] = ((n - i) * P[n - i]) / n;
+    // See stage 2 formula (9.7) (page 563).
+    Matrix4cd M;
+    Vector4cd y;
 
-    return K0;
+    M << 1, -s2, 0,   0,
+         0,   0, 1,  -1,
+         1, -s1, 0,   0,
+         0,   0, 1, -s1;
+
+    y << P(s1), P(s2), K0(s1), K0(s2);
+    Vector4cd x = M.colPivHouseholderQr().solve(y);
+
+    const auto a = std::real(x[0]);
+    const auto b = std::real(x[1]);
+    const auto c = std::real(x[2]);
+    const auto d = std::real(x[3]);
+
+    const auto u = - std::real(s1 + s2);
+
+    const auto P_remainder = b * (Z + u) + a;
+    const auto K0_remainder = d * (Z + u) + c;
+
+    return {P_remainder, K0_remainder};
   }
-
-  auto K1_stage1(const UnivariatePolynomial<double>& K0,
-                 const UnivariatePolynomial<double>& P)
-      -> UnivariatePolynomial<double>
-  {
-    // See stage 1 formula: no-shift process (page 556).
-    auto K1 = (K0 - (K0(0) / P(0)) * P) / Z;
-    return K1.first;
-  }
-
-  auto K1_stage2(const UnivariatePolynomial<double>& K0,
-                 const UnivariatePolynomial<double>& P,
-                 const UnivariatePolynomial<double>& sigma,
-                 const std::complex<double>& s1, const std::complex<double>& s2)
-    -> UnivariatePolynomial<double>
-  {
-    return {};
-  }
-
-  //auto K1_stage2(const UnivariatePolynomial<double>& K0,
-  //               const UnivariatePolynomial<double>& P,
-  //               const UnivariatePolynomial<double>& sigma,
-  //               const std::complex<double>& s1, const std::complex<double>& s2)
-  //  -> std::array<double, 6>
-  //{
-  //  // See stage 2 formula (9.7) (page 563).
-  //  Matrix4cd M;
-  //  Vector4cd y;
-
-  //  M << 1, -s2, 0,   0,
-  //       0,   0, 1,  -1,
-  //       1, -s1, 0,   0,
-  //       0,   0, 1, -s1;
-
-  //  y << P(s1), P(s2), K0(s1), K0(s2);
-  //  Vector4cd x = M.colPivHouseholderQr().solve(y);
-
-  //  const auto a = std::real(x[0]);
-  //  const auto b = std::real(x[1]);
-  //  const auto c = std::real(x[2]);
-  //  const auto d = std::real(x[3]);
-
-  //  const auto u = - std::real(s1 + s2);
-  //  const auto v = - std::real(s1 * s2);
-
-  //  return {a, b, c, d, u, v};
-  //}
 
 
   //auto stage2(const UnivariatePolynomial<double>& K0_,
