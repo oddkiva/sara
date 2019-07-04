21e2f9a7f5058d653b94dd1d8ff1a8d6727d3399
David OK <david.ok8@gmail.com>
2019-07-03 21:52:02 -0400

    MAINT: remove gcc flags since Eigen is updated.

--- cmake/sara_configure_cxx_compiler.cmake
+++ cmake/sara_configure_cxx_compiler.cmake
@@ -62,12 +62,6 @@ elseif (CMAKE_COMPILER_IS_GNUCXX)
     sara_substep_message("Enable colored output of GCC.")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fdiagnostics-color=always")
   endif ()
-
-  # Silence some annoying compiler warnings in Eigen since GCC 7.0.
-  if (GCC_VERSION VERSION_GREATER 7.0 OR
-      GCC_VERSION VERSION_EQUAL 7.0)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-int-in-bool-context")
-  endif ()
 endif ()
 
 if (UNIX)
