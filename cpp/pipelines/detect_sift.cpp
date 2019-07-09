692c913c7c343665aa8010eb410d5117869bf404
David OK <david.ok8@gmail.com>
2019-07-08 20:12:14 -0400

    ENH: add program options.

--- cpp/pipelines/detect_sift.cpp
+++ cpp/pipelines/detect_sift.cpp
@@ -9,6 +9,7 @@
 // you can obtain one at http://mozilla.org/MPL/2.0/.
 // ========================================================================== //
 
+#include <DO/Sara/Core/StdVectorHelpers.hpp>
 #include <DO/Sara/Core/DebugUtilities.hpp>
 #include <DO/Sara/Core/HDF5.hpp>
 #include <DO/Sara/FileSystem.hpp>
@@ -17,27 +18,22 @@
 #include <DO/Sara/SfM/Detectors/SIFT.hpp>
 
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
 
+namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 namespace sara = DO::Sara;
 
 
-void detect_keypoints()
+void detect_keypoints(const std::string& dirpath,
+                      const std::string& h5_filepath)
 {
-#if defined(__APPLE__)
-  const auto dirpath = fs::path{"/Users/david/Desktop/Datasets/sfm/castle_int"};
-  const auto image_paths = sara::ls(dirpath.string(), ".png");
+  auto h5_file = sara::H5File{h5_filepath, H5F_ACC_TRUNC};
 
-  auto h5_file = sara::H5File{
-      "/Users/david/Desktop/Datasets/sfm/castle_int.h5", H5F_ACC_TRUNC};
-#else
-  const auto dirpath = fs::path{"/home/david/Desktop/Datasets/sfm/castle_int"};
-  const auto image_paths = sara::ls(dirpath.string(), ".png");
-
-  auto h5_file = sara::H5File{
-      "/home/david/Desktop/Datasets/sfm/castle_int.h5", H5F_ACC_TRUNC};
-#endif
+  auto image_paths = std::vector<std::string>{};
+  append(image_paths, sara::ls(dirpath, ".png"));
+  append(image_paths, sara::ls(dirpath, ".jpg"));
 
   std::for_each(
       std::begin(image_paths), std::end(image_paths), [&](const auto& path) {
@@ -58,19 +54,13 @@ void detect_keypoints()
       });
 }
 
-void read_keypoints()
-{
-#if defined(__APPLE__)
-  const auto dirpath = fs::path{"/Users/david/Desktop/Datasets/sfm/castle_int"};
-  auto h5_file = sara::H5File{"/Users/david/Desktop/Datasets/sfm/castle_int.h5",
-                              H5F_ACC_RDONLY};
-#else
-  const auto dirpath = fs::path{"/home/david/Desktop/Datasets/sfm/castle_int"};
-  auto h5_file = sara::H5File{"/home/david/Desktop/Datasets/sfm/castle_int.h5",
-                              H5F_ACC_RDONLY};
-#endif
 
-  const auto image_paths = sara::ls(dirpath.string(), ".png");
+void read_keypoints(const std::string& dirpath, const std::string& h5_filepath)
+{
+  auto h5_file = sara::H5File{h5_filepath, H5F_ACC_RDONLY};
+  auto image_paths = std::vector<std::string>{};
+  append(image_paths, sara::ls(dirpath, ".png"));
+  append(image_paths, sara::ls(dirpath, ".jpg"));
 
   std::for_each(
       std::begin(image_paths), std::end(image_paths), [&](const auto& path) {
@@ -107,9 +97,60 @@ void read_keypoints()
 }
 
 
-GRAPHICS_MAIN()
+int __main(int argc, char **argv)
 {
-  detect_keypoints();
-  read_keypoints();
-  return 0;
+  try
+  {
+    po::options_description desc{"Detect SIFT keypoints"};
+    desc.add_options()                                                 //
+        ("help, h", "Help screen")                                     //
+        ("dirpath", po::value<std::string>(), "Image directory path")  //
+        ("out_h5_file", po::value<std::string>(), "Output HDF5 file")  //
+        ("read", "Visualize detected keypoints")  //
+        ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+    {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+
+    if (!vm.count("dirpath"))
+    {
+      std::cout << "Missing image directory path" << std::endl;
+      return 0;
+    }
+    if (!vm.count("out_h5_file"))
+    {
+      std::cout << "Missing output H5 file path" << std::endl;
+      return 0;
+    }
+
+    const auto dirpath = vm["dirpath"].as<std::string>();
+    const auto h5_filepath = vm["out_h5_file"].as<std::string>();
+    if (vm.count("read"))
+      read_keypoints(dirpath, h5_filepath);
+    else
+      detect_keypoints(dirpath, h5_filepath);
+
+    return 0;
+  }
+  catch (const po::error& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+
+}
+
+
+int main(int argc, char** argv)
+{
+  DO::Sara::GraphicsApplication app(argc, argv);
+  app.register_user_main(__main);
+  return app.exec();
 }
