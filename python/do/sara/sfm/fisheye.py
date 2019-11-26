a4dbee446774c547edb4f269a6c4f4b295c90c08
David OK <david@humanisingautonomy.com>
2019-11-26 14:41:45 +0000

    FIX: fix misunderstanding regarding angle w.r.t. optical axis.

--- python/do/sara/sfm/fisheye.py
+++ python/do/sara/sfm/fisheye.py
@@ -3,33 +3,57 @@ import numpy as np
 import pylab
 
 
+def tangent_of_angle_from_optical_axis(points):
+    # All the 2D points correspond to 3D points lying on the plane z = 1.
+    x = points[:, 0]
+    y = points[:, 1]
+    z = 1.2
+    return np.sqrt(x ** 2 + y ** 2) / z
+
+
+def radial_distance(points):
+    # We assume in this exercise that the optical center is at the origin
+    # (0, 0, 0).
+    x = points[:, 0]
+    y = points[:, 1]
+    return np.sqrt(x ** 2 + y ** 2)
+
+
 def rectilinear(points, f):
-    # Calculate the radial distance w.r.t. the optical center.
-    tan_theta = points[:, 1] / points[:, 0]
+    print('rectilinear')
+    tan_theta = tangent_of_angle_from_optical_axis(points)
     r = f * tan_theta
-    r = r[:, np.newaxis]
+    return r
 
-    # Recover the unit orientation vector on which to apply the radial
-    # transformation.
-    theta = np.arctan2(points[:, 1], points[:, 0])
-    u = np.stack((np.cos(theta), np.sin(theta))).T
-
-    # The distorted points.
-    return r * u
 
 def equidistant(points, f):
-    theta = np.arctan2(points[:, 1], points[:, 0])
+    print('equidistant')
+    theta = np.arctan(tangent_of_angle_from_optical_axis(points))
 
-    # Calculate the radial distance w.r.t. the optical center.
-    r = f * theta
+    r = 2 * f * np.tan(theta / 2)
+    return r
+
+
+def equisolid_angle(points, f):
+    print('equisolid_angle')
+    theta = np.arctan(tangent_of_angle_from_optical_axis(points))
+    r = 2 * f * np.sin(theta / 2)
+    return r
+
+
+def fisheye(points, f, mapping_fn):
+    # Apply the mapping function.
+    r = mapping_fn(points, f)
     r = r[:, np.newaxis]
 
     # Recover the unit orientation vector on which to apply the radial
     # transformation.
-    u = np.stack((np.cos(theta), np.sin(theta))).T
+    angle = np.arctan2(points[:, 1], points[:, 0])
+    u = np.stack((np.cos(angle), np.sin(angle))).T
 
     # The distorted points.
     return r * u
+
 
 def opensfm_fisheye(points, f, k1=1e-1, k2=1e-1):
     # All the 2D points correspond to 3D points lying on the plane z = 1.
@@ -42,6 +66,7 @@ def opensfm_fisheye(points, f, k1=1e-1, k2=1e-1):
     u = f * d * theta * x/r
     v = f * d * theta * y/r
     return np.stack((u, v)).T
+
 
 def opensfm_spherical(points):
     # All the 2D points correspond to 3D points lying on the plane z = 1.
@@ -66,20 +91,17 @@ a2 = np.stack((a2, np.ones(len(a2)))).T
 # The focal length.
 f = 1.
 
-# Fisheye rectilinear distortion model.
-# a1_d = opensfm_fisheye(a1, f)
-# a2_d = opensfm_fisheye(a2, f)
-# pylab.scatter(a1[:, 0], a1[:, 1])
-# pylab.scatter(a1_d[:, 0], a1_d[:, 1])
-# pylab.scatter(a2[:, 0], a2[:, 1])
-# pylab.scatter(a2_d[:, 0], a2_d[:, 1])
-
 a = np.concatenate((a1, a2))
+
+# Apply the fisheye distortion model.
+# a_d = fisheye(a, f, rectilinear)
+# a_d = fisheye(a, f, equidistant)
+a_d = fisheye(a, f, equisolid_angle)
 # a_d = opensfm_fisheye(a, f)
-a_d = opensfm_spherical(a)
+# a_d = opensfm_spherical(a)
+
 pylab.scatter(a[:, 0], a[:, 1])
 pylab.scatter(a_d[:, 0], a_d[:, 1])
-
 
 
 pylab.show()
