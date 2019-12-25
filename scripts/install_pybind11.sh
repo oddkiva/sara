13b1f0dfd5bfb462d3f84df554111f1b78e5c2fd
David OK <david.ok8@gmail.com>
2019-12-25 11:03:14 -0500

    MAINT: make installation scripts more uniform.

--- scripts/install_pybind11.sh
+++ scripts/install_pybind11.sh
@@ -4,9 +4,9 @@ set -ex
 GITHUB_MASTER_REPOSITORY_PATH=${HOME}/GitHub
 GITHUB_URL=https://github.com
 
-ORGANISATION_NAME=CNugteren
-REPOSITORY_NAME=CLBlast
-VERSION=1.5.0
+ORGANISATION_NAME=pybind
+REPOSITORY_NAME=pybind11
+VERSION=v2.4.3
 
 if [ ! -d "${GITHUB_MASTER_REPOSITORY_PATH}" ]; then
   mkdir -p ${GITHUB_MASTER_REPOSITORY_PATH}
@@ -34,9 +34,9 @@ pushd ${REPOSITORY_DIR}
 
   pushd build
   {
-    cmake -DTESTS:BOOL=ON -DTUNERS:BOOL=ON ..
+    cmake ..
+    #  make -j$(nproc) check
     make -j$(nproc)
-    ctest --verbose -j$(nproc)
     sudo make install
   }
   popd
