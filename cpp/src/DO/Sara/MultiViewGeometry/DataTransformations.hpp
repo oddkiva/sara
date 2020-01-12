854c46fbb571ffc92907fecb8fe2444d4a2a8ca7
David OK <david.ok8@gmail.com>
2020-01-12 12:30:37 -0500

    MAINT: reorganize documentation.

--- cpp/src/DO/Sara/MultiViewGeometry/DataTransformations.hpp
+++ cpp/src/DO/Sara/MultiViewGeometry/DataTransformations.hpp
@@ -19,41 +19,48 @@
 
 namespace DO::Sara {
 
-DO_SARA_EXPORT
-auto to_tensor(const std::vector<Match>& matches) -> Tensor_<int, 2>;
+  /*!
+   *  @ingroup MultiViewGeometry
+   *  @{
+   */
 
-DO_SARA_EXPORT
-auto extract_centers(const std::vector<OERegion>& features)
-    -> Tensor_<float, 2>;
+  DO_SARA_EXPORT
+  auto to_tensor(const std::vector<Match>& matches) -> Tensor_<int, 2>;
 
-DO_SARA_EXPORT
-auto to_point_indices(const TensorView_<int, 2>& samples,
-                      const TensorView_<int, 2>& matches)  //
-    -> Tensor_<int, 3>;
+  DO_SARA_EXPORT
+  auto extract_centers(const std::vector<OERegion>& features)
+      -> Tensor_<float, 2>;
 
-template <typename T>
-auto to_coordinates(const TensorView_<int, 3>& point_indices,
-                    const TensorView_<T, 2>& p1,
-                    const TensorView_<T, 2>& p2)  //
-{
-  const auto num_samples = point_indices.size(0);
-  const auto sample_size = point_indices.size(1);
-  const auto num_points = 2;
-  const auto coords_dim = p1.size(1);
+  DO_SARA_EXPORT
+  auto to_point_indices(const TensorView_<int, 2>& samples,
+                        const TensorView_<int, 2>& matches)  //
+      -> Tensor_<int, 3>;
 
-  auto p = Tensor_<T, 4>{{num_samples, sample_size, num_points, coords_dim}};
+  template <typename T>
+  auto to_coordinates(const TensorView_<int, 3>& point_indices,
+                      const TensorView_<T, 2>& p1,
+                      const TensorView_<T, 2>& p2)  //
+  {
+    const auto num_samples = point_indices.size(0);
+    const auto sample_size = point_indices.size(1);
+    const auto num_points = 2;
+    const auto coords_dim = p1.size(1);
 
-  for (auto s = 0; s < num_samples; ++s)
-    for (auto m = 0; m < sample_size; ++m)
-    {
-      auto p1_idx = point_indices(s, m, 0);
-      auto p2_idx = point_indices(s, m, 1);
+    auto p = Tensor_<T, 4>{{num_samples, sample_size, num_points, coords_dim}};
 
-      p[s][m][0].flat_array() = p1[p1_idx].flat_array();
-      p[s][m][1].flat_array() = p2[p2_idx].flat_array();
-    }
+    for (auto s = 0; s < num_samples; ++s)
+      for (auto m = 0; m < sample_size; ++m)
+      {
+        auto p1_idx = point_indices(s, m, 0);
+        auto p2_idx = point_indices(s, m, 1);
 
-  return p;
-}
+        p[s][m][0].flat_array() = p1[p1_idx].flat_array();
+        p[s][m][1].flat_array() = p2[p2_idx].flat_array();
+      }
+
+    return p;
+  }
+
+  //! @}
 
 } /* namespace DO::Sara */
