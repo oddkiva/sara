801cdd156f7ca89be44077caa33e8f41be55f0a9
David OK <david.ok8@gmail.com>
2020-01-25 18:48:13 -0500

    MAINT: save work.

--- cpp/drafts/MatchPropagation/cpp/tools/validate_region_growing_neighborhood_params.cpp
+++ cpp/drafts/MatchPropagation/cpp/tools/validate_region_growing_neighborhood_params.cpp
@@ -1,12 +1,22 @@
 // ========================================================================== //
-// This file is part of DO++ MatchPropagation which was presented in:
+// This file is part of Sara, a basic set of libraries in C++ for computer
+// vision.
 //
-//  Efficient and Scalable 4th-order Match Propagation
-//  David Ok, Renaud Marlet, and Jean-Yves Audibert.
-//  ACCV 2012, Daejeon, South Korea.
+// Copyright (C) 2013-present David Ok <david.ok8@gmail.com>
 //
-// Copyright (c) 2013. David Ok, Imagine (ENPC/CSTB).
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
 // ========================================================================== //
+
+//! @file
+/*!
+ *  This file implements a part of the method published in:
+ *
+ *  Efficient and Scalable 4th-order Match Propagation
+ *  David Ok, Renaud Marlet, and Jean-Yves Audibert.
+ *  ACCV 2012, Daejeon, South Korea.
+ */
 
 #include <DO/Sara/FeatureDetectors.hpp>
 #include <DO/Sara/FeatureMatching.hpp>
