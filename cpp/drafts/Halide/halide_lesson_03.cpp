7668c537272aa686b7d734db469272ac519a2af6
David OK <david.ok8@gmail.com>
2020-03-08 16:39:22 +0000

    MAINT: refactor code.

--- cpp/drafts/Halide/halide_lesson_03.cpp
+++ cpp/drafts/Halide/halide_lesson_03.cpp
@@ -16,7 +16,7 @@ using namespace std;
 using namespace DO::Sara;
 
 
-namespace DO::Sara::halide {
+namespace DO::Sara::HalideBackend {
 
   auto find_non_cuda_gpu_target() -> Halide::Target
   {
@@ -72,6 +72,13 @@ namespace DO::Sara::halide {
     return target;
   }
 
+  auto as_interleaved_rgb_buffer(ImageView<Rgb8>& image)
+  {
+    return Halide::Buffer<uint8_t>::make_interleaved(
+        reinterpret_cast<uint8_t*>(image.data()), image.width(), image.height(),
+        3);
+  }
+
   template <typename T = std::uint8_t>
   struct Pipeline
   {
@@ -84,54 +91,23 @@ namespace DO::Sara::halide {
     Halide::Var xi{};
     Halide::Var yi{};
 
-    Halide::Buffer<T> input_buffer;
+    Halide::Target target{get_gpu_target()};
 
-    explicit Pipeline(const ImageView<T>& input_image)
-      : input_buffer{Halide::Buffer<T>::make_interleaved(
-            const_cast<T*>(input_image.data()), input_image.width(),
-            input_image.height(), 3)}
+    Halide::Buffer<T>& input_buffer;
+
+    Pipeline(Halide::Buffer<T>& input)
+      : input_buffer{input}
     {
     }
 
-    explicit Pipeline(const ImageView<Rgb8>& input_image)
-      : input_buffer{Halide::Buffer<T>::make_interleaved(
-            const_cast<std::uint8_t*>(
-                reinterpret_cast<const std::uint8_t*>(input_image.data())),
-            input_image.width(), input_image.height(), 3)}
+    auto set_target(const Halide::Target& t) -> void
     {
-      static_assert(std::is_same_v<T, std::uint8_t>);
+      target = t;
     }
-  };
 
-  struct CastToFloat : public Pipeline<std::uint8_t>
-  {
-    using base_type = Pipeline<std::uint8_t>
-
-    Halide::Func cast{"Cast"};
-    Halide::Buffer<float> output_buffer;
-
-    explicit CastToFloat(const ImageView<Rgb8>& input_image,
-                         ImageView<Rgb8>& output_image)
-      : {Halide::Buffer<uint8_t>::make_interleaved(
-            (uint8_t*) (input_image.data()), input_image.width(),
-            input_image.height(), 3)}
-      , output_buffer{Halide::Buffer<uint8_t>::make_interleaved(
-            reinterpret_cast<uint8_t*>(output_image.data()),
-            output_image.width(), output_image.height(), 3)}
+    auto set_output_format_to_packed_rgb(Halide::Func& f) -> void
     {
-      // Halide uses the following indexing (x, y, c)
-      // 0: x-coordinate
-      // 1: y-coordinate
-      // 2: c being the channel index.
-      cast(x, y, c) = input_buffer(x, y, c) / 255.f;
-
-      // The image is assumed to be in packed pixel format.
-      //             0   1       w - 1
-      // row 0     : RGB RGB ... RGB
-      // row 1     : RGB RGB ... RGB
-      // ...
-      // row h - 1 : RGB RGB ... RGB
-      cast.output_buffer()    //
+      f.output_buffer()       //
           .dim(0)             // x
           .set_stride(3)      // because of the packed pixel format.
           .dim(2)             // c
@@ -141,24 +117,149 @@ namespace DO::Sara::halide {
       // for y:
       //   for x:
       //     for c: <- unroll this loop
-      cast.reorder(c, x, y).bound(c, 0, 3).unroll(c);
+      f.reorder(c, x, y).bound(c, 0, 3).unroll(c);
 
-      cast.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
-    };
+      f.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
+    }
+  };
 
-    // auto operator()(const )
-    // {
-    //   input_buffer.set_host_dirty();
-    //   cast.realize(output_buffer);
-    //   output_buffer.copy_to_host();
-    // }
+  struct CastToFloat : public Pipeline<std::uint8_t>
+  {
+    using base_type = Pipeline<std::uint8_t>;
+
+    mutable Halide::Func cast{"Cast"};
+
+    CastToFloat(Halide::Buffer<std::uint8_t>& input)
+      : base_type{input}
+    {
+      cast(x, y, c) = input_buffer(x, y, c) / 255.f;
+    }
+
+    auto operator()(Halide::Buffer<float>& output_buffer) const -> void
+    {
+      input_buffer.set_host_dirty();
+      cast.realize(output_buffer);
+      output_buffer.copy_to_host();
+    }
+  };
+
+  struct Padded : public CastToFloat
+  {
+    using base_type = CastToFloat;
+
+    mutable Halide::Func padded{"Padded"};
+
+    Padded(Halide::Buffer<std::uint8_t>& input)
+      : base_type{input}
+    {
+      padded(x, y, c) = cast(clamp(x, 0, input_buffer.width() - 1),   //
+                             clamp(y, 0, input_buffer.height() - 1),  //
+                             c);
+    }
+  };
+
+  struct Blur3x3 : public Padded
+  {
+    using base_type = Padded;
+
+    mutable Halide::Func blur{"Blur3x3"};
+
+    Blur3x3(Halide::Buffer<std::uint8_t>& input)
+      : base_type{input}
+    {
+      blur(x, y, c) = (padded(x - 1, y - 1, c) + padded(x - 0, y - 1, c) +
+                       padded(x + 1, y - 1, c) + padded(x - 1, y + 0, c) +
+                       padded(x - 0, y + 0, c) + padded(x + 1, y + 0, c) +
+                       padded(x - 1, y + 1, c) + padded(x - 0, y + 1, c) +
+                       padded(x + 1, y + 1, c)) /
+                      9.f;
+    }
+  };
+
+  struct Laplacian : public Padded
+  {
+    using base_type = Padded;
+
+    mutable Halide::Func laplacian{"Laplacian"};
+
+    Laplacian(Halide::Buffer<std::uint8_t>& input)
+      : base_type{input}
+    {
+      laplacian(x, y, c) = padded(x, y, c) -
+                           (padded(x + 1, y + 0, c) + padded(x - 1, y + 0, c) +
+                            padded(x + 0, y + 1, c) + padded(x + 0, y - 1, c)) /
+                               4.f;
+    }
+  };
+
+  struct Blur3x3Vis : public Blur3x3
+  {
+    using base_type = Blur3x3;
+
+    mutable Halide::Func rescaled{"Rescaled"};
+
+    Blur3x3Vis(Halide::Buffer<std::uint8_t>& input)
+      : base_type{input}
+    {
+      // The output result to show on the screen.
+      rescaled(x, y, c) = Halide::cast<uint8_t>(blur(x, y, c) * 255.f);
+
+      set_output_format_to_packed_rgb(rescaled);
+
+      // Calculate the padded image as an intermediate result for the laplacian
+      // calculation in a shared memory.
+      padded.compute_at(rescaled, xo);
+      padded.gpu_threads(x, y);
+
+      rescaled.compile_jit(target);
+    }
+
+    auto operator()(Halide::Buffer<std::uint8_t>& output_buffer) const -> void
+    {
+      input_buffer.set_host_dirty();
+      rescaled.realize(output_buffer);
+      output_buffer.copy_to_host();
+    }
+  };
+
+  struct LaplacianVis : public Laplacian
+  {
+    using base_type = Laplacian;
+
+    mutable Halide::Func rescaled{"Rescaled"};
+
+    LaplacianVis(Halide::Buffer<std::uint8_t>& input)
+      : base_type{input}
+    {
+      // The output result to show on the screen.
+      rescaled(x, y, c) = Halide::cast<uint8_t>(laplacian(x, y, c) * 255.f);
+
+      set_output_format_to_packed_rgb(rescaled);
+
+      // Calculate the padded image as an intermediate result for the laplacian
+      // calculation in a shared memory.
+      padded.compute_at(rescaled, xo);
+      padded.gpu_threads(x, y);
+
+      rescaled.compile_jit(target);
+    }
+
+    auto operator()(Halide::Buffer<std::uint8_t>& output_buffer) const -> void
+    {
+      input_buffer.set_host_dirty();
+      rescaled.realize(output_buffer);
+      output_buffer.copy_to_host();
+    }
   };
 
   struct Deriche : Pipeline<std::uint8_t>
   {
+    using base_type = Pipeline<std::uint8_t>;
+
     Halide::Func deriche{"Deriche"};
 
-    Deriche()
+    Deriche(Halide::Buffer<std::uint8_t>& input)
+      : base_type{input}
     {
       deriche(x, y, c) = input_buffer(x, y, c) / 255.f;
 
@@ -178,9 +279,31 @@ namespace DO::Sara::halide {
 
 }  // namespace DO::Sara::halide
 
+
+struct TicToc {
+  // Timer.
+  Timer timer;
+  double elapsed;
+} tictoc;
+
+void tic()
+{
+  tictoc.timer.restart();
+}
+
+void toc(const std::string& what)
+{
+  const auto elapsed = tictoc.timer.elapsed_ms();
+  std::cout << "[" << what << "] " << elapsed <<  " ms" << std::endl;
+}
+
+
 auto halide_pipeline() -> void
 {
+  namespace halide = HalideBackend;
+
   using namespace std::string_literals;
+
 #ifdef _WIN32
   const auto video_filepath =
       "C:/Users/David/Desktop/david-archives/gopro-backup-2/GOPR0542.MP4"s;
@@ -194,198 +317,33 @@ auto halide_pipeline() -> void
 
   VideoStream video_stream(video_filepath);
 
-  // Configure Halide to use the GPU.
-  auto target = halide::get_gpu_target();
-
   // Input and output images.
   auto input_image = video_stream.frame();
   auto output_image = Image<Rgb8>{video_stream.sizes()};
 
   // Halide input and output buffers.
-  auto input_buffer = Halide::Buffer<uint8_t>::make_interleaved(
-      reinterpret_cast<uint8_t*>(input_image.data()), input_image.width(),
-      input_image.height(), 3);
+  auto input_buffer = halide::as_interleaved_rgb_buffer(input_image);
+  auto output_buffer = halide::as_interleaved_rgb_buffer(output_image);
 
-  auto output_buffer = Halide::Buffer<uint8_t>::make_interleaved(
-      reinterpret_cast<uint8_t*>(output_image.data()), output_image.width(),
-      output_image.height(), 3);
+  // Blur pipeline.
+  //const auto pipeline = halide::Blur3x3Vis{input_buffer};
+  const auto pipeline = halide::LaplacianVis{input_buffer};
 
-  // Halide pipeline.
-  auto x = Halide::Var{};
-  auto y = Halide::Var{};
-  auto c = Halide::Var{};
-
-  auto xo = Halide::Var{};
-  auto yo = Halide::Var{};
-  auto xi = Halide::Var{};
-  auto yi = Halide::Var{};
-
-  // Implicitly cast into RGB planar format.
-  auto cast = Halide::Func{"cast"};
-  cast(x, y, c) = input_buffer(x, y, c) / 255.f;
-
-  auto padded = Halide::Func{"padded"};
-  padded(x, y, c) = cast(clamp(x, 0, input_buffer.width() - 1),   //
-                         clamp(y, 0, input_buffer.height() - 1),  //
-                         c);
-
-  // The filter
-  auto filter = Halide::Func{"filter"};
-
-  // Laplacian.
-  // filter(x, y, c) = Halide::abs(
-  //    padded(x, y, c) - (padded(x + 1, y + 0, c) + padded(x - 1, y + 0, c) +
-  //                       padded(x + 0, y + 1, c) + padded(x + 0, y - 1, c)) /
-  //                          4.f);
-
-  // Blur.
-  filter(x, y, c) = (padded(x - 1, y - 1, c) + padded(x - 0, y - 1, c) +
-                     padded(x + 1, y - 1, c) + padded(x - 1, y + 0, c) +
-                     padded(x - 0, y + 0, c) + padded(x + 1, y + 0, c) +
-                     padded(x - 1, y + 1, c) + padded(x - 0, y + 1, c) +
-                     padded(x + 1, y + 1, c)) /
-                    9.f;
-
-  // The output result to show on the screen.
-  auto filter_rescaled = Halide::Func{"rescaled"};
-  // filter_rescaled(x, y, c) =
-  //     Halide::cast<uint8_t>((filter(x, y, c) / 2.f) * 255.f);
-  filter_rescaled(x, y, c) = Halide::cast<uint8_t>(filter(x, y, c) * 255.f);
-
-  // Specify that the output buffer is in interleaved RGB format.
-  filter_rescaled.output_buffer()
-      .dim(0)
-      .set_stride(3)
-      .dim(2)
-      .set_stride(1)
-      .set_bounds(0, 3);
-  filter_rescaled.reorder(c, x, y).bound(c, 0, 3).unroll(c);
-  filter_rescaled.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
-
-  // Calculate the padded image as an intermediate result for the laplacian
-  // calculation in a shared memory.
-  padded.compute_at(filter_rescaled, xo);
-  padded.gpu_threads(x, y);
-
-  filter_rescaled.compile_jit(target);
-
-  // Timer.
-  auto timer = Timer{};
-  auto elapsed = double{};
 
   create_window(video_stream.sizes());
   while (true)
   {
-    timer.restart();
+    tic();
     if (!video_stream.read())
     {
       std::cout << "Reached the end of the video!" << std::endl;
       break;
     }
-    elapsed = timer.elapsed_ms();
-    std::cout << "Video decoding time = " << elapsed << " ms" << std::endl;
+    toc("Video Decoding");
 
-    timer.restart();
-    {
-      input_buffer.set_host_dirty();
-      filter_rescaled.realize(output_buffer);
-      output_buffer.copy_to_host();
-    }
-    elapsed = timer.elapsed_ms();
-    std::cout << "Halide computation time = " << elapsed << " ms" << std::endl;
-
-    display(output_image);
-  }
-}
-
-
-auto halide_deriche_pipeline(const std::string& video_filepath) -> void
-{
-  VideoStream video_stream(video_filepath);
-
-  const auto gpu_target = halide::get_gpu_target();
-
-  // Input and output images.
-  auto input_image = video_stream.frame();
-  auto output_image = Image<Rgb8>{video_stream.sizes()};
-
-  // Halide input and output buffers.
-  auto input_buffer = Halide::Buffer<uint8_t>::make_interleaved(
-      reinterpret_cast<uint8_t*>(input_image.data()), input_image.width(),
-      input_image.height(), 3);
-
-  auto output_buffer = Halide::Buffer<uint8_t>::make_interleaved(
-      reinterpret_cast<uint8_t*>(output_image.data()), output_image.width(),
-      output_image.height(), 3);
-
-  // Halide pipeline.
-  auto x = Halide::Var{};
-  auto y = Halide::Var{};
-  auto c = Halide::Var{};
-
-  auto xo = Halide::Var{};
-  auto yo = Halide::Var{};
-  auto xi = Halide::Var{};
-  auto yi = Halide::Var{};
-
-  // Implicitly cast into RGB planar format.
-  auto cast = Halide::Func{"cast"};
-  cast(x, y, c) = input_buffer(x, y, c) / 255.f;
-
-  Halide::RDom ry(1, input_image.height() - 1);
-  Halide::Expr flip_ry = input_image.height() - ry - 1;
-  Halide::Expr alpha;
-
-  // The filter
-  auto deriche = Halide::Func{"filter"};
-  deriche(x, ry, c) =
-      (1 - alpha) * deriche(x, ry - 1, c) + alpha * cast(x, ry, c);
-
-  // The output result to show on the screen.
-  auto filter_rescaled = Halide::Func{"rescaled"};
-  filter_rescaled(x, y, c) = Halide::cast<uint8_t>(deriche(x, y, c) * 255.f);
-
-  // Specify that the output buffer is in interleaved RGB format.
-  filter_rescaled.output_buffer()
-      .dim(0)
-      .set_stride(3)
-      .dim(2)
-      .set_stride(1)
-      .set_bounds(0, 3);
-  filter_rescaled.reorder(c, x, y).bound(c, 0, 3).unroll(c);
-  filter_rescaled.gpu_tile(x, y, xo, yo, xi, yi, 16, 16);
-
-  // Calculate the padded image as an intermediate result for the laplacian
-  // calculation in a shared memory.
-  // padded.compute_at(filter_rescaled, xo);
-  // padded.gpu_threads(x, y);
-
-  filter_rescaled.compile_jit(gpu_target);
-
-  // Timer.
-  auto timer = Timer{};
-  auto elapsed = double{};
-
-  create_window(video_stream.sizes());
-  while (true)
-  {
-    timer.restart();
-    if (!video_stream.read())
-    {
-      std::cout << "Reached the end of the video!" << std::endl;
-      break;
-    }
-    elapsed = timer.elapsed_ms();
-    std::cout << "Video decoding time = " << elapsed << " ms" << std::endl;
-
-    timer.restart();
-    {
-      input_buffer.set_host_dirty();
-      filter_rescaled.realize(output_buffer);
-      output_buffer.copy_to_host();
-    }
-    elapsed = timer.elapsed_ms();
-    std::cout << "Halide computation time = " << elapsed << " ms" << std::endl;
+    tic();
+    pipeline(output_buffer);
+    toc("Halide");
 
     display(output_image);
   }
