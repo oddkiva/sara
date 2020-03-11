34a46495193f7f84dc9eb73e23581d5c69e64a76
David OK <david.ok8@gmail.com>
2020-03-11 13:32:50 -0400

    MAINT: fix function.

--- cpp/drafts/Halide/sara_halide_rgb_to_gray.cpp
+++ cpp/drafts/Halide/sara_halide_rgb_to_gray.cpp
@@ -14,11 +14,15 @@ namespace {
 
     void generate()
     {
+      // Deal with interleaved RGB pixel format.
+      input.dim(0).set_stride(3).dim(2).set_stride(1);
+      input.dim(2).set_bounds(0, 3);
+
       Var x{"x"}, y{"y"};
 
-      auto r = input(x, y, 0);
-      auto g = input(x, y, 1);
-      auto b = input(x, y, 2);
+      auto r = input(x, y, 0) / 255.f;
+      auto g = input(x, y, 1) / 255.f;
+      auto b = input(x, y, 2) / 255.f;
 
       auto gray = Func{"gray"};
       output(x, y) = 0.2125f * r + 0.7154f * g + 0.0721f * b;
