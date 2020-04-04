3ff4628cff390bba50d738af0f4134047a13c8de
David OK <david.ok8@gmail.com>
2020-04-04 16:38:31 +0100

    MAINT: version missing files.

---
+++ cpp/drafts/Halide/MyHalide.hpp
@@ -0,0 +1,23 @@
+// ========================================================================== //
+// This file is part of Sara, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2020-present David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+
+#include <Halide.h>
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
+
