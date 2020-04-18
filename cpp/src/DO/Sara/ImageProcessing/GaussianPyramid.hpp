05155e92ba74c92fac111f0014c6e0c890e8489b
David OK <david.ok8@gmail.com>
2020-04-18 14:55:04 +0100

    DOC: improve explanation.

--- cpp/src/DO/Sara/ImageProcessing/GaussianPyramid.hpp
+++ cpp/src/DO/Sara/ImageProcessing/GaussianPyramid.hpp
@@ -84,7 +84,7 @@ namespace DO { namespace Sara {
       G.octave_scaling_factor(o) =
           (o == 0) ? 1.f / resize_factor : G.octave_scaling_factor(o - 1) * 2;
 
-      // Compute the gaussians in octave \f$o\f$
+      // Compute the gaussians in octave @f$o@f$
       auto sigma_s_1 = init_sigma;
       G(0, o) = o == 0 ? I : downscale(G(downscale_index, o - 1), 2);
 
@@ -149,8 +149,8 @@ namespace DO { namespace Sara {
     return LoG;
   }
 
-  //! Computes the gradient vector of \f$I(x,y,\sigma)\f$ at \f$(x,y,\sigma)\f$,
-  //! where \f$\sigma = 2^{s/S + o}\f$ where \f$S\f$ is the number of scales per
+  //! Computes the gradient vector of @f$I(x,y,\sigma)@f$ at @f$(x,y,\sigma)@f$,
+  //! where @f$\sigma = 2^{s/S + o}@f$ where @f$S@f$ is the number of scales per
   //! octave.
   template <typename T>
   Matrix<T, 3, 1> gradient(const ImagePyramid<T>& I, int x, int y, int s, int o)
@@ -167,8 +167,8 @@ namespace DO { namespace Sara {
     return d;
   }
 
-  //! Computes the hessian matrix of \f$I(x,y,\sigma)\f$ at \f$(x,y,\sigma)\f$,
-  //! where \f$\sigma = 2^{s/S + o}\f$ where \f$S\f$ is the number of scales
+  //! Computes the hessian matrix of @f$I(x,y,\sigma)@f$ at @f$(x,y,\sigma)@f$,
+  //! where @f$\sigma = 2^{s/S + o}@f$ where @f$S@f$ is the number of scales
   //! per octave.
   template <typename T>
   Matrix<T, 3, 3> hessian(const ImagePyramid<T>& I, int x, int y, int s, int o)
--- cpp/src/DO/Sara/ImageProcessing/ImagePyramid.hpp
+++ cpp/src/DO/Sara/ImageProcessing/ImagePyramid.hpp
@@ -46,20 +46,54 @@ namespace DO { namespace Sara {
     }
 
     /*!
-      Let us consider an image \f$I\f$. In the scale-space framework, we work
-      with the scale-space image function
-      \f$ I : (x,y,\sigma) \mapsto (g_\sigma * I)\ (x,y) \f$,
-      where \f$g_\sigma\f$ is the gaussian distribution with zero mean and
-      standard deviation \f$\sigma\f$.
-
-      We also denote by \f$ I_\sigma : (x,y) \mapsto I(x,y,\sigma) \f$ the
-      image \f$I\f$ at scale \f$\sigma\f$.
-      Note that the real image is \f$I_0: (x,y) \mapsto I(x,y)\f$.
-      Because of the spatial sampling, the camera captures the real image
-      \f$I_0\f$ with some blurring and the camera-acquired image is
-      \f$I_{\sigma_\textrm{camera}}\f$.
-
-      Here cameraSigma() thus corresponds to \f$\sigma_\textrm{camera}\f$.
+     *  Let @f$I@f$ be an image function that maps a position @f$(x, y) \in
+     *  \mathbb{R}^2@f$ to an intensity value in @f$\mathbb{R}@f$.
+     *
+     *  In theory, this corresponds to the ideal image function where we would
+     *  know the intensity value with perfect precision at any position @f$(x,
+     *  y)@f$ at any fine resolution @f$\sigma@f$ in meters (millimeters,
+     *  microns, and so on...). The resolution is the scale.
+     *
+     *  There is this nice stackexchange answer:
+     *  https://biology.stackexchange.com/questions/26189/whats-the-smallest-size-a-human-eye-can-see.
+     *
+     *  Just like the smallest size that the human eye can perceive is of the
+     *  order of a tens of microns, the camera captures photographs with details
+     *  limited by its resolution @f$\sigma@f$. From this resolution and coarser
+     *  resolutions, the pixel value is known with good certainty.
+     *
+     *  Each pixel of a photograph can be viewed as a local averaging of all
+     *  values @f$I(x, y)@f$ on a patch of size @f$\sigma@f$ meters. All these
+     *  pixel values would be known with infinitesimally small resolution
+     *  @f$\sigma \rightarrow 0@f$.
+     *
+     *  In the scale-space framework, the local averaging function that is
+     *  chosen is the Gaussian kernel and the image function is augmented with
+     *  an additional dimension which is the scale:
+     *
+     *  @f$ I : (x,y,\sigma) \mapsto (g_\sigma * I)\ (x,y) @f$,
+     *
+     *  where @f$g_\sigma@f$ is the Gaussian distribution with zero mean and
+     *  standard deviation @f$\sigma@f$.
+     *
+     *  We also denote by @f$ I_\sigma : (x,y) \mapsto I(x,y,\sigma) @f$ the
+     *  image @f$I@f$ at scale @f$\sigma@f$.
+     *
+     *
+     *  Because of the spatial sampling, the camera captures the real image
+     *  @f$I_0@f$ with some blurring and the camera-acquired image is
+     *  @f$I_{\sigma_\textrm{camera}}@f$.
+     *
+     *  Here scale_camera() corresponds to camera resolution
+     *  @f$\sigma_\textrm{camera}@f$.
+     *
+     *  The digital image that our computers read is
+     *  @f$I_{\sigma_\textrm{camera}}@f$ and not @f$I@f$.
+     *
+     *  Now the caveat here is that @f$\sigma_\textrm{camera}@f$ is not known in
+     *  meters and we work with pixels. The default parameter we use is
+     *  @f$0.5@f$, which means that we assume we can estimate the pixel values
+     *  with good confidence up to half a pixel resolution.
      */
     double scale_camera() const
     {
@@ -67,12 +101,12 @@ namespace DO { namespace Sara {
     }
 
     /*!
-      The image pyramid discretizes image function
-      \f$(x,y,\sigma) \mapsto I(x,y,\sigma)\f$ in the scale-space.
-      With the discretization, the image pyramid will consist of a stack of
-      blurred images \f$(I_{\sigma_i})_{1 \leq i \leq N}\f$.
-
-      Here initSigma() corresponds to \f$\sigma_0\f$.
+     *  The image pyramid discretizes image function
+     *  @f$(x,y,\sigma) \mapsto I(x,y,\sigma)@f$ in the scale-space.
+     *  With the discretization, the image pyramid will consist of a stack of
+     *  blurred images @f$(I_{\sigma_i})_{0 \leq i < N}@f$.
+     *
+     *  Here scale_initial() corresponds to @f$\sigma_0@f$.
      */
     double scale_initial() const
     {
@@ -80,20 +114,21 @@ namespace DO { namespace Sara {
     }
 
     /*!
-      The sequence \f$ (\sigma_i)_{1 \leq i \leq N} \f$ follows a geometric
-      progression, i.e., \f$\sigma_i = k^i \sigma_0\f$.
-
-      Laplacians of Gaussians \f$ \nabla^2 I_{\sigma} \f$ can be approximated
-      efficiently with differences of Gaussians \f$ I_{k\sigma} - I_{\sigma} \f$
-      for each \f$ \sigma = \sigma_i \f$*without needing to renormalize*.
-
-      Indeed:
-      \f{eqnarray*}{
-        \frac{\partial I_\sigma}{\partial \sigma} &=& \sigma \nabla^2 I_\sigma \\
-        I_{k\sigma} - I_{\sigma} &\approx& (k-1) \sigma^2 \nabla^2 I_\sigma
-      \f}
-
-      Here scale_geometric_factor() corresponds to the value \f$ k \f$.
+     *  The sequence @f$ (\sigma_i)_{1 \leq i \leq N} @f$ follows a geometric
+     *  progression, i.e., @f$\sigma_i = k^i \sigma_0@f$.
+     *
+     *  Laplacians of Gaussians @f$ \nabla^2 I_{\sigma} @f$ can be approximated
+     *  efficiently with differences of Gaussians @f$ I_{k\sigma} - I_{\sigma} @f$
+     *  for each @f$ \sigma = \sigma_i @f$ without the need to normalize the
+     *  difference of Gaussians.
+     *
+     *  Thus
+     *  \f{eqnarray*}{
+     *    \frac{\partial I_\sigma}{\partial \sigma} &=& \sigma \nabla^2 I_\sigma \\
+     *    I_{k\sigma} - I_{\sigma} &\approx& (k-1) \sigma^2 \nabla^2 I_\sigma
+     *  \f}
+     *
+     *  Here scale_geometric_factor() corresponds to the value @f$ k @f$.
      */
     double scale_geometric_factor() const
     {
@@ -101,15 +136,15 @@ namespace DO { namespace Sara {
     }
 
     /*!
-      The smoothed image \f$I_{2\sigma}\f$ is equivalent to the downsampled
-      image \f$I_{\sigma}\f$. Because of this observation, a pyramid of
-      gaussians is divided into octaves.
-
-      We call an octave a stack of \f$S\f$ blurred images \f$I_{\sigma_i}\f$
-      separated by a constant factor \f$k\f$ in the scale space, i.e.,
-      \f$ \sigma_{i+1} = k \sigma_i \f$.
-      The number of scales in each octave is the integer \f$S\f$ such that
-      \f$k^S \sigma = 2 \sigma\f$, i.e., \f$ k= 2^{1/S} \f$.
+     *  The smoothed image @f$I_{2\sigma}@f$ is equivalent to the downsampled
+     *  image @f$I_{\sigma}@f$. Because of this observation, a pyramid of
+     *  gaussians is divided into octaves.
+     *
+     *  We call an octave a stack of @f$S@f$ blurred images @f$I_{\sigma_i}@f$
+     *  separated by a constant factor @f$k@f$ in the scale space, i.e.,
+     *  @f$ \sigma_{i+1} = k \sigma_i @f$.
+     *  The number of scales in each octave is the integer @f$S@f$ such that
+     *  @f$k^S \sigma = 2 \sigma@f$, i.e., @f$ k= 2^{1/S} @f$.
      */
     int num_scales_per_octave() const
     {
@@ -123,9 +158,9 @@ namespace DO { namespace Sara {
     }
 
     /*!
-      \todo. Improve explanation.
-      \f$(1/2)^i\f$ is the rescaling factor of the downsampled image of octave
-      \f$i\f$.
+     *  @todo. Improve explanation.
+     *  @f$(1/2)^i@f$ is the rescaling factor of the downsampled image of octave
+     *  @f$i@f$.
      */
     int first_octave_index() const
     {
