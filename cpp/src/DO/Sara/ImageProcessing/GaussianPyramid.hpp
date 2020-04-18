813d7e613b1211f2fe07b3c86587d61b2e795936
David OK <david.ok8@gmail.com>
2020-04-18 22:21:43 +0100

    MAINT: update copyright years.

--- cpp/src/DO/Sara/ImageProcessing/GaussianPyramid.hpp
+++ cpp/src/DO/Sara/ImageProcessing/GaussianPyramid.hpp
@@ -2,7 +2,7 @@
 // This file is part of Sara, a basic set of libraries in C++ for computer
 // vision.
 //
-// Copyright (C) 2013-2016 David Ok <david.ok8@gmail.com>
+// Copyright (C) 2013-present David Ok <david.ok8@gmail.com>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License v. 2.0. If a copy of the MPL was not distributed with this file,
