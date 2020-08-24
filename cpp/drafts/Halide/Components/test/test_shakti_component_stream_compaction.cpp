baee97c71e985c5de9d44350fbcbbbb83c70dcea
David OK <david.ok8@gmail.com>
2020-08-24 23:51:55 +0100

    WIP: scaffold stream compaction.

--- cpp/drafts/Halide/Components/test/test_shakti_component_stream_compaction.cpp
+++ cpp/drafts/Halide/Components/test/test_shakti_component_stream_compaction.cpp
@@ -15,13 +15,10 @@
 
 #include <DO/Sara/Core/DebugUtilities.hpp>
 
-#include <drafts/Halide/Components/Evaluation.hpp>
-#include <drafts/Halide/Components/TinyLinearAlgebra.hpp>
+#include <drafts/Halide/Components/StreamCompaction.hpp>
 
 
-using DO::Shakti::HalideBackend::eval;
-using DO::Shakti::HalideBackend::Matrix2;
-using DO::Shakti::HalideBackend::Matrix3;
+namespace halide = DO::Shakti::HalideBackend;
 
 using namespace Halide;
 
@@ -55,48 +52,18 @@ BOOST_AUTO_TEST_CASE(test_stream_compaction)
   const auto size = w * h * c * n;
 
   Var x;
-  Func in_flattened;
-  in_flattened(x) = in(x % w, (x / w) % h, (x / (w * h)) % c, x / (w * h * c));
+  auto in_flattened = halide::flatten_4d(in, x, w, h, c);
 
-  Func prefix_sum;
-  prefix_sum(x) = cast<int32_t>(0);
+  auto compacted_indices = halide::non_zeros_indices(in_flattened, x, size);
 
-  RDom range(1, size - 1);
-  prefix_sum(range) = select(in_flattened(range - 1) != 0,  //
-                             prefix_sum(range - 1) + 1,     //
-                             prefix_sum(range - 1));
+  auto [non_zeros_x, non_zeros_y, non_zeros_c, non_zeros_n] =
+      halide::unflatten_4d(compacted_indices, x, w, h, c);
 
-  RDom in_range(0, size);
-  Func compacted_indices;
-  compacted_indices(x) = -1;
-  compacted_indices(clamp(prefix_sum(in_range), 0, size - 1)) =
-      select(in_flattened(in_range) != 0, in_range, -1);
-
-  auto num_non_zeros_fn = Func{};
-  num_non_zeros_fn() = cast<int32_t>(0);
-  num_non_zeros_fn() += select(in_flattened(in_range) != 0, 1, 0);
-
-  auto non_zeros_x = Func{};
-  auto non_zeros_y = Func{};
-  auto non_zeros_c = Func{};
-  auto non_zeros_n = Func{};
-
-  const auto flat_index = compacted_indices(x);
-  non_zeros_x(x) = select(flat_index != -1, flat_index % w, -1);
-  non_zeros_y(x) = select(flat_index != -1, (flat_index / w) % h, -1);
-  non_zeros_c(x) = select(flat_index != -1, (flat_index / (w * h)) % c, -1);
-  non_zeros_n(x) = select(flat_index != -1, flat_index / (w * h * c), -1);
-
-  Buffer<int32_t> sum = prefix_sum.realize(8);
   Buffer<int32_t> indices = compacted_indices.realize(size);
-  Buffer<int32_t> num_non_zeros = num_non_zeros_fn.realize();
   Buffer<int32_t> xs = non_zeros_x.realize(size);
   Buffer<int32_t> ys = non_zeros_y.realize(size);
   Buffer<int32_t> cs = non_zeros_c.realize(size);
   Buffer<int32_t> ns = non_zeros_n.realize(size);
-
-  SARA_CHECK(num_non_zeros());
-  print_1d(sum);
 
   std::cout << "Flat indices =" << std::endl;
   print_1d(indices);
---
+++ cpp/drafts/Halide/Generators/StreamCompactionGenerator.cpp
@@ -0,0 +1,106 @@
+// ========================================================================== //
+// This file is part of Sara, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2020-present David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+#include <drafts/Halide/Components/StreamCompaction.hpp>
+
+
+namespace {
+
+  using namespace Halide;
+  namespace halide = DO::Shakti::HalideBackend;
+
+  class StreamCompaction : public Generator<StreamCompaction>
+  {
+  public:
+    GeneratorParam<int> tile_x{"tile_x", 16};
+    GeneratorParam<int> tile_y{"tile_y", 16};
+
+    Input<Buffer<std::int8_t>> input{"input", 4};
+    Output<Buffer<std::int32_t>[4]> output{"output", 1};
+
+    Var x{"x"}, y{"y"}, c{"c"}, n{"n"};
+    Var xo{"xo"}, yo{"yo"}, co{"co"};
+    Var xi{"xi"}, yi{"yi"}, ci{"ci"};
+
+    void generate()
+    {
+      const auto w = input.dim(0).extent();
+      const auto h = input.dim(1).extent();
+      const auto c = input.dim(2).extent();
+      const auto n = input.dim(3).extent();
+      const auto size = w * h * c * n;
+
+      auto in_flattened = halide::flatten_4d(input, x, w, h, c);
+
+      // Express the prefix sum by a recurrence relation.
+      Func prefix_sum;
+      prefix_sum(x) = cast<int32_t>(0);
+
+      RDom range(1, size - 1);
+      prefix_sum(range) = select(in_flattened(range - 1) != 0,    //
+                                 prefix_sum(range - 1) + 1,  //
+                                 prefix_sum(range - 1));
+      prefix_sum.compute_root();
+
+      // Compacted indices.
+      RDom in_range(0, size);
+      Func compacted_indices;
+      compacted_indices(x) = -1;
+      compacted_indices(clamp(prefix_sum(in_range), 0, size - 1)) =
+          select(in_flattened(in_range) != 0, in_range, -1);
+      compacted_indices.compute_root();
+
+      auto& x_indices = output[0];
+      auto& y_indices = output[1];
+      auto& c_indices = output[2];
+      auto& n_indices = output[3];
+
+      const auto flat_index = compacted_indices(x);
+      x_indices(x) = select(flat_index != -1, flat_index % w, -1);
+      y_indices(x) = select(flat_index != -1, (flat_index / w) % h, -1);
+      c_indices(x) = select(flat_index != -1, (flat_index / (w * h)) % c, -1);
+      n_indices(x) = select(flat_index != -1, flat_index / (w * h * c), -1);
+    }
+
+    void schedule()
+    {
+      // GPU schedule.
+      if (get_target().has_gpu_feature())
+      {
+        output.gpu_tile(x, xo, xi, tile_x, TailStrategy::GuardWithIf);
+      }
+
+      // Hexagon schedule.
+      else if (get_target().features_any_of({Target::HVX_64, Target::HVX_128}))
+      {
+        const auto vector_size =
+            get_target().has_feature(Target::HVX_128) ? 128 : 64;
+
+        output.hexagon()
+            .split(x, xo, xi, 128)
+            .parallel(xo)
+            .vectorize(xi, vector_size, TailStrategy::GuardWithIf);
+      }
+
+      // CPU schedule.
+      else
+      {
+        output.split(x, xo, xi, 8)
+            .parallel(xo)
+            .vectorize(xi, 8, TailStrategy::GuardWithIf);
+      }
+    }
+  };
+
+}  // namespace
+
+
+HALIDE_REGISTER_GENERATOR(StreamCompaction, shakti_stream_compaction)
