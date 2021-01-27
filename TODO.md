155eba7f95a34d8ffb9d5761830c5f95d2a46ef4
David OK <david.ok8@gmail.com>
2021-01-27 19:54:38 +0000

    MAINT: remove obsolete code.

--- TODO.md
+++ TODO.md
@@ -1,1 +1,45 @@
-fix the fast marching with dealing with negative sign.
+Level Sets
+==========
+- fix the fast marching with dealing with negative sign.
+
+Bundle Adjustment Tests
+=======================
+
+Form the bundle adjustment problem.
+TODO:
+- Choose a reference camera.
+- Populate the list of cameras in BundleAdjustmentProblems:
+  As a reminder:
+  image #i -> camera #i -> BundleAdjustmentProblem.camera_parameters.row(i)
+- Initialize the absolute camera poses.
+
+Vanishing Point Detection
+=========================
+
+- Barnard's Gaussian sphere:
+  - interpretation line
+  - vp = pole, dual of point = equator
+  - n = (u1 x u2) x (v1 x v2)  (normalized coordinates).
+- 3-line RANSAC: Bazin
+- Rother approach: two nice distance functions:
+  - distance(line, line segment), distance(line segment, vp)
+
+Unsupervised Vanishing Point Detection and Camera Calibrationfrom a Single Manhattan Image with Radial Distortion
+=================================================================================================================
+- Plucker coordinates for 3D line computations.
+- LCC = line of circle center, bisector of the chord.
+- Circle fitting: second moment matrix, scattering ellipse
+- centroid of the chord determined from the scattering ellipse
+- angle between the LCC and the x-axis is determined.
+
+HALIDE
+======
+
+The Halide implementation of SIFT has been written with good confidence regarding the
+implementation details:
+
+Optimization
+------------
+- [ ] Minimize transfers back-and-forth between host memory and device memory.
+      - [ ] Provide API in which only we interact only with
+            `Halide::Runtime::Buffer<T>` objects.
--- cpp/benchmark/Geometry/benchmark_ellipse_interesections.cpp
+++ cpp/benchmark/Geometry/benchmark_ellipse_interesections.cpp
@@ -11,11 +11,10 @@
 
 
 using namespace std;
-using namespace DO;
+using namespace DO::Sara;
 
 void compareComputationTimes(const Ellipse& e1, const Ellipse& e2,
-                             int discretization,
-                             double& bestSpeedGain,
+                             int discretization, double& bestSpeedGain,
                              double& worstSpeedGain,
                              vector<double>& speedGainHistory)
 {
@@ -26,19 +25,20 @@ void compareComputationTimes(const Ellipse& e1, const Ellipse& e2,
   double approxRatio, analyticRatio;
   for (int i = 0; i < times; ++i)
     approxRatio = approxJaccardSimilarity(e1, e2, discretization);
-  double approximateTime = t.elapsedMs()/double(times);
+  double approximateTime = t.elapsedMs() / double(times);
 
   t.restart();
   for (int i = 0; i < times; ++i)
     analyticRatio = analyticJaccardSimilarity(e1, e2);
-  double closedFormTime = t.elapsedMs()/double(times);
+  double closedFormTime = t.elapsedMs() / double(times);
 
   double speedGain = approximateTime / closedFormTime;
   speedGainHistory.push_back(speedGain);
   std::sort(begin(speedGainHistory), end(speedGainHistory));
 
   double meanSpeedGain = 0;
-  meanSpeedGain = std::accumulate(begin(speedGainHistory), end(speedGainHistory), 0);
+  meanSpeedGain =
+      std::accumulate(begin(speedGainHistory), end(speedGainHistory), 0);
   meanSpeedGain /= speedGainHistory.size();
 
   if (speedGain > bestSpeedGain || speedGain < worstSpeedGain ||
@@ -48,12 +48,15 @@ void compareComputationTimes(const Ellipse& e1, const Ellipse& e2,
     bestSpeedGain = max(speedGain, bestSpeedGain);
 
     printStage("Update speed gain");
-    cout << "Approximate computation time = " << approximateTime << " ms" << endl;
-    cout << "Closed-form computation time = " << closedFormTime << " ms" << endl;
+    cout << "Approximate computation time = " << approximateTime << " ms"
+         << endl;
+    cout << "Closed-form computation time = " << closedFormTime << " ms"
+         << endl;
     cout << "Worst speed gain = " << worstSpeedGain << endl;
     cout << "Best speed gain = " << bestSpeedGain << endl;
     cout << "Mean speed gain = " << meanSpeedGain << endl;
-    cout << "[0.7]-ian speed gain = " << speedGainHistory[0.7*speedGainHistory.size()] << endl;
+    cout << "[0.7]-ian speed gain = "
+         << speedGainHistory[0.7 * speedGainHistory.size()] << endl;
   }
 }
 
@@ -72,7 +75,7 @@ void viewEllipses(const Ellipse& e1, const Ellipse& e2)
   for (int i = 0; i < numInter; ++i)
   {
     fillCircle(interPts[i].cast<float>(), 5.f, Green8);
-    drawString(interPts[i].x()+10, interPts[i].y()+10, toString(i), Green8);
+    drawString(interPts[i].x() + 10, interPts[i].y() + 10, toString(i), Green8);
     cout << "[" << i << "] " << interPts[i].transpose() << endl;
   }
 }
@@ -130,7 +133,8 @@ TEST(DO_SARA_Geometry_Test, ellipseAlgorithmsTest)
       auto insideEllipseIntersection = [&](const Point2d& p) {
         return inside(p, e1) && inside(p, e2);
       };
-      double pixelInterArea = sweepCountPixels(insideEllipseIntersection, false);
+      double pixelInterArea =
+          sweepCountPixels(insideEllipseIntersection, false);
       CHECK(exactInterArea);
       CHECK(approxInterArea);
       CHECK(pixelInterArea);
@@ -142,16 +146,19 @@ TEST(DO_SARA_Geometry_Test, ellipseAlgorithmsTest)
     // Experimental settings.
     bool compareSpeed = false;
     if (compareSpeed)
-      compareComputationTimes(e1, e2, discretization,
-                              bestSpeedGain, worstSpeedGain,
-                              speedGainHistory);
+      compareComputationTimes(e1, e2, discretization, bestSpeedGain,
+                              worstSpeedGain, speedGainHistory);
 
     // Summary
-    double jaccardRelError = abs(approxRatio-analyticRatio) / approxRatio;
-    double jaccardAbsError = abs(approxRatio-analyticRatio);
+    double jaccardRelError = abs(approxRatio - analyticRatio) / approxRatio;
+    double jaccardAbsError = abs(approxRatio - analyticRatio);
 
-    bool degen1 = min(e1.radius1(), e1.radius2())/max(e1.radius1(), e1.radius2()) < 1e-2;
-    bool degen2 = min(e2.radius1(), e2.radius2())/max(e2.radius1(), e2.radius2()) < 1e-2;
+    bool degen1 =
+        min(e1.radius1(), e1.radius2()) / max(e1.radius1(), e1.radius2()) <
+        1e-2;
+    bool degen2 =
+        min(e2.radius1(), e2.radius2()) / max(e2.radius1(), e2.radius2()) <
+        1e-2;
 
     if (jaccardAbsError > 1e-1 && (!degen1 && !degen2))
     {
@@ -169,7 +176,8 @@ TEST(DO_SARA_Geometry_Test, ellipseAlgorithmsTest)
       CHECK(inside(e2.center(), e1));
       CHECK(inside(e1.center(), e2));
       CHECK(analyticIntersection(e1, e2));
-      cout << "Approximate intersection-union area ratio = " << approxRatio << endl;
+      cout << "Approximate intersection-union area ratio = " << approxRatio
+           << endl;
       cout << "Exact intersection-union area ratio = " << analyticRatio << endl;
       cout << "Relative error = " << jaccardRelError << endl;
       cout << "Absolute error = " << jaccardAbsError << endl;
@@ -177,7 +185,7 @@ TEST(DO_SARA_Geometry_Test, ellipseAlgorithmsTest)
     }
 
     ++attempts;
-  } while(true);
+  } while (true);
 }
 
 int main(int argc, char** argv)
