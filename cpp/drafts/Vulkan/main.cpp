62bc6ffee5a7197c5bff5cbd12a3d6479481d2d3
David OK <david.ok8@gmail.com>
2021-01-31 23:16:24 +0000

    DOC: add comments as a mental model to understand Vulkan.

--- cpp/drafts/Vulkan/main.cpp
+++ cpp/drafts/Vulkan/main.cpp
@@ -1013,44 +1013,72 @@ private:
   //
   auto draw_frame() -> void
   {
+    // The GPU is blocking the CPU at this point of the road:
+    // - The CPU is stopped at a fence
+    // - The first fence is closed
     vkWaitForFences(_device, 1, &_in_flight_fences[current_frame], VK_TRUE,
                     UINT64_MAX);
+    // - The first fence has opened just now.
 
+    // The CPU pursues its journey:
+    // - it acquires the next image for rendering.
     auto image_index = std::uint32_t{};
     vkAcquireNextImageKHR(_device, _swapchain, UINT64_MAX,
                           _image_available_semaphores[current_frame],
                           VK_NULL_HANDLE, &image_index);
 
+    // The CPU encounters another fence controlled by the GPU:
+    // - The CPU is waiting from the GPU until the image becomes available for
+    //   rendering.
+    // - The second fence displays a red light
+    // - When rendering for the first time, the second fence is already opened,
+    //   so the CPU can pursue its journey.
     if (_images_in_flight[image_index] != VK_NULL_HANDLE)
       vkWaitForFences(_device, 1, &_images_in_flight[current_frame], VK_TRUE,
                       UINT64_MAX);
     _images_in_flight[image_index] = _in_flight_fences[current_frame];
+    // - The second fence has opened just now.
 
+    // The CPU pursues its journey:
+    //
+    // - The CPU will now detail what to do at the drawing stage.
     auto submit_info = VkSubmitInfo{};
     submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
 
     VkPipelineStageFlags wait_stages =
         VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+    // - The CPU tells the GPU to ensure that it starts drawing only when the
+    // image becomes available.
     submit_info.waitSemaphoreCount = 1;
     submit_info.pWaitSemaphores = &_image_available_semaphores[current_frame];
     submit_info.pWaitDstStageMask = &wait_stages;
 
+    // - The CPU tells the GPU to ensure that it notifies when the drawing is
+    //   finished and thus ready to present onto the screen.
     submit_info.signalSemaphoreCount = 1;
     submit_info.pSignalSemaphores = &_render_finished_semaphores[current_frame];
 
     submit_info.commandBufferCount = 1;
     submit_info.pCommandBuffers = &_command_buffers[image_index];
 
+    // - The CPU now tells the GPU to make the first fence display a red light.
     vkResetFences(_device, 1, &_in_flight_fences[current_frame]);
 
+    // - The CPU submits a drawing command to the GPU (on the graphics queue).
     if (vkQueueSubmit(_graphics_queue, 1, &submit_info,
                       _in_flight_fences[current_frame]) != VK_SUCCESS)
       throw std::runtime_error{"Failed to submit draw command buffer!"};
 
+    // - The CPU details the screen presentation command.
     auto present_info = VkPresentInfoKHR{};
     {
       present_info.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
 
+      // - The CPU tells the GPU to trigger the frame display only when the
+      //   rendering is finished.
+      //
+      //   This completely specifies the dependency between the rendering
+      //   command and the screen display command.
       present_info.waitSemaphoreCount = 1;
       present_info.pWaitSemaphores =
           &_render_finished_semaphores[current_frame];
@@ -1061,8 +1089,10 @@ private:
       present_info.pImageIndices = &image_index;
     }
 
+    // - The CPU submits another command to the GPU (on the present queue).
     vkQueuePresentKHR(_present_queue, &present_info);
 
+    // Move to the next framebuffer.
     current_frame = (current_frame + 1) % MAX_FRAMES_IN_FLIGHT;
   }
 
