bcb69ab6ee13658bbb5b2a1d7875aff84b7bb5e9
David OK <david.ok8@gmail.com>
2021-07-18 14:04:27 +0100

    MAINT: add some more benchmarking.

--- python/do/sara/benchmark/image_processing.py
+++ python/do/sara/benchmark/image_processing.py
@@ -21,28 +21,53 @@ def user_main():
     video_frame_gray8 = np.empty(video_stream.sizes()[:2], dtype=np.uint8)
     video_frame_gray32f = np.empty(video_stream.sizes()[:2], dtype=np.float32)
     video_frame_convolved = np.empty(video_stream.sizes()[:2], dtype=np.float32)
+    gradx = np.empty(video_stream.sizes()[:2], dtype=np.float32)
+    grady = np.empty(video_stream.sizes()[:2], dtype=np.float32)
+    mag = np.empty(video_stream.sizes()[:2], dtype=np.float32)
+    ori = np.empty(video_stream.sizes()[:2], dtype=np.float32)
 
     ed = sara.EdgeDetector()
 
     sigma = 1.6
     gauss_trunc = 4
 
+    # Benchmarking on 4K video against Vanilla OpenCV.
+    # Should compare with OpenCV CUDA implementation.
+
     while video_stream.read(video_frame):
+        # Shakti wins.
         with sara.Timer("[SHAKTI] rgb8 to gray32f"):
             shakti.convert_rgb8_to_gray32f_cpu(video_frame, video_frame_gray8)
-
         with sara.Timer("[OPENCV] rgb8 to gray32f"):
             cv2.cvtColor(video_frame, cv2.COLOR_RGB2GRAY, video_frame_gray8)
             video_frame_gray32f = video_frame_gray8.astype(np.float32) / 255
-        print()
 
+        # On CUDA-architecture: Shakti by almost a factor 2.
+        # On Macbook Air: OpenCV, CPU-GPU transfer may be the bottleneck.
         with sara.Timer("[SHAKTI] gaussian convolution"):
             shakti.gaussian_convolution(video_frame_gray32f,
                                         video_frame_convolved, sigma, 4)
-
         with sara.Timer("[OPENCV] gaussian convolution"):
             cv2.GaussianBlur(video_frame_gray32f, None, sigma,
                              video_frame_convolved)
+        print()
+
+        # OpenCV wins with a small edge but it also does more arithmetic
+        # operations because of Sobel. So the comparison was not very fair.
+        with sara.Timer("[SHAKTI] gradient"):
+            shakti.gradient_2d_32f(video_frame_gray32f, gradx, grady)
+        with sara.Timer("[OPENCV] (sobel) gradient"):
+            cv2.Sobel(video_frame_gray32f, cv2.CV_32F, 1, 0, gradx)
+            cv2.Sobel(video_frame_gray32f, cv2.CV_32F, 0, 1, grady)
+        print()
+
+        # Shakti wins on CUDA-architecture by almost a factor 2.
+        with sara.Timer("[SHAKTI] polar gradient"):
+            shakti.polar_gradient_2d_32f(video_frame_gray32f, mag, ori)
+        with sara.Timer("[OPENCV] polar gradient"):
+            cv2.Sobel(video_frame_gray32f, cv2.CV_32F, 1, 0, gradx)
+            cv2.Sobel(video_frame_gray32f, cv2.CV_32F, 0, 1, grady)
+            cv2.cartToPolar(gradx, grady, mag, ori)
         print()
 
 if __name__ == '__main__':
