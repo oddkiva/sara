8c97e3268bdbfb01f83b2bde9351fd6bebea6bfe
David OK <david.ok8@gmail.com>
2021-07-22 09:26:58 +0100

    PERF: compare video streaming performance with OpenCV.
    Some way to go...

--- python/do/shakti/examples/image_processing.py
+++ python/do/shakti/examples/image_processing.py
@@ -7,19 +7,24 @@ import numpy as np
 import do.sara as sara
 import do.shakti as shakti
 
-import cv2
+USE_OPENCV = True
+
+if USE_OPENCV:
+    import cv2
 
 
 def user_main():
     video_file = sys.argv[1]
-    video_stream = sara.VideoStream()
-    video_stream.open(video_file)
-    # video_stream = cv2.VideoCapture(video_file)
-    # w = int(video_stream.get(cv2.CAP_PROP_FRAME_WIDTH))
-    # h = int(video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    sizes = video_stream.sizes()
-    h, w, _ = sizes
+    if USE_OPENCV:
+        video_stream = cv2.VideoCapture(video_file)
+        w = int(video_stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = int(video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        sizes = (h, w, 3)
+    else:
+        video_stream = sara.VideoStream()
+        video_stream.open(video_file)
+        sizes = video_stream.sizes()
+        h, w, _ = sizes
 
     video_frame = np.empty(sizes, dtype=np.uint8)
     video_frame_gray8 = np.empty(sizes[:2], dtype=np.uint8)
@@ -33,17 +38,14 @@ def user_main():
     sigma = 1.6
     gauss_trunc = 4
 
-    try:
-        shakti_cuda_gaussian_filter = shakti.CudaGaussianFilter(sigma, gauss_trunc)
-    except:
-        return
-
     sara.create_window(w, h)
 
     i = 0
     while True:
         with sara.Timer("Read"):
-            if not video_stream.read(video_frame):
+            if USE_OPENCV:
+                ret, _ = video_stream.read(video_frame)
+            elif not video_stream.read(video_frame):
                 break
 
         if i % 2 == 1:
@@ -51,8 +53,9 @@ def user_main():
 
         with sara.Timer("Processing"):
             shakti.convert_rgb8_to_gray32f_cpu(video_frame, video_frame_gray32f)
-            shakti_cuda_gaussian_filter.apply(video_frame_gray32f,
-                                              video_frame_convolved);
+            shakti.gaussian_convolution(video_frame_gray32f,
+                                        video_frame_convolved,
+                                        sigma, gauss_trunc, False);
             shakti.polar_gradient_2d_32f(video_frame_convolved, mag, ori)
 
             mag = (mag + 1) * 0.5
