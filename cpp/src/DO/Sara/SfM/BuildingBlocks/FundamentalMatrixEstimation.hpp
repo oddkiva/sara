0e54a6c7b834c197f33267500539e1558b8293a1
David OK <david.ok8@gmail.com>
2021-08-08 01:15:59 +0100

    MAINT: fix linker errors.
    It turns out the DO_Sara_SfM is always compiled as a static library. Figure out why.

--- cpp/src/DO/Sara/SfM/BuildingBlocks/FundamentalMatrixEstimation.hpp
+++ cpp/src/DO/Sara/SfM/BuildingBlocks/FundamentalMatrixEstimation.hpp
@@ -22,20 +22,17 @@ namespace DO::Sara {
 
   //! @{
   //! @brief Fundamental matrix estimation.
-  DO_SARA_EXPORT
   auto estimate_fundamental_matrix(const std::vector<Match>& Mij,
                                    const KeypointList<OERegion, float>& ki,
                                    const KeypointList<OERegion, float>& kj,
                                    int num_samples, double err_thres)
       -> std::tuple<FundamentalMatrix, Tensor_<bool, 1>, Tensor_<int, 1>>;
 
-  DO_SARA_EXPORT
   auto estimate_fundamental_matrices(const std::string& dirpath,
                                      const std::string& h5_filepath,
                                      bool overwrite, bool debug,
                                      bool wait_key = false) -> void;
 
-  DO_SARA_EXPORT
   auto check_epipolar_constraints(const Image<Rgb8>& Ii, const Image<Rgb8>& Ij,
                                   const FundamentalMatrix& F,
                                   const std::vector<Match>& Mij,
@@ -44,7 +41,6 @@ namespace DO::Sara {
                                   int display_step, bool wait_key = true)
       -> void;
 
-  DO_SARA_EXPORT
   auto inspect_fundamental_matrices(const std::string& dirpath,
                                     const std::string& h5_filepath,
                                     int display_step, bool wait_key) -> void;
