f3216441caa29fa1f729470fb05a604d10b05ab5
David OK <david.ok8@gmail.com>
2021-08-09 13:28:39 +0100

    MAINT: save work.

--- cpp/examples/Sara/MultiViewGeometry/essential_matrix_estimation_example.cpp
+++ cpp/examples/Sara/MultiViewGeometry/essential_matrix_estimation_example.cpp
@@ -251,9 +251,23 @@ int __main(int, char** argv)
   SARA_DEBUG << "Angle = " << axis_angle.angle() * 180 / M_PI << " deg"
              << std::endl;
 
+  // Rotation matrix to change from computer vision camera axis-convention to
+  // automotive axis convention.
+  //
+  // clang-format off
+  const auto P = (Eigen::Matrix3d{} <<
+     0,  0, 1,
+    -1,  0, 0,
+     0, -1, 0
+  ).finished();
+  // clang-format on
+
+  const Eigen::Matrix3d Rw1 = Rw * P;
+  const Eigen::Vector3d tw1 = P * tw;
+  SARA_DEBUG << "Rw1 =\n" << Rw1 << std::endl;
+  SARA_DEBUG << "tw1 =\n" << tw1 << std::endl;
   SARA_DEBUG << "[BUGGY] yaw pitch roll =\n"
-             << Rw.eulerAngles(1, 0, 2) * 180. / M_PI << std::endl;
-  // << calculate_yaw_pitch_roll(Rw) * 180. / M_PI << std::endl;
+             << Rw1.eulerAngles(2, 1, 0) * 180. / M_PI << std::endl;
 
   get_key();
 
