4bd93c006a1331b43f43397991487d6bbf081cb9
David OK <david.ok8@gmail.com>
2021-08-11 14:34:56 +0100

    MAINT: fix code coverage.

--- cmake/sara_configure_code_coverage.cmake
+++ cmake/sara_configure_code_coverage.cmake
@@ -11,14 +11,26 @@ if (CMAKE_COMPILER_IS_GNUCXX)
                            --base-directory=${DO_Sara_SOURCE_DIR}
                            --capture
                            --output-file=coverage.info
+      # Remove System libraries.
       COMMAND ${LCOV_PATH} --remove coverage.info '/usr/*'
                            --output-file coverage.info
+      # Remove tmp files.
+      COMMAND ${LCOV_PATH}
+              --remove coverage.info '/tmp/*'
+              --output-file coverage.info
+
+      # Third-party software.
       COMMAND ${LCOV_PATH}
               --remove coverage.info '${DO_Sara_DIR}/cpp/third-party/*'
               --output-file coverage.info
       COMMAND ${LCOV_PATH}
               --remove coverage.info '${DO_Sara_DIR}/cpp/test/*'
               --output-file coverage.info
+      COMMAND ${LCOV_PATH}
+              --remove coverage.info '/home/david/opt/*'
+              --output-file coverage.info
+
+      # Ignore additional files generated in the binary folder.
       COMMAND ${LCOV_PATH}
               --remove coverage.info '${CMAKE_BINARY_DIR}/*'
               --output-file coverage.info
@@ -31,7 +43,13 @@ if (CMAKE_COMPILER_IS_GNUCXX)
               --remove coverage.info '*/numpy/*'
               --output-file coverage.info
 
-      # Remove tests in the drafts folder.
+      # Remove tests in the peculiar folders.
+      COMMAND ${LCOV_PATH}
+              --remove coverage.info '${DO_Sara_DIR}/cpp/src/DO/Shakti/Halide/Generators/test/*'
+              --output-file coverage.info
+      COMMAND ${LCOV_PATH}
+              --remove coverage.info '${DO_Sara_DIR}/cpp/src/DO/Shakti/Halide/Components/test/*'
+              --output-file coverage.info
       COMMAND ${LCOV_PATH}
               --remove coverage.info '${DO_Sara_DIR}/cpp/drafts/*/test*'
               --output-file coverage.info
