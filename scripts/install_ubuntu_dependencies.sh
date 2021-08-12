39d1c9acde0f30398b996bc7bf13403ef07091a4
David OK <david.ok8@gmail.com>
2021-08-12 20:25:44 +0100

    MAINT: fix doc generation.

--- scripts/install_ubuntu_dependencies.sh
+++ scripts/install_ubuntu_dependencies.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 set -ex
 
-sudo apt-get update -y -qq
+# Update the packages list.
+apt-get update -y -qq
 
-# # Install the latest CMake please.
-# sudo apt-get install -y -qq \
-#   apt-transport-https \
-#   ca-certificates \
-#   gnupg \
-#   software-properties-common \
-#   wget
-# wget -O \
-#   - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null \
-#   | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-# sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+# Install the preliminary packages.
+apt-get install -y -qq \
+  apt-transport-https \
+  ca-certificates \
+  gnupg \
+  software-properties-common \
+  wget
+
+# Latest CMake.
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | \
+  gpg --dearmor - | \
+  tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+# Vulkan SDK.
+wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+wget -qO /etc/apt/sources.list.d/lunarg-vulkan-focal.list http://packages.lunarg.com/vulkan/lunarg-vulkan-focal.list
+# CLBlast.
+add-apt-repository ppa:cnugteren/clblast
+
+# Update the packages list again.
+apt-get update -y -qq
 
 # All the packages to compile the C++ codebase.
 apt_packages=(
@@ -24,8 +35,22 @@ apt_packages=(
   "doxygen"
   "git"
   "graphviz"
+  "gstreamer1.0-plugins-base"
+  "gstreamer1.0-plugins-good"
+  "gstreamer1.0-plugins-bad"
+  "gstreamer1.0-plugins-ugly"
+  "gstreamer1.0-libav"
+  "gstreamer1.0-doc"
+  "gstreamer1.0-tools"
+  "gstreamer1.0-x"
+  "gstreamer1.0-alsa"
+  "gstreamer1.0-gl"
+  "gstreamer1.0-gtk3"
+  "gstreamer1.0-qt5"
+  "gstreamer1.0-pulseaudio"
   "lcov"
   "libboost-all-dev"
+  "libgstreamer1.0-0"
   "libhdf5-dev"
   "libjpeg-dev"
   "libpng-dev"
@@ -37,12 +62,17 @@ apt_packages=(
   "libglew-dev"
   "libglfw3-dev"
   "libceres-dev"
+  "libpocl-dev"
   "libz3-4"  # For swiftc
+  "ocl-icd-opencl-dev"
+  "opencl-headers"
   "python3-dev"
   "qtbase5-dev"
+  "rubygems"
+  "vulkan-sdk"
 )
 apt_packages_str=$(printf "%s " "${apt_packages[@]}")
-sudo apt-get install -y -qq ${apt_packages_str}
+apt-get install -y -qq ${apt_packages_str}
 
 # Build CLBlast.
 ./scripts/install_clblast.sh
