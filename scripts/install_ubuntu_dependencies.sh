25c52ae7f577eda51596ebd77453502044da116d
David OK <david.ok8@gmail.com>
2021-08-13 16:17:33 +0100

    MAINT: update the list of packages to install.

--- scripts/install_ubuntu_dependencies.sh
+++ scripts/install_ubuntu_dependencies.sh
@@ -12,15 +12,15 @@ apt-get install -y -qq \
   software-properties-common \
   wget
 
-# Latest CMake.
+# CMake APT repository.
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | \
   gpg --dearmor - | \
   tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
-# Vulkan SDK.
-wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+# Vulkan SDK APT repository.
+wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
 wget -qO /etc/apt/sources.list.d/lunarg-vulkan-focal.list http://packages.lunarg.com/vulkan/lunarg-vulkan-focal.list
-# CLBlast.
+# CLBlast APT repository.
 add-apt-repository ppa:cnugteren/clblast
 
 # Update the packages list again.
@@ -50,6 +50,7 @@ apt_packages=(
   "gstreamer1.0-pulseaudio"
   "lcov"
   "libboost-all-dev"
+  "libclblast-dev"
   "libgstreamer1.0-0"
   "libhdf5-dev"
   "libjpeg-dev"
@@ -72,7 +73,6 @@ apt_packages=(
   "vulkan-sdk"
 )
 apt_packages_str=$(printf "%s " "${apt_packages[@]}")
-apt-get install -y -qq ${apt_packages_str}
 
-# Build CLBlast.
-./scripts/install_clblast.sh
+# Install all packages in a non-interactive way.
+apt-get install -y ${apt_packages_str}
