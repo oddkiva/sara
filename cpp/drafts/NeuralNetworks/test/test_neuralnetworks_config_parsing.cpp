c9c01aeee05d9d6b3eab87741f2239f702ecb415
David OK <david.ok8@gmail.com>
2021-11-02 23:02:49 +0000

    MAINT: save work.

---
+++ cpp/drafts/NeuralNetworks/test/test_neuralnetworks_config_parsing.cpp
@@ -0,0 +1,112 @@
+// ========================================================================== //
+// This file is part of Sara, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2013-2016 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+#define BOOST_TEST_MODULE "NeuralNetworks/Yolo Configuration Parsing"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+
+auto read_line(std::ifstream& file, std::string& line)
+{
+  if (!std::getline(file, line))
+    return false;
+  line = boost::algorithm::trim_copy(line);
+  return true;
+}
+
+auto is_section(const std::string& line)
+{
+  return line.front() == '[';
+}
+
+auto is_comment(const std::string& line)
+{
+  return line.front() == '#';
+}
+
+auto section_name(const std::string& line)
+{
+  auto line_trimmed = line;
+  boost::algorithm::trim_if(
+      line_trimmed, [](const auto ch) { return ch == '[' or ch == ']'; });
+  return line_trimmed;
+}
+
+
+auto parse_option(const std::string& line)
+{
+  auto line_split = std::vector<std::string>{};
+  boost::split(line_split, line, boost::is_any_of("= "),
+               boost::token_compress_on);
+  return line_split;
+}
+
+
+BOOST_AUTO_TEST_SUITE(TestLayers)
+
+BOOST_AUTO_TEST_CASE(test_yolov4_tiny_config_parsing)
+{
+  namespace fs = std::filesystem;
+
+  const auto filepath =                            //
+      fs::path{"/home/david/GitLab/DO-CV/sara"} /  //
+      "data/trained_models/" /                     //
+      "yolov4-tiny.cfg";
+  BOOST_CHECK(fs::exists(filepath));
+
+  auto file = std::ifstream{filepath.string()};
+  BOOST_CHECK(file.is_open());
+
+  auto line = std::string{};
+
+  auto in_current_section = false;
+  auto enter_new_section = false;
+  while (read_line(file, line))
+  {
+    if (line.empty())
+      continue;
+
+    if (is_comment(line))
+      continue;
+
+    // Enter a new section.
+    if (is_section(line))
+    {
+      std::cout << "ENTERING NEW SECTION: ";
+      std::cout << section_name(line) << std::endl;
+
+      enter_new_section = true;
+      in_current_section = false;
+      continue;
+    }
+
+    if (enter_new_section)
+    {
+      in_current_section = true;
+      enter_new_section = false;
+    }
+
+    if (in_current_section)
+    {
+      const auto line_split = parse_option(line);
+      for (const auto& str : line_split)
+        std::cout << str << ";";
+      std::cout << std::endl;
+    }
+  }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
