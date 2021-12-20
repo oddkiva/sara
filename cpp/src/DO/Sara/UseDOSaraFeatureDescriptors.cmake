6edc37ce31ce1e04ecc1cb4141cbfa233e85bb04
David OK <david.ok8@gmail.com>
2021-12-20 00:53:14 +0000

    MAINT: fix CMake script.

--- cpp/src/DO/Sara/UseDOSaraFeatureDescriptors.cmake
+++ cpp/src/DO/Sara/UseDOSaraFeatureDescriptors.cmake
@@ -1,14 +1,15 @@
-if (SARA_USE_FROM_SOURCE)
-  get_property(DO_Sara_FeatureDescriptors_ADDED GLOBAL PROPERTY
-               _DO_Sara_FeatureDescriptors_INCLUDED)
-  if (NOT DO_Sara_FeatureDescriptors_ADDED)
+if(SARA_USE_FROM_SOURCE)
+  get_property(DO_Sara_FeatureDescriptors_ADDED GLOBAL
+               PROPERTY _DO_Sara_FeatureDescriptors_INCLUDED)
+  if(NOT DO_Sara_FeatureDescriptors_ADDED)
     sara_glob_directory(${DO_Sara_SOURCE_DIR}/FeatureDescriptors)
     sara_create_common_variables("FeatureDescriptors")
     sara_generate_library("FeatureDescriptors")
 
-    target_link_libraries(DO_Sara_FeatureDescriptors
-      PUBLIC
-      DO::Sara::Features
-      $<$<BOOL:OpenMP_CXX_FOUND>:OpenMP::OpenMP_CXX>)
-  endif ()
-endif ()
+    target_link_libraries(
+      DO_Sara_FeatureDescriptors
+      PUBLIC DO::Sara::Features #
+             DO::Sara::ImageProcessing
+             $<$<BOOL:OpenMP_CXX_FOUND>:OpenMP::OpenMP_CXX>)
+  endif()
+endif()
