181f0e57712d243d6bf0ce850192b27038a24625
David OK <david.ok8@gmail.com>
2022-01-16 17:38:17 +0000

    ENH: implement a method that computes a full Gaussian octave.

---
+++ cpp/src/DO/Shakti/Cuda/FeatureDetectors/examples/example_gaussian_pyramid.cu
@@ -0,0 +1,227 @@
+// ========================================================================== //
+// This file is part of DO-CV, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2015 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
+//! @example
+
+#include <DO/Sara/Core.hpp>
+#include <DO/Sara/Graphics.hpp>
+#include <DO/Sara/ImageProcessing/FastColorConversion.hpp>
+#include <DO/Sara/ImageProcessing/LinearFiltering.hpp>
+#include <DO/Sara/VideoIO.hpp>
+
+#include <DO/Shakti/Cuda/FeatureDetectors/Octave.hpp>
+#include <DO/Shakti/Cuda/FeatureDetectors/TunedConvolutions/SmallGaussianConvolutionFP32.hpp>
+#include <DO/Shakti/Cuda/MultiArray.hpp>
+#include <DO/Shakti/Cuda/MultiArray/ManagedMemoryAllocator.hpp>
+#include <DO/Shakti/Cuda/Utilities/DeviceInfo.hpp>
+#include <DO/Shakti/Cuda/Utilities/Timer.hpp>
+
+
+namespace sara = DO::Sara;
+namespace shakti = DO::Shakti;
+namespace sc = shakti::Cuda;
+namespace scg = sc::Gaussian;
+
+
+auto example_1() -> void
+{
+  auto devices = shakti::get_devices();
+  auto& device = devices.back();
+  device.make_current_device();
+
+  constexpr auto video_filepath =
+#ifdef _WIN32
+      "C:/Users/David/Desktop/GOPR0542.MP4"
+#else
+      "/home/david/Desktop/Datasets/sfm/oddkiva/bali-excursion.mp4"
+  // "/home/david/Desktop/Datasets/sfm/Family.mp4"
+#endif
+      ;
+  std::cout << video_filepath << std::endl;
+  sara::VideoStream video_stream{video_filepath};
+  const auto w = video_stream.width();
+  const auto h = video_stream.height();
+
+  auto frame_index = int{-1};
+  auto frame = video_stream.frame();
+  // Use pinned memory, it's much much faster.
+  auto frame_gray32f =
+      sara::Image<float, 2, shakti::PinnedMemoryAllocator>{w, h};
+  auto frame_blurred =
+      sara::Image<float, 2, shakti::PinnedMemoryAllocator>{w, h};
+
+  static constexpr auto scale_count = 4;
+  const auto host_kernels = sc::GaussianOctaveKernels<float>{scale_count};
+  auto device_kernels = scg::DeviceGaussianFilterBank{host_kernels};
+  device_kernels.copy_filters_to_device_constant_memory();
+  device_kernels.peek_filters_in_device_constant_memory();
+
+
+  auto d_in = shakti::MultiArray<float, 2, shakti::RowMajorStrides>{
+      frame_gray32f.data(), {w, h}};
+  auto d_convx = shakti::MultiArray<float, 2>{{w, h}};
+  auto d_convy = shakti::MultiArray<float, 2>{{w, h}};
+
+  auto d_timer = shakti::Timer{};
+
+  // Display.
+  sara::create_window(video_stream.sizes());
+  while (video_stream.read())
+  {
+    ++frame_index;
+    std::cout << "[Read frame] " << frame_index << "" << std::endl;
+
+    sara::tic();
+    sara::from_rgb8_to_gray32f(frame, frame_gray32f);
+    sara::toc("Grayscale");
+
+    shakti::tic(d_timer);
+    d_in.copy_from_host(frame_gray32f.data(), w, h);
+    shakti::toc(d_timer, "Host to Device");
+
+    for (auto kernel_index = 0u; kernel_index < host_kernels.sigmas.size();
+         ++kernel_index)
+    {
+      shakti::tic(d_timer);
+      device_kernels(d_in, d_convx, d_convy, kernel_index);
+      shakti::toc(d_timer, "GF");
+
+      shakti::tic(d_timer);
+      d_convy.copy_to_host(frame_blurred.data());
+      shakti::toc(d_timer, "Device To Host");
+
+#define CHECK_WITH_REFERENCE_IMPL
+#ifdef CHECK_WITH_REFERENCE_IMPL
+      const auto gt =
+          sara::gaussian(frame_gray32f, host_kernels.sigmas[kernel_index]);
+      SARA_CHECK(kernel_index);
+      SARA_DEBUG
+          << "L-inf norm = "
+          << (gt.matrix() - frame_blurred.matrix()).lpNorm<Eigen::Infinity>()
+          << std::endl;
+      SARA_DEBUG << "L2 norm = "
+                 << (gt.matrix() - frame_blurred.matrix()).norm() << std::endl;
+      SARA_DEBUG << "Relative error = "
+                 << (gt.matrix() - frame_blurred.matrix()).norm() /
+                        gt.matrix().norm()
+                 << std::endl;
+
+      // TODO: double-check again...
+      // L-inf norm = 0.000273228
+      // L2 norm = 0.154609
+      // Relative error = 0.000268316
+    }
+
+    sara::tic();
+    sara::display(frame_blurred);
+    sara::toc("Display");
+
+
+    // device_kernels.peek_filters_in_device_constant_memory();
+#endif
+  }
+}
+
+auto example_2() -> void
+{
+  auto devices = shakti::get_devices();
+  auto& device = devices.back();
+  device.make_current_device();
+
+  constexpr auto video_filepath =
+#ifdef _WIN32
+      "C:/Users/David/Desktop/GOPR0542.MP4"
+#else
+        "/home/david/Desktop/Datasets/sfm/oddkiva/bali-excursion.mp4"
+        // "/home/david/Desktop/Datasets/sfm/Family.mp4"
+#endif
+      ;
+  std::cout << video_filepath << std::endl;
+  sara::VideoStream video_stream{video_filepath};
+  const auto w = video_stream.width();
+  const auto h = video_stream.height();
+
+  // Use pinned memory, it's much much faster.
+  auto frame_gray32f =
+      sara::Image<float, 2, shakti::PinnedMemoryAllocator>{w, h};
+  auto frame_blurred =
+      sara::Image<float, 2, shakti::PinnedMemoryAllocator>{w, h};
+
+  static constexpr auto scale_count = 4;
+  const auto host_kernels = sc::GaussianOctaveKernels<float>{scale_count};
+  auto device_kernels = scg::DeviceGaussianFilterBank{host_kernels};
+  device_kernels.copy_filters_to_device_constant_memory();
+  device_kernels.peek_filters_in_device_constant_memory();
+
+  // Host and device input grayscale data.
+  auto& h_in = frame_gray32f;
+  auto d_in = shakti::MultiArray<float, 2, shakti::RowMajorStrides>{
+      frame_gray32f.data(), {w, h}};
+
+  // Work array.
+  auto d_convx = shakti::MultiArray<float, 2>{{w, h}};
+
+  // Initialize the octave CUDA surface.
+  auto d_octave = sc::make_gaussian_octave<float>(w, h, scale_count);
+  d_octave.init_surface();
+  auto h_octave = sara::Image<float, 3, shakti::PinnedMemoryAllocator>{
+      w, h, d_octave.scale_count()};
+
+  // Profile.
+  auto d_timer = shakti::Timer{};
+
+  // Display.
+  sara::create_window(video_stream.sizes());
+  auto frame_index = int{-1};
+  auto frame = video_stream.frame();
+  while (video_stream.read())
+  {
+    ++frame_index;
+    std::cout << "[Read frame] " << frame_index << "" << std::endl;
+
+    sara::tic();
+    sara::from_rgb8_to_gray32f(frame, frame_gray32f);
+    sara::toc("Grayscale");
+
+    shakti::tic(d_timer);
+    d_in.copy_from_host(h_in.data(), w, h);
+    shakti::toc(d_timer, "Transfer to Device");
+
+    shakti::tic(d_timer);
+    device_kernels(d_in, d_convx, d_octave);
+    shakti::toc(d_timer, "Gaussian Octave");
+
+    // This is an extremely expensive copy.
+    shakti::tic(d_timer);
+    d_octave.array().copy_to(h_octave);
+    shakti::toc(d_timer, "Device to Host");
+
+#ifdef INSPECT_ALL
+    for (auto s = 0; s < d_octave.scale_count(); ++s)
+#else
+    const auto s = d_octave.scale_count() - 1;
+#endif
+    {
+      const auto layer_s = sara::image_view(sara::tensor_view(h_octave)[s]);
+
+      sara::tic();
+      sara::display(layer_s);
+      sara::toc("Display");
+    }
+  }
+}
+
+
+GRAPHICS_MAIN()
+{
+  example_2();
+  return 0;
+}
