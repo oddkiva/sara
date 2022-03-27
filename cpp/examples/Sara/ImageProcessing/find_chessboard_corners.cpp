205e722cdef0007b5b78ae476a18f076563e0c6c
David OK <david.ok8@gmail.com>
2022-03-28 00:15:20 +0100

    MAINT: save work.

--- cpp/examples/Sara/ImageProcessing/find_chessboard_corners.cpp
+++ cpp/examples/Sara/ImageProcessing/find_chessboard_corners.cpp
@@ -12,186 +12,138 @@
 //! @example
 
 #include <DO/Sara/Core/TicToc.hpp>
-#include <DO/Sara/FeatureDetectors/EdgeDetector.hpp>
+#include <DO/Sara/FeatureDetectors.hpp>
 #include <DO/Sara/Graphics.hpp>
 #include <DO/Sara/ImageIO.hpp>
 #include <DO/Sara/ImageProcessing.hpp>
+#include <DO/Sara/VideoIO.hpp>
+#include <DO/Sara/Visualization.hpp>
 
-#include "Chessboard/SaddleDetection.hpp"
+#include "Chessboard/SaddlePointDetection.hpp"
 
 
 namespace sara = DO::Sara;
 
 
-namespace DO::Sara {
-
-  auto weight_mask(const std::vector<int>& radius)
-  {
-    std::unordered_map<int, Image<float>> masks;
-    static constexpr auto ring_radius = 0.3f;
-    static constexpr auto dist_min = 1 - ring_radius;
-    static constexpr auto dist_max = 1 + ring_radius;
-    static constexpr auto weight_typical = 0.6f;
-
-    for (const auto& r : radius)
-    {
-      auto& mask = masks[r];
-      mask.resize(2 * r + 1, 2 * r + 1);
-      for (auto v = 0; v < mask.height(); ++v)
-      {
-        for (auto u = 0; u < mask.width(); ++u)
-        {
-          // Calculate the normalized distance.
-          const auto dist =
-              (Eigen::Vector2f(u, v) - r * Eigen::Vector2f::Ones()).norm() / r;
-          const auto clamped_dist = std::clamp(dist, dist_min, dist_max);
-          // Linear function.
-          mask(u, v) = (dist_max - clamped_dist) / weight_typical;
-        }
-      }
-    }
-    return masks;
-  }
-
-  auto filter_saddle_points(const ImageView<float>& image,
-                            const ImageView<float>& gradient_ori,
-                            const ImageView<float>& gradient_mag,
-                            const std::vector<SaddlePoint>& saddle_points)
-  {
-    static constexpr auto num_circle_points = 32;
-    static constexpr auto num_bins = 32;
-    static constexpr auto crossing_threshold = 3;
-    static constexpr auto need_crossing = 4;
-    static constexpr auto need_mode = 2;
-
-    static constexpr auto radius = 5.;
-
-    auto ori_vectors = std::array<Eigen::Vector2d, num_circle_points>{};
-    for (auto i = 0; i < num_circle_points; ++i)
-    {
-      static const auto pi = static_cast<double>(M_PI);
-      const auto angle = i * 2 * pi / (num_circle_points - 1);
-      ori_vectors[i] << std::cos(angle), std::sin(angle);
-    }
-
-    auto saddle_points_filtered = std::vector<SaddlePoint>{};
-
-    for (const auto& s : saddle_points)
-    {
-      // Sample the image intensity along the circle.
-      auto circle_intensities = Eigen::Array<float, num_circle_points, 1>{};
-      for (auto n = 0; n < num_circle_points; ++n)
-      {
-        const Eigen::Vector2d circle_point_n =
-            s.p.cast<double>() + radius * ori_vectors[n];
-
-        // Get the interpolated intensity value.
-        circle_intensities[n] = interpolate(image, circle_point_n);
-      }
-
-      // Rescale the intensity values.
-      const auto min_intensity = circle_intensities.minCoeff();
-      const auto max_intensity = circle_intensities.minCoeff();
-      const auto mid_intensity = (max_intensity + min_intensity) * 0.5f;
-      circle_intensities = circle_intensities - mid_intensity;
-
-      // ANALYSIS ON IMAGE INTENSITY:
-      // Count the number of zero-crossings: there must be 4 zero-crossings
-      // because of the chessboard pattern.
-      auto num_crossings = 0;
-      for (auto n = 0; n < num_circle_points; ++n)
-      {
-        // A zero-crossing is characterized by a negative sign between
-        // consecutive intensity values.
-        const auto& a = circle_intensities[n];
-        const auto& b = circle_intensities[(n + 1) % num_circle_points];
-        if (a * b < 0)
-          ++num_crossings;
-      }
-
-      auto image_patch = safe_crop(gradient_mag, s.p, std::round(radius));
-
-      // TODO: apply the mask.
-      // image_patch.array() *= mask[r].array();
-
-      // Calculate the half of the max intensity value in the image patch.
-      const auto half_max_patch_intensity =
-          image_patch.flat_array().maxCoeff() * 0.5f;
-
-      // Clamp the intensity values.
-      image_patch.cwise_transform_inplace(
-          [half_max_patch_intensity](auto& val) {
-            val = val < half_max_patch_intensity ? 0 : val;
-          });
-
-      // ANALYSIS ON THE IMAGE GRADIENTS.
-      //
-      // Count the number gradient orientation peaks in the image patch: there
-      // must be only two peaks in the gradient absolute orientation because of
-      // the chessboard pattern.
-      auto ori_hist = Eigen::Array<float, num_bins, 1>{};
-      ori_hist.setZero();
-      auto r = int{};
-      for (auto v = -r; v <= r; ++v)
-      {
-        for (auto u = -r; u <= r; ++u)
-        {
-          const auto& angle = gradient_ori(s.p.x() + u, s.p.y() + v);
-          const auto angle_normalized =
-              static_cast<float>(std::abs(angle) / M_PI) * num_bins;
-          auto angle_int = float{};
-          const auto angle_frac = std::modf(angle_normalized, &angle_int);
-          const auto angle_weight = 1 - angle_frac;
-
-          auto angle_bin_0 = static_cast<int>(angle_int);
-          if (angle_bin_0 == num_bins)
-            angle_bin_0 = 0;
-          const auto angle_bin_1 = (angle_bin_0 + 1) % num_bins;
-
-          ori_hist[angle_bin_0] += angle_weight;
-          ori_hist[angle_bin_1] += (1 - angle_weight);
-        }
-      }
-
-      // Find the mean shift modes, or simply perform like David Lowe, smoothe
-      // the orientation histogram and find the peaks.
-
-      // TODO: keep only the candidate saddle points where there are 4
-      // zero-crossings and only two mode gradient modes.
-    }
-
-    return saddle_points_filtered;
-  }
-
-}  // namespace DO::Sara
-
-int __main(int argc, char** argv)
+struct CircularProfileExtractor
 {
-  using namespace std::string_literals;
+  inline CircularProfileExtractor()
+  {
+    initialize_circle_sample_points();
+  }
 
+  // Sample a unit circle centered at the origin.
+  inline auto initialize_circle_sample_points() -> void
+  {
+    static constexpr auto pi = static_cast<double>(M_PI);
+
+    const auto& n = num_circle_sample_points;
+    circle_sample_points = std::vector<Eigen::Vector2d>(n);
+
+    for (auto i = 0; i < n; ++i)
+    {
+      const auto angle = i * 2 * pi / n;
+      circle_sample_points[i] << std::cos(angle), std::sin(angle);
+    }
+  }
+
+  inline auto operator()(const sara::ImageView<float>& image,
+                         const Eigen::Vector2d& center) const -> Eigen::ArrayXf
+  {
+    auto intensity_profile = Eigen::ArrayXf(num_circle_sample_points);
+
+    for (auto n = 0; n < num_circle_sample_points; ++n)
+    {
+      const Eigen::Vector2d pn =
+          center + circle_radius * circle_sample_points[n];
+
+      // Get the interpolated intensity value.
+      intensity_profile(n) = interpolate(image, pn);
+    }
+
+    // Normalize the intensities.
+    const auto min_intensity = intensity_profile.minCoeff();
+    const auto max_intensity = intensity_profile.maxCoeff();
+    const auto mid_intensity = (max_intensity + min_intensity) * 0.5f;
+    intensity_profile -= mid_intensity;
+
+    return intensity_profile;
+  }
+
+  int num_circle_sample_points = 36;
+  double circle_radius = 10.;
+  std::vector<Eigen::Vector2d> circle_sample_points;
+};
+
+
+auto localize_zero_crossings(const Eigen::ArrayXf& profile, int num_bins)
+    -> std::vector<float>
+{
+  auto zero_crossings = std::vector<float>{};
+  for (auto n = 0; n < profile.size(); ++n)
+  {
+    const auto ia = n;
+    const auto ib = (n + 1) % profile.size();
+
+    const auto& a = profile[ia];
+    const auto& b = profile[ib];
+
+    const auto angle_a = ia * 2 * M_PI / num_bins;
+    const auto angle_b = ib * 2 * M_PI / num_bins;
+
+    const auto ea = Eigen::Vector2d{std::cos(angle_a),  //
+                                    std::sin(angle_a)};
+    const auto eb = Eigen::Vector2d{std::cos(angle_b),  //
+                                    std::sin(angle_b)};
+
+    // TODO: this all could have been simplified.
+    const Eigen::Vector2d dir = (ea + eb) * 0.5;
+    auto angle = std::atan2(dir.y(), dir.x());
+    if (angle < 0)
+      angle += 2 * M_PI;
+
+    // A zero-crossing is characterized by a negative sign between
+    // consecutive intensity values.
+    if (a * b < 0)
+      zero_crossings.push_back(angle);
+  }
+
+  return zero_crossings;
+}
+
+
+auto __main(int argc, char** argv) -> int
+{
   if (argc < 2)
     return 1;
-  const auto folder = std::string{argv[1]};
 
+  static constexpr auto nms_radius = 5;
+  static constexpr auto adaptive_thres = 0.05f;
   static constexpr auto sigma = 1.6f;
-  static constexpr auto nms_radius = 10;
-  static constexpr float adaptive_thres = 0.05f;
 
-  for (auto i = 0; i <= 1790; i += 10)
+  const auto video_file = std::string{argv[1]};
+  auto video_stream = sara::VideoStream{video_file};
+  auto video_frame = video_stream.frame();
+  auto frame_number = -1;
+
+  auto profile_extractor = CircularProfileExtractor{};
+
+  while (video_stream.read())
   {
-    const auto image_filepath = folder + "/" + std::to_string(i) + ".png";
-
-    auto image = sara::imread<sara::Rgb8>(image_filepath);
-    const auto image_gray = image.convert<float>();
-    const auto image_blurred = image_gray.compute<sara::Gaussian>(sigma);
+    ++frame_number;
+    if (frame_number % 3 != 0)
+      continue;
 
     if (sara::active_window() == nullptr)
     {
-      sara::create_window(image.sizes());
+      sara::create_window(video_frame.sizes());
       sara::set_antialiasing();
     }
 
-    // Calculate the first derivative.
+    const auto image_gray = video_frame.convert<float>();
+    const auto image_blurred = image_gray.compute<sara::Gaussian>(sigma);
+
+    // Calculate the hessian matrix.
     const auto hessian = image_blurred.compute<sara::Hessian>();
 
     // Chessboard corners are saddle points of the image, which are
@@ -203,27 +155,73 @@ int __main(int argc, char** argv)
     auto saddle_points = extract_saddle_points(det_of_hessian, hessian, thres);
 
     // Non-maxima suppression.
-    nms(saddle_points, image.sizes(), nms_radius);
+    nms(saddle_points, video_frame.sizes(), profile_extractor.circle_radius);
 
     for (const auto& s : saddle_points)
     {
-      sara::fill_circle(image, s.p.x(), s.p.y(), 5, sara::Red8);
+      const auto r = profile_extractor.circle_radius;
+      if (s.p.x() < r || s.p.x() >= video_frame.width() - r ||  //
+          s.p.y() < r || s.p.y() >= video_frame.height() - r)
+        continue;
 
-      const auto svd =
-          s.hessian.jacobiSvd(Eigen::ComputeFullU | Eigen::ComputeFullV);
-      const Eigen::Vector2f S = svd.singularValues();
-      const auto& axes = svd.matrixU();
+      const auto profile = profile_extractor(image_blurred, s.p.cast<double>());
+      const auto zero_crossings = localize_zero_crossings(  //
+          profile,                                          //
+          profile_extractor.num_circle_sample_points        //
+      );
 
-      const auto a = Eigen::Vector2f(s.p.x(), s.p.y());
-      static constexpr auto radius = 20.f;
-      const Eigen::Vector2f b = a + radius * axes.col(0);
-      const Eigen::Vector2f c = a + radius * axes.col(1);
+      // ANALYSIS ON IMAGE INTENSITY:
+      //
+      // Count the number of zero-crossings: there must be 4 zero-crossings
+      // because of the chessboard pattern.
+      if (zero_crossings.size() != 4u)
+        continue;
 
-      sara::draw_arrow(image, a, b, sara::Cyan8, 2);
-      sara::draw_arrow(image, a, c, sara::Cyan8, 2);
+      auto angle_deltas = std::vector<double>{};
+      for (auto i = 0; i < 4; ++i)
+      {
+        const auto ia = i;
+        const auto ib = (i + 1) % 4;
+
+        const auto anglea = zero_crossings[ia];
+        const auto angleb = zero_crossings[ib];
+
+        const auto ea = Eigen::Vector2d{std::cos(anglea), std::sin(anglea)};
+        const auto eb = Eigen::Vector2d{std::cos(angleb), std::sin(angleb)};
+
+        auto e = Eigen::Matrix2d{};
+        e << ea, eb;
+
+        const auto c_ab = ea.dot(eb);
+        const auto s_ab = e.determinant();
+        const auto angle = std::atan2(s_ab, c_ab);
+        angle_deltas.push_back(std::abs(angle));
+      }
+
+      auto bad = false;
+      for (auto i = 0; i < 4; ++i)
+        if (std::abs(angle_deltas[i] - M_PI / 2) > M_PI / 3)
+        {
+          bad = true;
+          break;
+        }
+      if (bad)
+        continue;
+
+      sara::draw_circle(video_frame, s.p.x(), s.p.y(),
+                        profile_extractor.circle_radius, sara::Red8);
+      sara::draw(video_frame, s);
+
+      for (const auto& angle : zero_crossings)
+      {
+        const auto& r = profile_extractor.circle_radius;
+        const auto e = Eigen::Vector2d{std::cos(angle), std::sin(angle)};
+        const auto p = s.p.cast<double>() + r * e;
+        sara::fill_circle(video_frame, p.x(), p.y(), 2, sara::Yellow8);
+      }
     }
 
-    sara::display(image);
+    sara::display(video_frame);
     sara::get_key();
   }
 
@@ -231,9 +229,24 @@ int __main(int argc, char** argv)
 }
 
 
-int main(int argc, char** argv)
+auto main(int argc, char** argv) -> int
 {
   DO::Sara::GraphicsApplication app(argc, argv);
   app.register_user_main(__main);
   return app.exec();
 }
+
+
+// auto gradient_hist_extractor = SymGradientHistogramExtractor{};
+
+// Calculate the gradient map in the polar coordinates.
+// auto grad_mag = sara::Image<float>{image_blurred.sizes()};
+// auto grad_ori = sara::Image<float>{image_blurred.sizes()};
+// sara::gradient_in_polar_coordinates(image_blurred, grad_mag, grad_ori);
+
+
+// const auto ori_hist = gradient_hist_extractor(grad_mag, grad_ori, s.p);
+// const auto ori_peaks = find_peaks(ori_hist);
+
+// if (ori_peaks.size() != 2)
+//   continue;
