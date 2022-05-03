fb4ca8e1a3a6744972553e96cb83ce44d6a0d776
David OK <david.ok8@gmail.com>
2022-05-03 13:37:34 +0100

    MAINT: quick-and dirty fix.

--- svelte/sara-app/src/components/emscripten.svelte
+++ svelte/sara-app/src/components/emscripten.svelte
@@ -1,65 +1,41 @@
-<script>
-	import { onMount } from 'svelte';
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
 	import { default as Module } from '../components/test_image_dewarp_renderer.js';
 
-	let files;
-	let videoUrl;
-	let video;
-	let copyVideo = false;
+	let files: FileList | null;
+	let videoUrl: string | null;
+	let video: HTMLMediaElement | null;
+	let videoInterval: ReturnType<typeof setInterval> | null;
 
 	// UI interactions.
-	let _stopVideo = false;
+	let _pauseVideo = false;
 	let _resizeCanvas = false;
 	let _hideMousePointer = false;
 
 	// Our reference to the Emscripten module.
-	let _emmodule;
+	let _emmodule: Object | null;
 	// Our reference to the GL canvas.
-	let _emcanvas;
-	let _emglctx;
+	let _emcanvas: HTMLCanvasElement | null;
+	let _emglctx: WebGL2RenderingContext | null;
 
-	const setupVideo = (url) => {
-		var playing = false;
-		var timeupdate = false;
-
-		video.autoplay = true;
-		video.muted = true;
-		video.loop = true;
-
-		// Waiting for these 2 events ensures
-		// there is data in the video
-
-		video.addEventListener(
-			'playing',
-			function () {
-				playing = true;
-				checkReady();
-			},
-			true
-		);
-
-		video.addEventListener(
-			'timeupdate',
-			function () {
-				timeupdate = true;
-				checkReady();
-			},
-			true
-		);
-
-		video.src = url;
-		video.play();
-
-		function checkReady() {
-			if (playing && timeupdate) {
-				copyVideo = true;
-			}
+	const setupVideo = (url: string) => {
+		if (video === null) {
+			return null;
 		}
 
-		return video;
+		video.src = url;
+		video.autoplay = true;
+		video.loop = true;
+		video.play().catch((e) => {
+			console.log('Caught video exception: ' + e);
+		});
 	};
 
-	const updateTexture = (gl, texture, video) => {
+	const updateTexture = (
+		gl: WebGL2RenderingContext,
+		texture: WebGLTexture,
+		video: HTMLCanvasElement
+	) => {
 		const level = 0;
 		const internalFormat = gl.RGB;
 		const srcFormat = gl.RGB;
@@ -79,7 +55,6 @@
 		// We have got no other choice but to keep regenerating mipmaps. Otherwise
 		// we will see white artefacts when zooming out on videos.
 		gl.generateMipmap(gl.TEXTURE_2D);
-		console.log('update texture');
 	};
 
 	onMount(async () => {
@@ -87,24 +62,39 @@
 		window.JSEvents = null;
 		window.specialHTMLTargets = null;
 
+		// Run WebGL.
 		_emmodule = await Module({
 			noInitialRun: true,
 			canvas: _emcanvas
 		});
-
-		// Run WebGL.
 		_emmodule.callMain();
 		_emglctx = _emmodule.GL.currentContext.GLctx;
 
 		// window.emmodule = _emmodule;
 		// window.GLctx = _emglctx;
 
-		setInterval(() => {
+		videoInterval = setInterval(() => {
 			if (files) {
 				const texture = _emmodule.GL.textures.slice(-1)[0];
-				updateTexture(_emglctx, texture, video);
+				if (video !== null) {
+					updateTexture(_emglctx, texture, video);
+				}
 			}
 		}, 16);
+	});
+
+	onDestroy(async () => {
+		if (videoInterval !== null) {
+			clearInterval(videoInterval);
+		}
+		if (video !== null) {
+			video.pause();
+		}
+
+		delete window.JSEvents;
+		delete window.specialHTMLTargets;
+		// delete window.emmodule;
+		// delete window.GLctx;
 	});
 
 	$: {
@@ -117,18 +107,35 @@
 				setupVideo(videoUrl);
 			}
 
-			_emcanvas.hidden = false;
+			if (_emcanvas !== null) {
+				_emcanvas.hidden = false;
+			}
 		}
 
 		if (typeof video !== 'undefined' && video !== null && videoUrl !== null) {
-			if (_stopVideo) video.pause();
-			else video.play();
+			if (_pauseVideo) {
+				clearInterval(videoInterval);
+				videoInterval = null;
+				video.pause();
+			} else {
+				if (videoInterval === null) {
+					videoInterval = setInterval(() => {
+						if (files) {
+							const texture = _emmodule.GL.textures.slice(-1)[0];
+							updateTexture(_emglctx, texture, video);
+						}
+					}, 16);
+				}
+				video.play().catch((e) => {
+					console.log('Caught video exception: ' + e);
+				});
+			}
 		}
 	}
 </script>
 
 <div class="w-full items-center">
-	<video bind:this={video} hidden="true" />
+	<video bind:this={video} hidden muted />
 
 	<input
 		class="m-1 p-1 bg-gray-100 hover:bg-gray-500 rounded-md"
@@ -137,10 +144,10 @@
 		type="file"
 	/>
 
-	<input class="btn btn-blue" type="checkbox" bind:checked={_stopVideo} />Stop video
-	<input class="btn btn-blue" type="checkbox" bind:checked={_resizeCanvas} />Resize canvas
-	<input class="btn btn-blue" type="checkbox" bind:checked={_hideMousePointer} />Lock/hide mouse
-	pointer &nbsp;&nbsp;&nbsp;
+	<input type="checkbox" bind:checked={_pauseVideo} />Stop video
+	<input type="checkbox" bind:checked={_resizeCanvas} />Resize canvas
+	<input type="checkbox" bind:checked={_hideMousePointer} />Lock/hide mouse pointer
+	&nbsp;&nbsp;&nbsp;
 	<button
 		class="m-1 p-1 bg-gray-100 hover:bg-gray-500 rounded-md"
 		on:click={_emmodule.requestFullscreen(_hideMousePointer, _resizeCanvas)}>Fullscreen</button
