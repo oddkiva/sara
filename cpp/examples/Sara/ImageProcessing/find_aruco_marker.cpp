00c33b434c15bd06d1bc8e309e1b5667664a844d
David OK <david.ok8@gmail.com>
2022-07-10 19:53:22 +0100

    MAINT: fix build.

--- cpp/examples/Sara/ImageProcessing/find_aruco_marker.cpp
+++ cpp/examples/Sara/ImageProcessing/find_aruco_marker.cpp
@@ -89,7 +89,6 @@ auto __main(int argc, char** argv) -> int
     const auto grad_thres = grad_adaptive_thres * grad_max;
     auto edge_map = sara::suppress_non_maximum_edgels(
         grad_f_norm, grad_f_ori, 2 * grad_thres, grad_thres);
-#  pragma omp parallel for
     for (auto e = edge_map.begin(); e != edge_map.end(); ++e)
       if (*e == 127)
         *e = 0;
