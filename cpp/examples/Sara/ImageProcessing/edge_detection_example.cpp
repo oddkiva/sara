b8d84b01e4a5e971cb036536dcd3b78ee263679c
David OK <david.ok8@gmail.com>
2022-07-11 13:35:37 +0100

    MAINT: some tweak for visual inspection.

--- cpp/examples/Sara/ImageProcessing/edge_detection_example.cpp
+++ cpp/examples/Sara/ImageProcessing/edge_detection_example.cpp
@@ -87,11 +87,11 @@ int __main(int argc, char** argv)
   create_window(frame.sizes());
   set_antialiasing();
 
-  constexpr float high_threshold_ratio = static_cast<float>(8._percent);
+  constexpr float high_threshold_ratio = static_cast<float>(4._percent);
   constexpr float low_threshold_ratio =
       static_cast<float>(high_threshold_ratio / 2.);
-  constexpr float angular_threshold = static_cast<float>((20._deg).value);
-  const auto sigma = std::sqrt(std::pow(1.4f, 2) - 1);
+  constexpr float angular_threshold = static_cast<float>((10._deg).value);
+  const auto sigma = 3.f;  // std::sqrt(square(1.6f) - square(0.5f));
 
   auto ed = EdgeDetector{{
       high_threshold_ratio,  //
@@ -132,11 +132,15 @@ int __main(int argc, char** argv)
     const auto& edges_simplified = ed.pipeline.edges_simplified;
 
     tic();
+    auto disp = frame.convert<float>().convert<Rgb8>();
     for (const auto& e : edges_simplified)
-      if (e.size() >= 2 && length(e) > 5)
-        draw_polyline(frame, e, Red8, Eigen::Vector2d(0, 0),
-                      float(downscale_factor));
-    display(frame);
+      if (e.size() >= 2 && length(e) > 3)
+      {
+        const auto color = Rgb8(rand() % 255, rand() % 255, rand() % 255);
+        draw_polyline(disp, e, color, Eigen::Vector2d{0, 0},
+                      static_cast<float>(downscale_factor));
+      }
+    display(disp);
     toc("Display");
 
     tic();
