0f2eef1b05513b29a68ddecad9fc44a233a5491b
David OK <david.ok8@gmail.com>
2022-07-14 00:57:48 +0100

    MAINT: clean up code.

--- cpp/examples/Sara/ImageProcessing/find_aruco_marker.cpp
+++ cpp/examples/Sara/ImageProcessing/find_aruco_marker.cpp
@@ -50,30 +50,6 @@ struct Corner
   }
 };
 
-// Select the local maxima of the cornerness functions.
-auto select(const sara::ImageView<float>& cornerness,
-            const float cornerness_adaptive_thres, const int border)
-    -> std::vector<Corner<int>>
-{
-  const auto extrema = sara::local_maxima(cornerness);
-
-  const auto cornerness_max = cornerness.flat_array().maxCoeff();
-  const auto cornerness_thres = cornerness_adaptive_thres * cornerness_max;
-
-  auto extrema_filtered = std::vector<Corner<int>>{};
-  extrema_filtered.reserve(extrema.size());
-  for (const auto& p : extrema)
-  {
-    const auto in_image_domain =
-        border <= p.x() && p.x() < cornerness.width() - border &&  //
-        border <= p.y() && p.y() < cornerness.height() - border;
-    if (in_image_domain && cornerness(p) > cornerness_thres)
-      extrema_filtered.push_back({p, cornerness(p)});
-  }
-
-  return extrema_filtered;
-};
-
 
 auto __main(int argc, char** argv) -> int
 {
