f571dba7e2fe8590fd0b0a4e6cabe8ec833d53a4
David OK <david.ok8@gmail.com>
2022-09-29 19:54:22 +0100

    MAINT: simplify unit test.

--- cpp/drafts/NeuralNetworks/TensorRT/test/test_neuralnetworks_tensorrt_yolo_plugin.cpp
+++ cpp/drafts/NeuralNetworks/TensorRT/test/test_neuralnetworks_tensorrt_yolo_plugin.cpp
@@ -53,10 +53,32 @@ BOOST_AUTO_TEST_CASE(test_that_the_yolo_plugin_is_automatically_registered)
       });
   BOOST_CHECK_NE(yolo_creator_it, plugin_creator_last);
 
-  auto yolo_plugin = yolo_plugin_creator->createPlugin("yolo", nullptr);
-  SARA_CHECK(yolo_plugin->getPluginNamespace());
-  SARA_CHECK(yolo_plugin->getPluginType());
+  // Create the plugin field collection.
+  auto fields = std::vector<nvinfer1::PluginField>{};
+  const auto num_boxes_per_grid_cell = 3;
+  const auto yolo_classes = 80;
+  const auto h = 13;
+  const auto w = 13;
+  const auto scale_x_y = 1.05f;
+  fields.emplace_back("num_boxes_per_grid_cell", &num_boxes_per_grid_cell,
+                      nvinfer1::PluginFieldType::kINT32, 1);
+  fields.emplace_back("num_classes", &yolo_classes,
+                      nvinfer1::PluginFieldType::kINT32, 1);
+  fields.emplace_back("height", &h, nvinfer1::PluginFieldType::kINT32, 1);
+  fields.emplace_back("width", &w, nvinfer1::PluginFieldType::kINT32, 1);
+  fields.emplace_back("scale_x_y", &scale_x_y,
+                      nvinfer1::PluginFieldType::kFLOAT32, 1);
 
+  auto fc = nvinfer1::PluginFieldCollection{};
+  fc.fields = fields.data();
+  fc.nbFields = static_cast<std::int32_t>(fields.size());
+
+  const auto yolo_plugin = yolo_plugin_creator->createPlugin("yolo", &fc);
+  BOOST_CHECK_NE(
+      std::string(yolo_plugin->getPluginNamespace()).find(""),
+      std::string::npos);
+
+#if defined(TODO)
   // Resize the host tensor.
   auto u_in_tensor = PinnedTensor<float, 3>{};
   // u_in_tensor.resize(3, input_layer.height(), input_layer.width());
@@ -66,8 +88,8 @@ BOOST_AUTO_TEST_CASE(test_that_the_yolo_plugin_is_automatically_registered)
   // u_out_tensor.resize{out_sizes(1), out_sizes(2), out_sizes(3)};
 
   const auto batch_size = 0;
-  auto inputs = std::vector<const void *>{};
-  auto outputs = std::vector<void *>{};
+  auto inputs = std::vector<const void*>{};
+  auto outputs = std::vector<void*>{};
   const auto workspace = nullptr;
 
   auto cuda_stream = trt::make_cuda_stream();
@@ -75,6 +97,7 @@ BOOST_AUTO_TEST_CASE(test_that_the_yolo_plugin_is_automatically_registered)
                                          outputs.data(), workspace,
                                          *cuda_stream),
                     0);
+#endif
 }
 
 BOOST_AUTO_TEST_SUITE_END()
