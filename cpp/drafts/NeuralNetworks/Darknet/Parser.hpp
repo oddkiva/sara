074cee9029cf3c39e834ca2324076f08a06a9811
David OK <david.ok8@gmail.com>
2022-09-29 19:46:19 +0100

    MAINT: copyright notice and compiler warning.

--- cpp/drafts/NeuralNetworks/Darknet/Parser.hpp
+++ cpp/drafts/NeuralNetworks/Darknet/Parser.hpp
@@ -1,3 +1,14 @@
+// ========================================================================== //
+// This file is part of Sara, a basic set of libraries in C++ for computer
+// vision.
+//
+// Copyright (C) 2022 David Ok <david.ok8@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+// ========================================================================== //
+
 #pragma once
 
 #include <drafts/NeuralNetworks/Darknet/Layer.hpp>
