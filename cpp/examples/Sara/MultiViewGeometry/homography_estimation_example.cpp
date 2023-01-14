d90640c46f9fe9c39d3bf11592218251681873e3
David OK <david.ok8@gmail.com>
2023-01-14 05:40:53 +0000

    WIP: save work.

--- cpp/examples/Sara/MultiViewGeometry/homography_estimation_example.cpp
+++ cpp/examples/Sara/MultiViewGeometry/homography_estimation_example.cpp
@@ -16,7 +16,10 @@
 #include <DO/Sara/ImageIO.hpp>
 #include <DO/Sara/Match.hpp>
 #include <DO/Sara/MultiViewGeometry.hpp>
+#include <DO/Sara/RANSAC/Utility.hpp>
 #include <DO/Sara/Visualization.hpp>
+
+#include <filesystem>
 
 
 using namespace std;
@@ -28,8 +31,7 @@ using namespace DO::Sara;
 // Feature detection and matching.
 //
 // Detect or read SIFT keypoints.
-auto get_keypoints(const Image<Rgb8>& image1,
-                   const Image<Rgb8>& image2,
+auto get_keypoints(const Image<Rgb8>& image1, const Image<Rgb8>& image2,
                    const std::string& keys1_filepath,
                    const std::string& keys2_filepath,
                    KeypointList<OERegion, float>& keys1,
@@ -37,33 +39,40 @@ auto get_keypoints(const Image<Rgb8>& image1,
 {
   print_stage("Computing/Reading keypoints");
 
-#ifdef COMPUTE_KEYPOINTS
-  keys1 = compute_sift_keypoints(image1.convert<float>());
-  keys2 = compute_sift_keypoints(image2.convert<float>());
-  cout << "Image 1: " << keys1.size() << " keypoints" << endl;
-  cout << "Image 2: " << keys2.size() << " keypoints" << endl;
+  namespace fs = std::filesystem;
 
-  const auto& [f1, d1] = keys1;
-  const auto& [f2, d2] = keys2;
+#ifdef FIXME
+  const auto load_or_compute_keys = [](auto& keys, const auto& keys_filepath,
+                                       const auto& image) {
+    if (!fs::exists(keys_filepath))
+    {
+      keys = compute_sift_keypoints(image.template convert<float>());
+      const auto& [f, d] = keys;
+      write_keypoints(f, d, keys_filepath);
+    }
+    else
+    {
+      auto& [f, d] = keys;
+      read_keypoints(f, d, keys_filepath);
+    }
+  };
 
-  write_keypoints(f1, d1, keys1_filepath);
-  write_keypoints(f2, d2, keys2_filepath);
+  load_or_compute_keys(keys1, keys1_filepath, image1);
+  load_or_compute_keys(keys2, keys2_filepath, image2);
 #else
-  (void) image1;
-  (void) image2;
-  auto& [f1, d1] = keys1;
-  auto& [f2, d2] = keys2;
-
-  read_keypoints(f1, d1, keys1_filepath);
-  read_keypoints(f2, d2, keys2_filepath);
+  keys1 = compute_sift_keypoints(image1.template convert<float>());
+  keys2 = compute_sift_keypoints(image2.template convert<float>());
 #endif
+
+  cout << "Image 1: " << features(keys1).size() << " keypoints" << endl;
+  cout << "Image 2: " << features(keys2).size() << " keypoints" << endl;
 }
 
 // Descriptor-based matching.
 //
-// TODO: by default a feature matcher should return a tensor containing pair of
-// indices instead. While becoming trickier to use, it is more powerful for data
-// manipulation.
+// TODO: by default a feature matcher should return a tensor containing pair
+// of indices instead. While becoming trickier to use, it is more powerful for
+// data manipulation.
 //
 // Convert a set of matches to a tensor.
 auto compute_matches(const KeypointList<OERegion, float>& keys1,
@@ -112,6 +121,56 @@ auto estimate_homography(const KeypointList<OERegion, float>& keys1,
   return std::make_tuple(H, inliers, sample_best);
 }
 
+auto estimate_homography_v2(const KeypointList<OERegion, float>& keys1,
+                            const KeypointList<OERegion, float>& keys2,
+                            const vector<Match>& matches, int num_samples,
+                            double h_err_thres)
+{
+  // ==========================================================================
+  // Normalize the points.
+  const auto& f1 = features(keys1);
+  const auto& f2 = features(keys2);
+  const auto p1 = extract_centers(f1).cast<double>();
+  const auto p2 = extract_centers(f2).cast<double>();
+
+  const auto M = to_tensor(matches);
+  const auto to_data = [](const TensorView_<int, 2>& M,
+                          const TensorView_<double, 2>& p1,
+                          const TensorView_<double, 2>& p2) {
+    auto X = Tensor_<double, 3>{2, M.size(0), p1.size(1)};
+    auto p1_mat = p1.matrix();
+    auto p2_mat = p2.matrix();
+    auto p1_matched = X[0].matrix();
+    auto p2_matched = X[1].matrix();
+    for (auto m = 0; m < M.size(0); ++m)
+    {
+      const auto& i1 = M(m, 0);
+      const auto& i2 = M(m, 1);
+
+      p1_matched.row(m) = p1_mat.row(i1);
+      p2_matched.row(m) = p2_mat.row(i2);
+    }
+    return X;
+  };
+
+  const auto X = to_data(M, p1, p2);
+
+  const auto data_normalizer = Normalizer<Homography>{X[0], X[1]};
+
+  std::cout << "OK\n";
+  auto Xn = Tensor_<double, 3>{X.sizes()};
+  // data_normalizer.normalize(X)
+  {
+    Xn[0] = apply_transform(data_normalizer.T1, X[0]);
+    Xn[1] = apply_transform(data_normalizer.T2, X[1]);
+  }
+
+  const auto minimal_index_subsets = random_samples(1000, 4, X.size(0));
+
+  // The simplest and most natural implementation.
+  auto p1_sampled = from_index_to_point(minimal_index_subsets, Xn[0]);
+  auto p2_sampled = from_index_to_point(minimal_index_subsets, Xn[1]);
+}
 
 // =============================================================================
 // Visual inspection.
@@ -173,7 +232,6 @@ void inspect_homography_estimation(const Image<Rgb8>& image1,
     drawer.draw_point(0, proj_Y.col(i).head(2).cast<float>(), Magenta8, 7);
     // Draw the best elemental subset drawn by RANSAC.
     drawer.draw_match(matches[sample_best(i)], Red8, true);
-
   }
 
   get_key();
@@ -186,11 +244,9 @@ GRAPHICS_MAIN()
   // Load images.
   print_stage("Loading images...");
 #ifdef __APPLE__
-  const auto data_dir =
-      "/Users/david/Desktop/Datasets/sfm/castle_int"s;
+  const auto data_dir = "/Users/david/Desktop/Datasets/sfm/castle_int"s;
 #else
-  const auto data_dir =
-      "/home/david/Desktop/Datasets/sfm/castle_int"s;
+  const auto data_dir = "/home/david/Desktop/Datasets/sfm/castle_int"s;
 #endif
   const auto file1 = "0000.png";
   const auto file2 = "0001.png";
@@ -209,6 +265,7 @@ GRAPHICS_MAIN()
   print_stage("Match keypoints...");
   const auto matches = compute_matches(keys1, keys2);
 
+#ifdef OLD
   print_stage("Estimate the principal homography...");
   const auto num_samples = 1000;
   const auto h_err_thres = 1.;
@@ -218,6 +275,16 @@ GRAPHICS_MAIN()
   print_stage("Inspect the homography estimation...");
   inspect_homography_estimation(image1, image2, matches, H, inliers,
                                 sample_best);
+#else
+  print_stage("Estimate the principal homography...");
+  const auto num_samples = 1000;
+  const auto h_err_thres = 1.;
+  estimate_homography_v2(keys1, keys2, matches, num_samples, h_err_thres);
+
+  // print_stage("Inspect the homography estimation...");
+  // inspect_homography_estimation(image1, image2, matches, H, inliers,
+  //                               sample_best);
+#endif
 
   return 0;
 }
