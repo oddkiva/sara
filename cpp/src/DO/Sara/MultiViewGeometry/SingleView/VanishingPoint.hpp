661f3794ffa9289797a85f1bee9e0bbe31a9b08f
David OK <david.ok8@gmail.com>
2023-01-15 15:39:40 +0000

    MAINT: fix compile error.

--- cpp/src/DO/Sara/MultiViewGeometry/SingleView/VanishingPoint.hpp
+++ cpp/src/DO/Sara/MultiViewGeometry/SingleView/VanishingPoint.hpp
@@ -179,9 +179,8 @@ namespace DO::Sara {
                                      std::size_t num_random_samples = 100)
   {
     auto vp_solver = VanishingPointSolver<T>{};
-    auto inlier_predicate = InlierPredicate{
-        LineToVanishingPointDistance<T>{},  //
-        threshold                           //
+    auto inlier_predicate = InlierPredicate<LineToVanishingPointDistance<T>>{
+        {}, threshold  //
     };
     return ransac(lines,             //
                   vp_solver,         //
