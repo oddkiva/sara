94cb7fd69d25f25cf316b499afac83b40e71949b
David OK <david.ok8@gmail.com>
2023-01-22 08:42:53 +0000

    MAINT: try to use ccache on windows.

--- cmake/sara_use_ccache.cmake
+++ cmake/sara_use_ccache.cmake
@@ -1,7 +1,7 @@
 # Use ccache if available!
 find_program(CCACHE_PROGRAM ccache)
 
-if (CCACHE_PROGRAM)
+if (CCACHE_PROGRAM AND NOT MSVC)
   sara_message("Found ccache: ${CCACHE_PROGRAM}, let's use it!")
   set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ${CCACHE_PROGRAM})
 endif ()
@@ -23,4 +23,13 @@ if (RULE_LAUNCH_COMPILE AND CMAKE_GENERATOR STREQUAL "Xcode")
   set(CMAKE_XCODE_ATTRIBUTE_CXX        "${CMAKE_BINARY_DIR}/launch-cxx")
   set(CMAKE_XCODE_ATTRIBUTE_LD         "${CMAKE_BINARY_DIR}/launch-c")
   set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS "${CMAKE_BINARY_DIR}/launch-cxx")
+elseif (${CMAKE_GENERATOR} MATCHES "Visual Studio" AND CCACHE_PROGRAM)
+  file(COPY_FILE ${CCACHE_PROGRAM} ${CMAKE_BINARY_DIR}/cl.exe ONLY_IF_DIFFERENT)
+
+  set(CMAKE_VS_GLOBALS
+      "CLToolExe=cl.exe"
+      "CLToolPath=${CMAKE_BINARY_DIR}"
+      "TrackFileAccess=false"
+      "UseMultiToolTask=true"
+      "DebugInformationFormat=OldStyle")
 endif ()
