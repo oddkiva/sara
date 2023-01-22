ca5c731b8fefaaf77a73d75baf9fa409029bda28
David OK <david.ok8@gmail.com>
2023-01-22 10:10:10 +0000

    MAINT: roll back to older version

--- build.ps1
+++ build.ps1
@@ -14,7 +14,7 @@ $cmake_toolset = $cmake_vsver[$vsver]
 $qt_dir = "C:\local\qt-everywhere-src-6.1.2\qtbase"
 $cudnn_dir = "C:\local\cudnn-windows-x86_64-8.7.0.84_cuda11-archive"
 $halide_dir = "C:\local\Halide-14.0.0-x86-64-windows"
-$nvidia_codec_sdk_dir = "C:\local\Video_Codec_SDK_12.0.16"
+$nvidia_codec_sdk_dir = "C:\local\Video_Codec_SDK_9.1.23"
 $tensorrt_dir = "TensorRT-8.5.2.2"
 
 $update_vcpkg = $false
