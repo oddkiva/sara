92fa977c5bdecef958072c16c254760cee39f56d
Odd Kiva <2375733-oddkiva@users.noreply.gitlab.com>
2024-01-05 12:00:51 +0100

    ENH: fix bugs and create differentiable geometry block.

--- python/oddkiva/brahma/torch/image_processing/examples/image_warp_example.py
+++ python/oddkiva/brahma/torch/image_processing/examples/image_warp_example.py
@@ -2,17 +2,20 @@ from pathlib import Path
 
 from PIL import Image
 
-import pylab
+import numpy as np
+
+import matplotlib.pyplot as plt
 
 import torch
 import torchvision.transforms.v2 as v2
 
-# from oddkiva.brahma.torch.image_processing import rotate, upscale
+import oddkiva.brahma.torch.image_processing.warp as W
+from oddkiva.brahma.torch import DEFAULT_DEVICE
 
 
 def rotation(theta):
     return np.array([[np.cos(theta), -np.sin(theta)],
-                     np.sin(theta),  np.cos(theta)]])
+                     [np.sin(theta),  np.cos(theta)]])
 
 
 THIS_FILE = __file__
@@ -23,11 +26,28 @@ assert DOG_IMAGE_PATH.exists()
 
 to_float_chw = v2.Compose([v2.ToImage(),
                            v2.ToDtype(torch.float32, scale=True)])
-image = to_float_chw(Image.open(DOG_IMAGE_PATH))
+image = to_float_chw(Image.open(DOG_IMAGE_PATH)).to(DEFAULT_DEVICE)
 
+# Geometric transform.
+R = torch.Tensor(rotation(np.pi / 6))
+Rinv = torch.Tensor(R.T).to(DEFAULT_DEVICE)
 
-R = rotation(0.5)
+# Enumerate all the coordinates.
+h, w = image[0].shape
+p = W.enumerate_coords(w, h).to(DEFAULT_DEVICE)
+pinv = torch.matmul(Rinv, p.float())
 
+# Differentiable geometric computational block
+interp = W.BilinearInterpolation2d().to(DEFAULT_DEVICE)
 
-# pylab.imshow(image_scaled.astype(np.uint8))
-# pylab.show()
+image_warped = torch.zeros_like(image)
+
+for c in range(3):
+    values, ixs_flat = interp.forward(image[c], pinv)
+    image_warped_plane_c = image_warped[c].flatten()
+    image_warped_plane_c[ixs_flat] = values
+
+image_warped_np = torch.permute(image_warped, (1, 2, 0)).cpu().numpy()
+
+plt.imshow(image_warped_np)
+plt.show()
--- python/oddkiva/brahma/torch/image_processing/warp.py
+++ python/oddkiva/brahma/torch/image_processing/warp.py
@@ -1,4 +1,7 @@
+from typing import Tuple
+
 import torch as T
+import torch.nn as nn
 
 
 def enumerate_coords(w: int, h: int) -> T.Tensor:
@@ -8,12 +11,15 @@ def enumerate_coords(w: int, h: int) -> T.Tensor:
     return p
 
 
-def bilinear_interpolation_2d(image: T.Tensor, coords: T.Tensor) -> T.Tensor:
+def bilinear_interpolation_2d(
+    image: T.Tensor,
+    coords: T.Tensor
+) -> Tuple[T.Tensor, T.Tensor]:
     x, y = coords[0, :], coords[1, :]
 
     # Calculate the corners for each coordinates.
-    x0, x1 = T.floor(x), T.ceil(x)
-    y0, y1 = T.floor(y), T.ceil(y)
+    x0, x1 = T.floor(x), T.floor(x) + 1
+    y0, y1 = T.floor(y), T.floor(y) + 1
 
     h, w = image.shape[0], image.shape[1]
 
@@ -28,29 +34,43 @@ def bilinear_interpolation_2d(image: T.Tensor, coords: T.Tensor) -> T.Tensor:
         T.logical_and(xmap0, xmap1),
         T.logical_and(ymap0, ymap1)
     )
-    ixs_where_all_corners_in_image_domain = T.where(
+    ixs_where_all_corners_in_image_domain, = T.where(
         all_corners_in_image_domain
     )
 
     # Filter the coordinates
-    x = x[ixs_where_all_corners_in_image_domain]
-    y = y[ixs_where_all_corners_in_image_domain]
-    x0 = x0[ixs_where_all_corners_in_image_domain].int()
-    x1 = x1[ixs_where_all_corners_in_image_domain].int()
-    y0 = y0[ixs_where_all_corners_in_image_domain].int()
-    y1 = y1[ixs_where_all_corners_in_image_domain].int()
+    xf = x[ixs_where_all_corners_in_image_domain]
+    yf = y[ixs_where_all_corners_in_image_domain]
+    x0f = x0[ixs_where_all_corners_in_image_domain]
+    x1f = x1[ixs_where_all_corners_in_image_domain]
+    y0f = y0[ixs_where_all_corners_in_image_domain]
+    y1f = y1[ixs_where_all_corners_in_image_domain]
+
+    x0i = x0f.int()
+    x1i = x1f.int()
+    y0i = y0f.int()
+    y1i = y1f.int()
 
     image_flat = image.flatten()
-    v00 = image_flat[y0 * w + x0]
-    v10 = image_flat[y0 * w + x1]
-    v01 = image_flat[y1 * w + x0]
-    v11 = image_flat[y1 * w + x1]
+    v00 = image_flat[y0i * w + x0i]
+    v10 = image_flat[y0i * w + x1i]
+    v01 = image_flat[y1i * w + x0i]
+    v11 = image_flat[y1i * w + x1i]
 
-    ax0, ax1 = x1 - x, x - x0
-    ay0, ay1 = y1 - y, y - y0
+    ax0, ax1 = x1f - xf, xf - x0f
+    ay0, ay1 = y1f - yf, yf - y0f
 
     values = \
         ax0 * ay0 * v00 + ax1 * ay0 * v10 + \
         ax0 * ay1 * v01 + ax1 * ay1 * v11
 
-    return values, T.stack((x, y))
+    return values, ixs_where_all_corners_in_image_domain
+
+
+class BilinearInterpolation2d(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, coords):
+        return bilinear_interpolation_2d(x, coords)
