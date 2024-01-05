f94b105deceac2421d5c9ac7542f1d16be293c4d
Odd Kiva <2375733-oddkiva@users.noreply.gitlab.com>
2024-01-05 16:51:46 +0100

    MAINT: code cleanup.

--- python/oddkiva/sara/sfm/seven_point_algorithm.py
+++ python/oddkiva/sara/sfm/seven_point_algorithm.py
@@ -1,18 +1,21 @@
-import sympy as sp
+def calculate_F_matrix_rank_2_constraint_from_7_points():
+    import sympy as sp
 
-# Solve the cubic polynomial.
-F1 = sp.MatrixSymbol('F1', 3, 3)
-F2 = sp.MatrixSymbol('F2', 3, 3)
-α = sp.symbols('α')
+    # Solve the cubic polynomial.
+    F1 = sp.MatrixSymbol('F1', 3, 3)
+    F2 = sp.MatrixSymbol('F2', 3, 3)
+    α = sp.symbols('α')
 
-# Form the symbolic matrix expression as reported in the paper.
-F = sp.Matrix(F1 + α * F2)
+    # The fundamental matrix has rank 2.
+    # Necessarily its determinant must be 0.
+    #
+    # Form the symbolic matrix expression as reported in the paper.
+    F = sp.Matrix(F1 + α * F2)
 
-# Form the polynomial in the variable α.
-det_F, _ = sp.polys.poly_from_expr(F.det(), α)
+    # Form the polynomial in the variable α.
+    det_F, _ = sp.polys.poly_from_expr(F.det(), α)
 
-# Collect the coefficients "c[i]" as denoted in the paper.
-c = det_F.all_coeffs()
+    # Collect the coefficients "c[i]" as denoted in the paper.
+    c = det_F.all_coeffs()
 
-
-import IPython; IPython.embed()
+    return c
