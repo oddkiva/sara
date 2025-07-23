ab1cda2aadef77e5ed1454a1be6bd71cbf39acf1
David OK <david.ok8@gmail.com>
2025-07-23 18:34:11 +0100

    MAINT: make weight regularization optional.

--- python/oddkiva/brahma/torch/datasets/reid/test/test_triplet_dataset.py
+++ python/oddkiva/brahma/torch/datasets/reid/test/test_triplet_dataset.py
@@ -6,7 +6,9 @@ from oddkiva.brahma.torch.datasets.reid.eth123 import ETH123
 
 
 def test_triplet_sampling():
-    eth123_root_path = Path('/Users/oddkiva/Downloads/reid/dataset_ETHZ/')
+    # eth123_root_path = Path('/Users/oddkiva/Downloads/reid/dataset_ETHZ/')
+    eth123_root_path = Path('/home/david/GitLab/oddkiva/sara/data/reid/dataset_ETHZ/')
+    assert eth123_root_path.exists()
     eth123_ds = ETH123(eth123_root_path)
 
     eth123_tds = TripletDatabase(eth123_ds)
