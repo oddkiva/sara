19bc9160837ac1c63b3b73623f7567af5dde07b6
David OK <david.ok8@gmail.com>
2025-07-30 16:56:42 +0100

    ENH: add composite triplet dataset class.

---
+++ python/oddkiva/brahma/torch/datasets/reid/multi_triplet_dataset.py
@@ -0,0 +1,37 @@
+from typing import List
+
+import torch
+from torch.utils.data import Dataset
+
+from oddkiva.brahma.torch.datasets.reid.triplet_dataset import TripletDatabase
+
+
+class MultiTripletDatabase(Dataset):
+
+    def __init__(self, triplet_datasets: List[TripletDatabase],
+                 num_dataset_samples: int):
+        self.triplet_datasets = triplet_datasets
+
+        num_datasets = len(self.triplet_datasets)
+        self.num_dataset_samples = num_dataset_samples
+        self.dataset_samples = torch.randint(0, num_datasets, (num_dataset_samples,))
+
+        # Track the current indices for each triplet datasets.
+        self._current_triplet_sample_idx = [0] * num_dataset_samples
+
+    def  __len__(self):
+        return self.num_dataset_samples * ([len(tds) for tds in self.triplet_datasets])
+
+    def __getitem__(self, idx: int) -> TripletDatabase.TripletSample:
+        tds_idx = self.dataset_samples[idx]
+        tds = self.triplet_datasets[tds_idx]
+
+        # Get the triplet sample.
+        triplet_sample_idx = self._current_triplet_sample_idx[tds_idx]
+        triplet = tds[triplet_sample_idx]
+
+        # Increment the current triplet sample index for the next queries.
+        self._current_triplet_sample_idx[tds_idx] = \
+            (triplet_sample_idx + 1) % len(tds)
+
+        return triplet
