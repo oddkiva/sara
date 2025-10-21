e2793e2bfbca9db131403460f040d42f098d5286
David OK <david.ok8@gmail.com>
2025-10-21 18:57:35 +0100

    MAINT: remove buggy import.

--- python/oddkiva/sara/__init__.py
+++ python/oddkiva/sara/__init__.py
@@ -2,5 +2,5 @@ from oddkiva.sara.timer import *
 # The following imports may fail because PySide2 may not be available on recent
 # Python versions.
 from oddkiva.sara.graphics import *
-from oddkiva.sara.pybind11 import VideoStream
+# from oddkiva.sara.pybind11 import VideoStream
 from pysara_pybind11 import *
