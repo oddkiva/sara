c55c167992df3045a03f792b9f0752c7e25a9aa2
David OK <david.ok8@gmail.com>
2025-10-21 18:34:28 +0100

    MAINT: fix tomli usage.
    cf: https://pypi.org/project/tomli/

--- python/oddkiva/__init__.py
+++ python/oddkiva/__init__.py
@@ -3,9 +3,9 @@ import pathlib
 
 CONFIG_FILE_PATH= pathlib.Path(__file__).parent / 'config.toml'
 if CONFIG_FILE_PATH.exists():
-    with open(CONFIG_FILE_PATH, 'rb') as f:
+    with open(str(CONFIG_FILE_PATH), 'r') as f:
         from pip._vendor import tomli
-        CONFIG = tomli.load(f)
+        CONFIG = tomli.loads(f.read())
         DATA_DIR_PATH = pathlib.Path(CONFIG['data']['path'])
         assert DATA_DIR_PATH.exists(), \
             "DATA_DIR_PATH from config.toml is invalid"
