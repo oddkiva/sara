af1ec7af1e508fb5c88b27000d47561d22efa372
Odd Kiva <2375733-oddkiva@users.noreply.gitlab.com>
2025-12-02 20:54:35 +0000

    MAINT: complete data-driven tests.

--- python/oddkiva/brahma/torch/object_detection/detr/architectures/rtdetr/decoder/test/test_transformer_decoder.py
+++ python/oddkiva/brahma/torch/object_detection/detr/architectures/rtdetr/decoder/test/test_transformer_decoder.py
@@ -1,3 +1,5 @@
+from loguru import logger
+
 import torch
 import torch.nn.functional as F
 
@@ -14,6 +16,12 @@ CKPT_FILEPATH = (DATA_DIR_PATH / 'model-weights' / 'rtdetrv2' /
                  'rtdetrv2_r50vd_6x_coco_ema.pth')
 DATA_FILEPATH = (DATA_DIR_PATH / 'model-weights' / 'rtdetrv2' /
                  'rtdetrv2_r50vd_6x_coco_ema.data.pt')
+
+
+def relative_error(a: torch.Tensor, b: torch.Tensor):
+    num = torch.dist(a, b)
+    denom = torch.norm(a)
+    return num / denom
 
 
 def test_multiscale_deformable_attention_in_decoder_layer_0():
@@ -197,7 +205,7 @@ def test_multiscale_deformable_attention_in_decoder_layer_0():
     assert torch.dist(value_q_backprojected, value_q_backprojected_true) < 5e-5
 
 
-def test_transformer_decoder_layer_0_detailed():
+def test_transformer_decoder_layer_0_details():
     ckpt = RTDETRV2Checkpoint(CKPT_FILEPATH, torch.device('cpu'))
     data = torch.load(DATA_FILEPATH, torch.device('cpu'))
 
@@ -291,7 +299,7 @@ def test_transformer_decoder_layer_0_detailed():
     assert torch.norm(target - target_true) < 1.5e-4
 
 
-def test_transformer_decoder_layer_0():
+def test_transformer_decoder_layer_0_api():
     ckpt = RTDETRV2Checkpoint(CKPT_FILEPATH, torch.device('cpu'))
     data = torch.load(DATA_FILEPATH, torch.device('cpu'))
 
@@ -324,24 +332,162 @@ def test_transformer_decoder_layer_0():
     assert torch.norm(layer_out - layer_out_true) < 1.5e-4
 
 
-# def test_transformer_decoder():
-#     ckpt = RTDETRV2Checkpoint(CKPT_FILEPATH, torch.device('cpu'))
-#     data = torch.load(DATA_FILEPATH, torch.device('cpu'))
-#
-#     memory, memory_spatial_hw_sizes = \
-#         data['intermediate']['decoder']['_get_encoder_input']
-#     init_ref_contents, init_ref_points_unact, _, _ = \
-#         data['intermediate']['decoder']['_get_decoder_input']
-#
-#     box_geometries_true, box_class_logits_true = \
-#         data['intermediate']['decoder']['decoder']
-#
-#     decoder = ckpt.load_transformer_decoder()
-#     box_geometries, box_class_logits = decoder.forward(
-#         init_ref_contents.detach(), init_ref_points_unact,
-#         memory, memory_spatial_hw_sizes,
-#         value_mask=None
-#     )
-#
-#     assert torch.norm(box_geometries - box_geometries_true) < 1e-12
-#     assert torch.norm(box_class_logits - box_class_logits_true) < 1e-12
+def test_transformer_decoder_details():
+    ckpt = RTDETRV2Checkpoint(CKPT_FILEPATH, torch.device('cpu'))
+    data = torch.load(DATA_FILEPATH, torch.device('cpu'))
+
+    memory, memory_spatial_hw_sizes = \
+        data['intermediate']['decoder']['_get_encoder_input']
+    init_ref_contents, init_ref_points_unact, _, _ = \
+        data['intermediate']['decoder']['_get_decoder_input']
+
+    decoder = ckpt.load_transformer_decoder()
+    # box_geometries, box_class_logits = decoder.forward(
+    #     init_ref_contents.detach(), init_ref_points_unact,
+    #     memory, memory_spatial_hw_sizes,
+    #     value_mask=None
+    # )
+
+    query = init_ref_contents
+    query_geometry_logits = init_ref_points_unact
+    value = memory
+    value_spatial_sizes = memory_spatial_hw_sizes
+    value_mask = None
+    memory_mask = None
+    assert query.requires_grad is False
+    assert query_geometry_logits.requires_grad is False
+
+    # assert torch.norm(box_geometries - box_geometries_true) < 1e-12
+    # assert torch.norm(box_class_logits - box_class_logits_true) < 1e-12
+
+    layers_gt = data['intermediate']['decoder']['decoder.layer-by-layer']
+
+    # Get the actual query geometry by activating the logits with the
+    # sigmoid function.
+
+    # Initialize:
+    # - the current query embedding
+    # - the current query geometry logits (the object geometry before the
+    #   sigmoid activation)
+    # - the current query geometry (the object box geometry)
+    query_curr = query
+    query_geom_logits_curr = query_geometry_logits
+    query_geom_curr = F.sigmoid(query_geom_logits_curr)
+
+    query_next: torch.Tensor | None = None
+    query_class_logits_next: torch.Tensor | None = None
+    query_geom_logits_next: torch.Tensor | None = None
+    query_geom_next: torch.Tensor | None = None
+
+    query_geometries_denoised = []
+    query_class_logits_denoised = []
+
+    query_geometries_denoised_true: list[torch.Tensor] = layers_gt['dec_out_bboxes']
+    query_class_logits_denoised_true: list[torch.Tensor] = layers_gt['dec_out_logits']
+
+    for i, decoder_layer in enumerate(decoder.layers):
+        logger.debug(f'[{i}] Checking decoder layer {i}')
+        layer_gt_i = layers_gt[str(i)]
+
+        assert type(decoder_layer) is \
+            MultiScaleDeformableTransformerDecoderLayer
+
+        # Calculate the corresponding embed vector of the box geometry
+        query_geom_embed_curr = decoder.box_geometry_embedding_map\
+            .forward(query_geom_curr)
+        query_geom_embed_curr_true = layer_gt_i['query_pos_embed']
+        assert torch.dist(query_geom_embed_curr,
+                          query_geom_embed_curr_true) < 2.5e-3
+        assert relative_error(query_geom_embed_curr,
+                              query_geom_embed_curr_true) < 1e-5
+
+        query_curr_true = layer_gt_i['input_ref_contents']
+        assert torch.dist(query_curr, query_curr_true) < 2.5e-3
+        assert relative_error(query_curr, query_curr_true) < 2e-5
+
+        # Denoise the current query.
+        query_next = decoder_layer.forward(
+            query_curr, query_geom_curr,
+            value, value_spatial_sizes,
+            query_positional_embeds=query_geom_embed_curr,
+            attn_mask=value_mask, memory_mask=memory_mask
+        )
+        query_next_true = layer_gt_i['output_ref_contents']
+        assert torch.dist(query_next, query_next_true) < 2.5e-3
+        assert relative_error(query_next, query_next_true) < 2e-5
+
+        # Estimate the new object class logits (object class probabilities).
+        query_class_logits_next = decoder.box_class_logit_heads[i](query_next)
+        query_class_logits_next_true: torch.Tensor = query_class_logits_denoised_true[i]
+        assert query_class_logits_next is not None
+        assert torch.dist(query_class_logits_next,
+                          query_class_logits_next_true) < 2.5e-3
+        assert relative_error(query_class_logits_next,
+                              query_class_logits_next_true) < 1e-5
+
+        # Estimate the new object geometry (cx cy w h).
+        Δ_query_geom_logits = decoder.box_geometry_logit_heads[i](query_next)
+        query_geom_logits_next = \
+            query_geom_logits_curr + Δ_query_geom_logits
+        query_geom_next = F.sigmoid(query_geom_logits_next)
+        query_geom_next_true: torch.Tensor = layer_gt_i['inter_ref_bbox']
+        assert torch.dist(query_geom_next,
+                          query_geom_next_true) < 2.5e-3
+        assert relative_error(query_geom_next, query_geom_next_true) < 1e-5
+
+        # Store the denoised results.
+        query_geometries_denoised.append(query_geom_next)
+        query_class_logits_denoised.append(query_class_logits_next)
+
+        # Update for the next denoising iteration.
+        query_curr = query_next
+        query_geom_logits_curr = query_geom_logits_next
+        # Make sure that we only optimize the residuals at the training
+        # stage.
+        query_geom_curr = query_geom_next.detach()
+
+
+    for i, (qgeom, qgeom_true) in enumerate(zip(query_geometries_denoised,
+                                                query_geometries_denoised_true)):
+        logger.debug(f'[{i}] Checking iterative query geometry denoising {i}')
+        assert torch.dist(qgeom, qgeom_true) < 2.5e-3
+        assert relative_error(qgeom, qgeom_true) < 5e-6
+
+    for i, (qc, qc_true) in enumerate(zip(query_class_logits_denoised,
+                                          query_class_logits_denoised_true)):
+        logger.debug(f'[{i}] Checking iterative query class denoising {i}')
+        assert torch.dist(qc, qc_true) < 2.5e-3
+        assert relative_error(qc, qc_true) < 5e-6
+
+def test_transformer_decoder_api():
+    ckpt = RTDETRV2Checkpoint(CKPT_FILEPATH, torch.device('cpu'))
+    data = torch.load(DATA_FILEPATH, torch.device('cpu'))
+
+    memory, memory_spatial_hw_sizes = \
+        data['intermediate']['decoder']['_get_encoder_input']
+    init_ref_contents, init_ref_points_unact, _, _ = \
+        data['intermediate']['decoder']['_get_decoder_input']
+
+    decoder = ckpt.load_transformer_decoder()
+
+    query = init_ref_contents
+    query_geometry_logits = init_ref_points_unact
+    value = memory
+    value_spatial_sizes = memory_spatial_hw_sizes
+    value_mask = None
+    memory_mask = None
+    assert query.requires_grad is False
+    assert query_geometry_logits.requires_grad is False
+
+    layers_gt = data['intermediate']['decoder']['decoder.layer-by-layer']
+
+    box_geometries, box_class_logits = decoder.forward(
+        query, query_geometry_logits,
+        value, value_spatial_sizes,
+        value_mask=None
+    )
+    box_geometries_true = torch.stack(layers_gt['dec_out_bboxes'])
+    box_class_logits_true = torch.stack(layers_gt['dec_out_logits'])
+
+    assert relative_error(box_geometries, box_geometries_true) < 5e-6
+    assert relative_error(box_class_logits, box_class_logits_true) < 5e-6
