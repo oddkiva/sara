a37cd13c6846910888d034d24a539098919c22b0
David OK <david.ok8@gmail.com>
2026-02-06 10:14:18 +0000

    MAINT: reduce the frequency of the mosaic data transform.

--- python/oddkiva/brahma/torch/tasks/object_detection/configs/train_config_rtdetrv2_r50vd_coco.py
+++ python/oddkiva/brahma/torch/tasks/object_detection/configs/train_config_rtdetrv2_r50vd_coco.py
@@ -96,7 +96,8 @@ class TrainValTestDatasetConfig:
             #
             # probability=0.8,
             # probability=0.5,
-            probability=0.3,
+            # probability=0.3,
+            probability=0.25,
             fill_value=0,
             use_cache=False,
             max_cached_images=50,
